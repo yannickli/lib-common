2305459ab586fe2ba3e4946b876d827251b6b1de
Pierre Habouzit <pierre.habouzit@intersec.com>
2005-09-07 08:03:22 +0000

    add data stacks, and Makefile

---
+++ lib-common/data_stack.c
@@ -0,0 +1,319 @@
+#include <string.h>
+#include <stdio.h>
+#include <limits.h>
+
+#include "err_report.h"
+#include "data_stack.h"
+#include "macros.h"
+
+/******************************************************************************/
+/* private types / macros                                                     */
+/******************************************************************************/
+
+#define FRAME_COUNT     32
+#define MIN_BLOCK_SIZE  4096
+#define MEM_ALIGN_SIZE  8
+#define MAX_DS_ALLOC    ((size_t)1 << (sizeof(size_t)*8-1))
+
+#define MEM_ALIGN(size) \
+    ( ((size) + MEM_ALIGN_SIZE - 1) & ~((unsigned int) MEM_ALIGN_SIZE-1) )
+
+typedef struct block_t {
+    struct block_t * next;
+    size_t size, left;
+
+    /*  unsigned char data[]; */
+} block_t;
+
+#define SIZEOF_BLOCK MEM_ALIGN(sizeof(block))
+#define BLOCK_CUR_PAYLOAD(block)                    \
+    ((unsigned char *) (block) + SIZEOF_BLOCK) +    \
+    (block->size - block->left)
+
+typedef struct frame_t {
+    struct frame_t * prev;
+    block_t * block[FRAME_COUNT];
+    size_t space_left[FRAME_COUNT];
+} frame_t;
+
+/******************************************************************************/
+/* private variables                                                          */
+/******************************************************************************/
+
+static struct {
+    int fpos;           /* our position in the current frame */
+    frame_t * frame;    /* current frame */
+    block_t * block;    /* current block */
+
+    void * buffer;      /* the last data segment returned */
+    size_t size;        /* the last requested size */
+} current = { -1, NULL, NULL, NULL, 0 };
+
+static struct {
+    frame_t * frames;   /* unused frame*s* */
+    block_t * block;    /* largest unused block */
+} unused = { NULL, NULL };
+
+/******************************************************************************/
+/* private functions                                                          */
+/******************************************************************************/
+
+/* free_next_blocks(block)
+ *
+ * every block listed from block->next will be deallocated.
+ *
+ * One exception :
+ *   if one of those blocks is bigger than the current unused.block,
+ *   it will replace the current unused.block, be reseted in a state where it's
+ *   immediately usable, and the current unused.block will be free'd instead.
+ *
+ * returns void
+ */
+static void free_next_blocks(block_t * block)
+{
+    block_t * next_next;
+    int reset_block_invariant = 0;
+
+    while (block->next != NULL) {
+        next_next = block->next->next;
+
+        if (unused.block == NULL || block->next->size > unused.block->size) {
+            free(unused.block);
+            unused.block = block->next;
+            reset_block_invariant = 1;
+        }
+        else {
+            free(block->next);
+        }
+
+        block->next = next_next;
+    }
+
+    if (reset_block_invariant) {
+        /* new block invariant */
+        unused.block->left = unused.block->size;
+        unused.block->next = NULL;
+    }
+}
+
+/* mem_block_alloc(min_size)
+ *
+ * Force the allocation (don't use unused.block) of a block of size of at least
+ * min_size bytes.
+ *
+ * returns a pointer to the allocated block_t
+ */
+static void * mem_block_alloc(size_t min_size)
+{
+    static size_t last_alloc_size = MIN_BLOCK_SIZE;
+    block_t * block;
+
+    min_size += last_alloc_size;
+    e_assert( min_size <= MAX_DS_ALLOC );
+
+    while (last_alloc_size <= min_size) {
+        last_alloc_size <<= 1;
+    }
+
+    block = malloc(SIZEOF_BLOCK + last_alloc_size);
+    if (block == NULL) {
+        e_fatal(FATAL_NOMEM, E_PREFIX("not enough memory"));
+    }
+
+    /* new block invariant */
+    block->size = last_alloc_size;
+    block->left = last_alloc_size;
+    block->next = NULL;
+
+    return block;
+}
+
+/* malloc_real(size_t, permanent)
+ *
+ * the pseudo-allocation function : tries to reuse unused.block, and if not
+ * possible, allocate a new block.
+ *
+ * if bloc is made permanent, it is protected from further ds_get/ds_reget.
+ * this function is responsible of maintaining the `current.size' and
+ * `current.buffer' invariants for ds_get/reget/tie functions.
+ *
+ * returns a pointer to the usable buffer.
+ */
+static void * malloc_real(size_t size, int permanent)
+{
+    block_t * block;
+    void * ret;
+
+    if (size == 0) return NULL;
+
+    size = MEM_ALIGN(size);
+
+    if (current.block->left < size) {
+        /* we need a new block */
+        if (unused.block != NULL && unused.block->size >= size) {
+            block        = unused.block;
+            unused.block = NULL;
+        }
+        else {
+            block = mem_block_alloc(size);
+        }
+
+        current.block->next = block;
+        current.block       = block;
+    }
+
+    ret = BLOCK_CUR_PAYLOAD(current.block);
+    if (permanent) {
+        current.block->left -= size;
+        current.buffer       = NULL;
+        current.size         = 0;
+    } else {
+        current.buffer       = ret;
+        current.size         = size;
+    }
+    return ret;
+}
+
+/******************************************************************************/
+/* public API                                                                 */
+/******************************************************************************/
+
+int ds_push()
+{
+    frame_t * frame;
+
+    if (current.fpos < 0) {
+        /* kludge to force ds_init() when forgeted */
+        ds_init();
+    }
+
+    current.fpos ++;
+
+    if (current.fpos == FRAME_COUNT) {
+        current.fpos = 0;
+        if (unused.frames == NULL) {
+            /* allocate new block */
+            frame = (frame_t *)calloc(1, sizeof(frame_t));
+            if (frame == NULL) {
+                e_fatal(FATAL_NOMEM, E_PREFIX("not enough memory"));
+            }
+        }
+        else {
+            /* use existing unused frame */
+            frame         = unused.frames;
+            unused.frames = unused.frames->prev;
+        }
+
+        frame->prev   = current.frame;
+        current.frame = frame;
+    }
+
+    current.frame->block[current.fpos]      = current.block;
+    current.frame->space_left[current.fpos] = current.block->left;
+    current.size   = 0;
+    current.buffer = NULL;
+
+    return current.fpos;
+}
+
+int ds_pop()
+{
+    frame_t * frame;
+    int ret;
+
+    if (current.frame == NULL) {
+        e_panic("ds_pop() called with empty stack");
+    }
+
+    current.size        = 0;
+    current.buffer      = NULL;
+    current.block       = current.frame->block[current.fpos];
+    current.block->left = current.frame->space_left[current.fpos];
+    free_next_blocks(current.block->next);
+
+    ret = current.fpos;
+
+    if (current.fpos > 0) {
+        current.fpos--;
+    }
+    else {
+        current.fpos  = FRAME_COUNT - 1;
+
+        frame         = current.frame;
+        current.frame = frame->prev;
+
+        frame->prev   = unused.frames;
+        unused.frames = frame;
+    }
+
+    return ret;
+}
+
+void * ds_get(size_t size)
+{
+    return malloc_real(size, 0);
+}
+
+void * ds_reget(void * buffer, size_t size)
+{
+    size_t old_size;
+    void * old_buffer;
+
+    e_assert(buffer == current.buffer);
+    
+    old_buffer = current.buffer;
+    old_size   = current.size;
+    if (old_buffer != ds_get(size)) {
+        /* old_size < size stands : else we would'nt have needed realloc */
+        memcpy(current.buffer, old_buffer, old_size);
+    }
+
+    return current.buffer;
+}
+
+void ds_tie(size_t size)
+{
+    e_assert(current.buffer != NULL);
+    e_assert(current.size >= size);
+    e_assert(current.block->left >= size);
+
+    malloc_real(size, 1);
+}
+
+void ds_init()
+{
+    if (current.fpos >= 0) {
+        e_warning(E_PREFIX("ds_init() called twice"));
+        return;
+    }
+
+    current.fpos = FRAME_COUNT - 1;
+    current.block = mem_block_alloc(MIN_BLOCK_SIZE);
+}
+
+void ds_deinit()
+{
+    frame_t * frame;
+
+    if (current.fpos != FRAME_COUNT-1 && current.frame != NULL) {
+        e_panic(MISSING_DSPOP);
+    }
+
+    while (unused.frames != NULL) {
+        frame         = unused.frames;
+        unused.frames = unused.frames->prev;
+        free(frame);
+    }
+
+    if (current.block != NULL) {
+        free0(current.block);
+    }
+    if (unused.block  != NULL) {
+        free0(unused.block);
+    }
+
+    current.fpos   = -1;
+    current.buffer = NULL;
+    current.size   = 0;
+}
+
---
+++ lib-common/data_stack.h
@@ -0,0 +1,68 @@
+#ifndef IS_DATA_STRACK_H
+#define IS_DATA_STRACK_H
+
+#include <stdlib.h>
+
+#include "err_report.h"
+
+#define MISSING_DSPOP "missing ds_pop() call"
+
+/* ds_push/ds_pop :
+ *
+ * ds_push create a new frame
+ * ds_pop forgets everything that has been (re)get-ed or tie-d since last ds_push
+ */
+int ds_push(void);
+int ds_pop(void);
+
+/* this macro verifies that the pop-ed frame is the one we pushed
+ *
+ * this is a macro since it uses __LINE__/__FILE__ magic
+ */
+#define ds_pop_check(frame)                     \
+    do {                                        \
+        if (frame != ds_pop()) {                \
+            e_panic(E_PREFIX(MISSING_DSPOP));   \
+        }                                       \
+    } while(0)                                  \
+
+/* ds_get :
+ *   allocate some new buffer.
+ *   - if ds_get is called twice, the second buffer may reuse the space used by
+ *     the first one
+ *   - a call to ds_push will invalidate any get-ed buffer if not ds_tie-d
+ *
+ * ds_reget :
+ *   expand the last get-ed buffer (with reallocation if needed).
+ *   be carefull, the buffer address may change
+ *
+ *   ds_reget ensure that the rignt buffer is beeing expansed.
+ *   calls to ds_reget without prior ds_get calls may cause strange behaviours
+ *   if misunderstood.
+ *
+ * ds_tie :
+ *   make last get/reget call permanent (until next ds_pop).
+ */
+void * ds_get(size_t size);
+void * ds_reget(void * buffer, size_t size);
+void ds_tie(size_t size);
+
+/*
+ * type safe macros for ds_get/reget
+ */
+#define ds_get_type(type, count)   ((type)*)(ds_get(sizeof(type)*(count)))
+#define ds_reget_type(type, count) ((type)*)(ds_get(sizeof(type)*(count)))
+
+/*
+ * init/deinit functions
+ *
+ * init function is optional, since ds_push has to be called first,
+ * and ds_push ensure that ds_init has been called.
+ *
+ * ds_deinit() has to be run in order to avoid valgrind (or alike)
+ * memory leaks warnings.
+ */
+void ds_init(void);
+void ds_deinit(void);
+
+#endif
