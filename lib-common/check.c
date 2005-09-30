a3d32e581314b3484d2eef1868b206f09c5e340a
ogu <ogu@intersec.com>
2005-09-30 14:27:01 +0000

    test string functions before blob functions that rely on them.

--- lib-common/check.c
+++ lib-common/check.c
@@ -9,12 +9,11 @@ int main(void)
     int nf;
     SRunner * sr = srunner_create(NULL);
 
-    srunner_add_suite(sr, check_make_blob_suite());
     srunner_add_suite(sr, check_string_is_suite());
+    srunner_add_suite(sr, check_make_blob_suite());
 
     srunner_run_all(sr, CK_NORMAL);
     nf = srunner_ntests_failed(sr);
     srunner_free(sr);
     return (nf == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
-
 }
