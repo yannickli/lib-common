deb6cd4bf3ce2bb82fdb37c8565856977a3c503c
Flavien Lebarbé <flavien.lebarbe@intersec.com>
2006-03-13 16:39:03 +0000

    Moved pool.h to lib-common so that other tools can use them too.

---
+++ lib-common/pool.h
@@ -0,0 +1,46 @@
+#ifndef __SPOOL_H__
+#define __SPOOL_H__
+
+#define GENERIC_POOL(type, size, New, Reset, Delete, attr)              \
+                                                                        \
+struct {                                                                \
+    int avail;                                                          \
+    type *pool[size];                                                   \
+} type##_pool;                                                          \
+                                                                        \
+attr type *type##_new(void)                                             \
+{                                                                       \
+    if (type##_pool.avail) {                                            \
+        return type##_pool.pool[--type##_pool.avail];                   \
+    }                                                                   \
+    return New();                                                       \
+}                                                                       \
+                                                                        \
+attr void type##_delete(type **value)                                   \
+{                                                                       \
+    if (value && *value) {                                              \
+        if (type##_pool.avail < size) {                                 \
+            Reset(*value);                                              \
+            type##_pool.pool[type##_pool.avail++] = *value;             \
+            value = NULL;                                               \
+        } else {                                                        \
+            Delete(value);                                              \
+        }                                                               \
+    }                                                                   \
+}                                                                       \
+                                                                        \
+static inline void type##_pool_empty(void)                              \
+{                                                                       \
+    while (type##_pool.avail > 0) {                                     \
+        --type##_pool.avail;                                            \
+        Delete(&type##_pool.pool[type##_pool.avail]);                   \
+    }                                                                   \
+}
+
+#define POOL(type, size, New, Reset, Delete)    \
+    GENERIC_POOL(type, size, New, Reset, Delete,)
+
+#define STATIC_POOL(type, size, New, Reset, Delete)    \
+    GENERIC_POOL(type, size, New, Reset, Delete, static)
+
+#endif
