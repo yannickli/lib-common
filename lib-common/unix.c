a3f9db5a40ab60ee283620168302b40d34dc7276
Flavien Lebarbé <flavien.lebarbe@intersec.com>
2006-06-14 15:48:15 +0000

    Added (non implemented) mkdir_p.

---
+++ lib-common/unix.c
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+#include "unix.h"
+/* Returns 0 if directory exists,
+ * Returns 1 if directory was created
+ * Returns -1 in case an error occured.
+ */
+int mkdir_p(const char *dir, mode_t mode)
+{
+/* FIXME : TOCODE !
+ */
+    dir = dir;
+    mode = mode;
+    return 0;
+}
---
+++ lib-common/unix.h
@@ -0,0 +1,5 @@
+#ifndef IS_UNIX_H
+#define IS_UNIX_H
+#include <sys/types.h>
+int mkdir_p(const char *dir, mode_t mode);
+#endif
