d866fded53019fe72e04f6ff6aa8fda9f9353adb
Flavien Lebarbé <flavien.lebarbe@intersec.com>
2006-06-27 07:25:17 +0000

    Implementation bits have nothing to do in the public header file.

--- lib-common/template.c
+++ lib-common/template.c
@@ -12,6 +12,64 @@
 #define VAR_START "${"
 #define VAR_END "}"
 
+/* A template is made of parts.
+ * Every part is encoded with a specific encoding
+ * Every part is made of parts that are either verbatim, chunked or evaluated
+ */
+typedef struct part_multi part_multi;
+
+struct template {
+    FILE *datafd;
+    part_multi *body;
+};
+
+typedef enum part_encoding {
+    ENC_NONE = 1,
+    ENC_HTML,
+} part_encoding;
+
+typedef enum part_type {
+    PART_VERBATIM,
+    PART_VARIABLE,
+    PART_MULTI,
+} part_type;
+
+typedef struct part_verbatim {
+    byte *data;
+    int size;
+} part_verbatim;
+
+typedef struct part_variable {
+    int index; /* Index of the variable in the data file */
+    part_encoding enc;
+} part_variable;
+
+typedef struct tpl_part {
+    part_type type;
+    union {
+        part_verbatim *verbatim;
+        part_variable *variable;
+        part_multi *multi;
+    } u;
+} tpl_part;
+
+struct part_multi {
+    part_encoding enc; /* Apply this encoding to the whole content */
+    int nbparts;
+    tpl_part parts[];
+};
+
+void part_multi_delete(part_multi **multi);
+void part_multi_wipe(part_multi *multi);
+
+part_variable *part_variable_new(int ind);
+void part_variable_delete(part_variable **variable);
+void part_variable_wipe(part_variable *variable);
+
+part_verbatim *part_verbatim_new(const char*src, int size);
+void part_verbatim_delete(part_verbatim **verbatim);
+void part_verbatim_wipe(part_verbatim *verbatim);
+
 template *template_new(const char *templatefile, const char *datafile)
 {
     int tplfd;
