8a1bf85a045548f8f0ac186f15be745d981359c6
ogu <ogu@intersec.com>
2006-06-27 12:59:41 +0000

    use field names in substitution code fixed various bugs

--- lib-common/msg_template.c
+++ lib-common/msg_template.c
@@ -10,7 +10,9 @@
 #include "msg_template.h"
 
 #define VAR_START "${"
+#define VAR_START_LEN  2
 #define VAR_END "}"
+#define VAR_END_LEN    1
 
 /* A template is made of parts.
  * Every part is encoded with a specific encoding
@@ -72,27 +74,59 @@ void part_verbatim_wipe(part_verbatim *verbatim);
 
 msg_template *msg_template_new(const char *templatefile, const char *datafile)
 {
+    char line[1024];
+    char *fields[256];
+    int i, nbfields;
     int tplfd;
     msg_template *tpl;
     char *buf, *p, *q;
     struct stat st;
     int toread, nb, size;
 
+    buf = NULL;
+    tplfd = -1;
     tpl = p_new(msg_template, 1);
 
-    tplfd = open(templatefile, O_RDONLY);
-    if (tplfd < 0) {
-        e_error("open template '%s' failed\n", templatefile);
-        goto error;
-    }
-    tpl->datafd = fopen(datafile, "rb");
+    /* Open the datafile and parse field name line */
+    tpl->datafd = fopen(datafile, "r");
     if (!tpl->datafd) {
         e_error("open datafile '%s' failed\n", datafile);
         goto error;
     }
 
-    /* FIXME: 10 is fixed and never checked. This is a bad idea */
-    tpl->body = calloc(1, sizeof(part_multi) + 10 * sizeof(tpl_part));
+    nbfields = 0;
+    if (!fgets(line, sizeof(line), tpl->datafd)) {
+        /* Data file is empty no fields defined */
+        //e_error("empty datafile '%s'\n", datafile);
+        //goto error;
+    } else {
+        /* Parse field names */
+        p = line;
+        q = p + strlen(p);
+        if (q > p && q[-1] == '\n')
+            *--q = '\0';
+
+        while (*p) {
+            fields[nbfields++] = p;
+            q = strchr(p, ';');
+            if (!q) {
+                break;
+            }
+            *q = '\0';
+            p = q + 1;
+        }
+    }
+    printf("Nbfields:%d\n", nbfields);
+
+    /* Open and load the template file */
+    tplfd = open(templatefile, O_RDONLY);
+    if (tplfd < 0) {
+        e_error("open template '%s' failed\n", templatefile);
+        goto error;
+    }
+
+    /* FIXME: 128 is fixed and never checked. This is a bad idea */
+    tpl->body = calloc(1, sizeof(part_multi) + 128 * sizeof(tpl_part));
 
     if (stat(templatefile, &st) != 0) {
         e_error("Unable to stat '%s'\n", templatefile);
@@ -104,7 +138,9 @@ msg_template *msg_template_new(const char *templatefile, const char *datafile)
     buf = p_new(char, toread);
     p = buf;
 
-    while (toread) {
+    printf("buf=%p\n", buf);
+
+    while (toread > 0) {
         nb = read(tplfd, p, toread);
         if (nb < 0) {
             if (errno == EINTR || errno == EAGAIN) {
@@ -125,11 +161,10 @@ msg_template *msg_template_new(const char *templatefile, const char *datafile)
     /* FIXME: should look for variable names on the first line of the
      * data file and use their indexes. For now assume they're in the same 
      * order in the template and in the datafile, without repetitions. */
-    int var = 0;
-    while(size) {
+    while (size > 0) {
         int verb_size;
         int var_size;
-        q = vmemsearch(p, size, VAR_START, strlen(VAR_START));
+        q = vmemsearch(p, size, VAR_START, VAR_START_LEN);
         if (!q) {
             /* Last chunk */
             curpart->type = PART_VERBATIM;
@@ -145,9 +180,9 @@ msg_template *msg_template_new(const char *templatefile, const char *datafile)
             curpart++;
             tpl->body->nbparts++;
         }
-        p += strlen(VAR_START) + verb_size;
-        size -= strlen(VAR_START) + verb_size;
-        q = vmemsearch(p, size, VAR_END, strlen(VAR_END));
+        p += verb_size + VAR_START_LEN;
+        size -= verb_size + VAR_START_LEN;
+        q = vmemsearch(p, size, VAR_END, VAR_END_LEN);
         if (!q) {
             /* Closing tag not found... */
             goto error;
@@ -155,42 +190,61 @@ msg_template *msg_template_new(const char *templatefile, const char *datafile)
         }
         var_size = q - p;
         if (var_size) {
-            curpart->type = PART_VARIABLE;
-            /* FIXME: See above comment about var */
-            curpart->u.variable = part_variable_new(var++);
+
+            for (i = 0; i < nbfields; i++) {
+                if (!strncmp(p, fields[i], var_size) && fields[i][var_size] == '\0')
+                    break;
+            }
+
+            if (i < nbfields) {
+                curpart->type = PART_VARIABLE;
+                curpart->u.variable = part_variable_new(i);
+            } else {
+                // Should warn about undefined variable
+                printf("variable not found: %.*s\n", var_size, p);
+
+                curpart->type = PART_VERBATIM;
+                curpart->u.verbatim = part_verbatim_new(p - VAR_START_LEN,
+                    var_size + VAR_START_LEN + VAR_END_LEN);
+            }
             curpart++;
             tpl->body->nbparts++;
         }
-        p += strlen(VAR_END) + var_size;
-        size -= strlen(VAR_END) + var_size;
+        p += var_size + VAR_END_LEN;
+        size -= var_size + VAR_END_LEN;
     }
-    p_delete(&buf);
 
-    int i;
     printf("nbparts:%d\n", tpl->body->nbparts);
-    for(i = 0; i < tpl->body->nbparts; i++) {
+    for (i = 0; i < tpl->body->nbparts; i++) {
         curpart = &tpl->body->parts[i];
         printf("[%02d]: ", i);
-        switch(curpart->type) {
-            case PART_VERBATIM:
-                printf("VERBATIM: (%d) '%s'\n", curpart->u.verbatim->size,
-                       curpart->u.verbatim->data);
+        switch (curpart->type) {
+          case PART_VERBATIM:
+              printf("VERBATIM: (%d) '%s'\n",
+                     curpart->u.verbatim->size,
+                     curpart->u.verbatim->data);
             break;
-            case PART_VARIABLE:
-                printf("VARIABLE: %d\n", curpart->u.variable->index);
+          case PART_VARIABLE:
+            printf("VARIABLE: %d\n", curpart->u.variable->index);
             break;
-            case PART_MULTI:
-                printf("MULTI: [skipped]\n");
+          case PART_MULTI:
+            printf("MULTI: [skipped]\n");
             break;
         }
     }
+    close(tplfd);
+
+    printf("buf=%p\n", buf);
+    p_delete(&buf);
+
     return tpl;
 
-error:
+  error:
     if (tplfd >= 0) {
         close(tplfd);
         tplfd = -1;
     }
+    p_delete(&buf);
     msg_template_delete(&tpl);
     return NULL;
 }
@@ -206,24 +260,25 @@ void msg_template_delete(msg_template **tpl)
     if ((*tpl)->body) {
         part_multi_delete(&(*tpl)->body);
     }
-    p_delete (tpl);
+    p_delete(tpl);
 }
 
 void part_multi_wipe(part_multi *multi)
 {
     tpl_part *curpart;
     int i;
-    for(i = 0; i < multi->nbparts; i++) {
+
+    for (i = 0; i < multi->nbparts; i++) {
         curpart = &multi->parts[i];
-        switch(curpart->type) {
-            case PART_VERBATIM:
-                part_verbatim_delete(&curpart->u.verbatim);
+        switch (curpart->type) {
+          case PART_VERBATIM:
+            part_verbatim_delete(&curpart->u.verbatim);
             break;
-            case PART_VARIABLE:
-                part_variable_delete(&curpart->u.variable);
+          case PART_VARIABLE:
+            part_variable_delete(&curpart->u.variable);
             break;
-            case PART_MULTI:
-                part_multi_delete(&curpart->u.multi);
+          case PART_MULTI:
+            part_multi_delete(&curpart->u.multi);
             break;
         }
     }
@@ -239,9 +294,11 @@ void part_multi_delete(part_multi **multi)
 part_verbatim *part_verbatim_new(const char *src, int size)
 {
     part_verbatim *verb;
+
     verb = p_new(part_verbatim, 1);
-    verb->data = p_new(byte, size);
+    verb->data = p_new(byte, size + 1);
     memcpy(verb->data, src, size);
+    verb->data[size] = '\0';
     verb->size = size;
     return verb;
 }
@@ -261,6 +318,7 @@ void part_verbatim_delete(part_verbatim **vb)
 part_variable *part_variable_new(int ind)
 {
     part_variable *var;
+
     var = p_new(part_variable, 1);
     var->index = ind;
     return var;
@@ -282,14 +340,17 @@ int msg_template_getnext(msg_template *tpl, blob_t *output)
     int i, nbfields;
     char line[1024];
     char *fields[256], *p, *q;
-    const char *line_end;
+
     if (!fgets(line, sizeof(line), tpl->datafd)) {
-        return 0;
+        return -1;
     }
     p = line;
-    line_end = p + strlen(line);
+    q = p + strlen(p);
+    if (q > p && q[-1] == '\n')
+        *--q = '\0';
+    
     nbfields = 0;
-    while (p < line_end) {
+    while (*p) {
         fields[nbfields++] = p;
         q = strchr(p, ';');
         if (!q) {
@@ -300,26 +361,26 @@ int msg_template_getnext(msg_template *tpl, blob_t *output)
     }
 
     printf("Nbparts:%d\n", tpl->body->nbparts);
-    for(i = 0; i < tpl->body->nbparts; i++) {
+    for (i = 0; i < tpl->body->nbparts; i++) {
         curpart = &tpl->body->parts[i];
         printf("[%d] ", i);
-        switch(curpart->type) {
-            case PART_VERBATIM:
-                printf("Verbatim:'%s'\n", curpart->u.verbatim->data);
-                blob_append_data(output, curpart->u.verbatim->data, 
-                                 curpart->u.verbatim->size);
+        switch (curpart->type) {
+          case PART_VERBATIM:
+            printf("Verbatim:'%s'\n", curpart->u.verbatim->data);
+            blob_append_data(output, curpart->u.verbatim->data, 
+                             curpart->u.verbatim->size);
             break;
-            case PART_VARIABLE:
-                if (curpart->u.variable->index > nbfields) {
-                    /* Ignore non-specified fields */
-                    break;
-                }
-                printf("Var:%d\n", curpart->u.variable->index);
-                blob_append_cstr(output, fields[curpart->u.variable->index]);
+          case PART_VARIABLE:
+            if (curpart->u.variable->index > nbfields) {
+                /* Ignore non-specified fields */
+                break;
+            }
+            printf("Var:%d\n", curpart->u.variable->index);
+            blob_append_cstr(output, fields[curpart->u.variable->index]);
             break;
-            case PART_MULTI:
-                /* FIXME */
-                blob_append_cstr(output, "***MULTI***");
+          case PART_MULTI:
+            /* FIXME */
+            blob_append_cstr(output, "***MULTI***");
             break;
         }
     }
@@ -334,6 +395,7 @@ START_TEST(check_msg_template_simple)
 {
     blob_t out;
     msg_template *tpl;
+
     tpl = msg_template_new("../samples/simple.tpl", "../samples/simple.csv");
     fail_if(tpl == NULL, "msg_template_new failed");
 
@@ -366,15 +428,14 @@ int main(void)
 {
     blob_t out;
     msg_template *tpl;
+
     tpl = msg_template_new("samples/simple.tpl", "samples/simple.csv");
     printf("tpl:%p\n", tpl);
 
     blob_init(&out);
-    if (msg_template_getnext(tpl, &out)) {
-        printf("getnext failed\n");
-        return 1;
+    while (!msg_template_getnext(tpl, &out)) {
+        printf("out:%s\n", out.data);
     }
-    printf("out:%s\n", out.data);
     msg_template_delete(&tpl);
     return 0;
 }
