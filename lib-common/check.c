126afdec455b88503c66fc17a3a6ff0b5d01b4a0
ogu <ogu@intersec.com>
2006-06-27 12:58:36 +0000

    fixed bad function name

--- lib-common/check.c
+++ lib-common/check.c
@@ -3,7 +3,7 @@
 
 #include "blob.h"
 #include "string_is.h"
-#include "template.h"
+#include "msg_template.h"
 
 int main(void)
 {
@@ -12,7 +12,7 @@ int main(void)
 
     srunner_add_suite(sr, check_string_is_suite());
     srunner_add_suite(sr, check_make_blob_suite());
-    srunner_add_suite(sr, check_template_suite());
+    srunner_add_suite(sr, check_msg_template_suite());
 
     srunner_run_all(sr, CK_NORMAL);
     nf = srunner_ntests_failed(sr);
