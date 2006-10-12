02252d0d2739e0a8375abad03985154827d60397
Pierre Habouzit <pierre.habouzit@intersec.com>
2006-10-12 22:52:08 +0000

    I'm tired of errors due to p_delete.
    we have GCC, damnit, use it to find type errors, thanks to the marvelous
    *typeof* construction !
    
     mem.h |   17 ++++++++++++++++-
     1 file changed, 16 insertions(+), 1 deletion(-)

--- lib-common/mem.h
+++ lib-common/mem.h
@@ -83,11 +83,26 @@ static inline void *mem_realloc0(void *mem, ssize_t oldsize, ssize_t newsize)
 #define p_blank(type, p, count) ((type *)memset(p, 0, sizeof(type) * (count)))
 #define p_new_raw(type, count)  ((type *)mem_alloc(sizeof(type) * (count)))
 #define p_new(type, count)      ((type *)mem_alloc0(sizeof(type) * (count)))
-#define p_delete(mem_p)         do {                             \
-                                    void *__ptr = (mem_p);       \
-                                    mem_free(*(void **)__ptr);   \
-                                    *(void **)__ptr = NULL;      \
-                                } while (0)
+
+#ifdef __GNUC__
+
+#  define p_delete(mem_pp)                      \
+        do {                                    \
+            typeof(**mem_pp) **ptr = mem_pp;    \
+            mem_free(*(void**)ptr);             \
+            *ptr = NULL;                        \
+        } while (0)
+
+#else
+
+#  define p_delete(mem_p)                \
+        do {                             \
+            void *__ptr = (mem_p);       \
+            mem_free(*(void **)__ptr);   \
+            *(void **)__ptr = NULL;      \
+        } while (0)
+
+#endif
 
 /* OG: should find a better name */
 #define p_renew(type, mem, oldcount, newcount) \
