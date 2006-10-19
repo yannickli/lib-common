50623a063d30634ca123eae9b6312843e49f403d
ogu <ogu@intersec.com>
2006-10-19 03:13:53 +0000

    cosmetics renamed some fields round size in mfp_malloc() to keep proper alignment renamed UPPER_MULTIPLE to ROUND_MULTIPLE comments, remarks

--- lib-common/mem-fifo-pool.c
+++ lib-common/mem-fifo-pool.c
@@ -17,6 +17,8 @@
 #include "mem.h"
 #include "mem-fifo-pool.h"
 
+#define ROUND_MULTIPLE(n, k) ((((n) + (k) - 1) / (k)) * (k))
+
 typedef struct mem_page {
     struct mem_page *next;
 
@@ -30,7 +32,7 @@ typedef struct mem_fifo_pool {
     mem_pool funcs;
     mem_page *pages;
     mem_page *freelist;
-    int pages_size;
+    int page_size;
     int nb_pages;
 } mem_fifo_pool;
 
@@ -39,7 +41,7 @@ static mem_page *mem_page_new(mem_fifo_pool *mfp)
 {
     mem_page *page = p_new(mem_page, 1);
 
-    page->size = mfp->pages_size;
+    page->size = mfp->page_size;
     page->area = mmap(NULL, page->size, PROT_READ | PROT_WRITE,
                       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
@@ -75,7 +77,7 @@ static inline bool mem_page_contains(mem_page *page, void *mem)
 
 static inline int mem_page_size_left(mem_page *page)
 {
-    return  page->size - page->used_size;
+    return (page->size - page->used_size);
 }
 
 static void *mfp_alloc(mem_pool *mp, ssize_t size)
@@ -83,21 +85,24 @@ static void *mfp_alloc(mem_pool *mp, ssize_t size)
     mem_fifo_pool *mfp = (mem_fifo_pool *)mp;
     void *res;
 
-    if (size > mfp->pages_size) {
+    /* Must round size up to keep proper alignment */
+    size = ROUND_MULTIPLE((size_t)size, 8);
+    
+    if (size > mfp->page_size) {
+        /* Should just map a larger page, yet we need a maximum value */
         e_panic(E_PREFIX("tried to alloc %zd bytes, cannot have more than %d"),
-                size, mfp->pages_size);
+                size, mfp->page_size);
     }
 
     if (!mfp->pages || mem_page_size_left(mfp->pages) < size) {
         if (mfp->freelist) {
-            mem_page *page;
-
             /* push the first free page on top of the pages list */
-            page = mfp->freelist;
+            mem_page *page = mfp->freelist;
             mfp->freelist = page->next;
             page->next    = mfp->pages;
             mfp->pages    = page;
         } else {
+            /* mmap a new page */
             mem_page *page = mem_page_new(mfp);
             mfp->nb_pages++;
             page->next = mfp->pages;
@@ -128,11 +133,17 @@ static void mfp_free(struct mem_pool *mp, void *mem)
 
         if (--page->used_blocks == 0) {
             *pagep = page->next;
+            /* OG: should release last page, so empty pool is indeed
+             * empty */
             if (mfp->freelist) {
                 mem_page_delete(&page);
                 mfp->nb_pages--;
             } else {
+                /* must clear area to 0 to ensure allocated blocks are
+                 * cleared.  mfp_malloc doesn't clear them.
+                 */
                 mem_page_reset(page);
+                page->next = mfp->freelist;
                 mfp->freelist = page;
             }
         }
@@ -148,22 +159,22 @@ static mem_pool mem_fifo_pool_funcs = {
     &mfp_free
 };
 
-#define UPPER_MULTIPLE(n, k) (((n + k - 1) / k) * k)
-
-mem_pool *mem_fifo_pool_new(int pages_size_hint)
+mem_pool *mem_fifo_pool_new(int page_size_hint)
 {
-    mem_fifo_pool *res;
+    mem_fifo_pool *mfp;
 
-    res             = p_new(mem_fifo_pool, 1);
-    res->funcs      = mem_fifo_pool_funcs;
-    res->pages_size = UPPER_MULTIPLE(pages_size_hint, 4096);
-    res->pages      = mem_page_new(res);
-    res->nb_pages   = 1;
+    mfp             = p_new(mem_fifo_pool, 1);
+    mfp->funcs      = mem_fifo_pool_funcs;
+    mfp->page_size  = ROUND_MULTIPLE(page_size_hint, 4096);
+    /* OG: this is not needed, an empty pool should be empty */
+    mfp->pages      = mem_page_new(mfp);
+    mfp->nb_pages   = 1;
 
-    return (mem_pool *)res;
+    return (mem_pool *)mfp;
 }
 
-void mem_fifo_pool_delete(mem_pool **poolp) {
+void mem_fifo_pool_delete(mem_pool **poolp)
+{
     if (*poolp) {
         mem_fifo_pool *mfp = (mem_fifo_pool *)(*poolp);
 
