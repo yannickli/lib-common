e54ace46fd407bda35db176503fbdd21f213df49
ogu <ogu@intersec.com>
2007-02-26 11:13:33 +0000

    added copyright notice, allow argument-less invokation to fix check target

--- lib-common/btree-dump.c
+++ lib-common/btree-dump.c
@@ -1,17 +1,31 @@
+/**************************************************************************/
+/*                                                                        */
+/*  Copyright (C) 2004-2007 INTERSEC SAS                                  */
+/*                                                                        */
+/*  Should you receive a copy of this source code, you must check you     */
+/*  have a proper, written authorization of INTERSEC to hold it. If you   */
+/*  don't have such an authorization, you must DELETE all source code     */
+/*  files in your possession, and inform INTERSEC of the fact you obtain  */
+/*  these files. Should you not comply to these terms, you can be         */
+/*  prosecuted in the extent permitted by applicable law.                 */
+/*                                                                        */
+/**************************************************************************/
+
+#include <stdio.h>
+
 #include "btree.h"
 
 int main(int argc, char **argv)
 {
     btree_t *bt;
 
-    assert(argc == 2);
+    if (argc > 1) {
+        bt = btree_open(argv[1], O_RDONLY);
+        assert(bt);
 
-    bt = btree_open(argv[1], O_RDONLY);
-    assert(bt);
+        btree_dump(stdout, bt);
 
-    btree_dump(stdout, bt);
-
-    btree_close(&bt);
-
+        btree_close(&bt);
+    }
     return 0;
 }
