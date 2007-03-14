1112d6fcfad9a0faff166c08d860cf455f9df7fb
Jean-Marc Coïc <jean-marc.coic@intersec.com>
2007-03-14 09:38:22 +0000

    Added str_explode to explode a string into a string_array

--- lib-common/str_array.c
+++ lib-common/str_array.c
@@ -24,3 +24,94 @@ void string_array_dump(const string_array *xp)
         sep = " ";
     }
 }
+
+string_array *str_explode(const char *s, const char *tokens)
+{
+    const char *p;
+    string_array *res;
+    char sep;
+
+    if (!s || !tokens || !*s) {
+        return NULL;
+    }
+
+    res = string_array_new();
+    p = strpbrk(s, tokens);
+
+    if (!p) {
+        string_array_append(res, p_strdup(s));
+    }
+    sep = *p;
+    string_array_append(res, p_dupstr(s, p - s));
+    s = p + 1;
+
+    while ((p = strchr(s, sep)) != NULL) {
+        string_array_append(res, p_dupstr(s, p - s));
+        s = p + 1;
+    }
+
+    /* Last part */
+    if (*s) {
+        string_array_append(res, p_strdup(s));
+    }
+
+    return res;
+}
+
+
+/*[ CHECK ]::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::{{{*/
+#ifdef CHECK
+/* {{{*/
+#include <check.h>
+
+START_TEST(check_str_explode)
+{
+    string_array *arr;
+
+#define STR_EXPL_CORRECT_TEST(str1, str2, str3, sep)                       \
+    arr = str_explode(str1 sep str2 sep str3, sep);                        \
+                                                                           \
+    fail_if(arr == NULL, "str_explode failed, res == NULL");               \
+    fail_if(arr->len != 3, "str_explode failed: len = %d != 3", arr->len); \
+    fail_if(strcmp(arr->tab[0], str1),                                     \
+            "str_explode failed: tab[0] (%s) != %s",                       \
+            arr->tab[0], str1);                                            \
+    fail_if(strcmp(arr->tab[1], str2),                                     \
+            "str_explode failed: tab[1] (%s) != %s",                       \
+            arr->tab[1], str2);                                            \
+    fail_if(strcmp(arr->tab[2], str3),                                     \
+            "str_explode failed: tab[2] (%s) != %s",                       \
+            arr->tab[2], str3);                                            \
+                                                                           \
+    string_array_delete(&arr, true);
+    STR_EXPL_CORRECT_TEST("123", "abc", "!%*", "/");
+    STR_EXPL_CORRECT_TEST("123", "abc", "!%*", " ");
+    STR_EXPL_CORRECT_TEST("123", "abc", "!%*", "$");
+    STR_EXPL_CORRECT_TEST("   ", ":::", "!!!", ",");
+
+    arr = str_explode("secret1 secret2 secret3", " ,;");
+    fail_if(arr == NULL, "str_explode failed, res == NULL");
+    fail_if(arr->len != 3, "str_explode failed: len = %d != 3", arr->len);
+    string_array_delete(&arr, true);
+    arr = str_explode("secret1;secret2;secret3", " ,;");
+    fail_if(arr == NULL, "str_explode failed, res == NULL");
+    fail_if(arr->len != 3, "str_explode failed: len = %d != 3", arr->len);
+    string_array_delete(&arr, true);
+    arr = str_explode("secret1,secret2 secret3", " ,;");
+    fail_if(arr == NULL, "str_explode failed, res == NULL");
+    fail_if(arr->len != 2, "str_explode failed: len = %d != 2", arr->len);
+    string_array_delete(&arr, true);
+}
+END_TEST
+
+Suite *check_str_array_suite(void)
+{
+    Suite *s  = suite_create("str_array");
+    TCase *tc = tcase_create("Core");
+
+    suite_add_tcase(s, tc);
+    tcase_add_test(tc, check_str_explode);
+    return s;
+}
+#endif
+
