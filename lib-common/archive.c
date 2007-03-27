d7902fcfc61b9a5a713725b844aae93a2a04ac19
ogu <ogu@intersec.com>
2007-03-27 16:55:48 +0000

    language

--- lib-common/archive.c
+++ lib-common/archive.c
@@ -427,7 +427,7 @@ const archive_file *archive_get_file_bloc(const archive_t *archive,
     archive_file *file;
     int i;
 
-    /* XXX: Should use a cached hash as file->name never change */
+    /* XXX: Should use a cached hash as file->name fields never change */
     for (i = 0; i < archive->nb_blocs; i++) {
         bloc = archive->blocs[i];
         if (bloc->tag == ARCHIVE_TAG_FILE) {
