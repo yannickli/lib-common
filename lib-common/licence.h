65543d83457c9d189f47fd095ef18ff5271f79ac
Pierre Habouzit <pierre.habouzit@intersec.com>
2007-04-02 17:43:29 +0000

    use list_my_cpus to generate intersecid
    Signed-off-by: pierre habouzit <pierre.habouzit@intersec.eu>

--- lib-common/licence.h
+++ lib-common/licence.h
@@ -20,6 +20,7 @@
 
 bool is_my_mac_addr(const char *addr);
 int list_my_macs(char *dst, size_t size);
+int list_my_cpus(char *dst, size_t size);
 int read_cpu_signature(uint32_t *dst);
 int licence_compute_conf_signature(const conf_t *conf, char *dst, size_t size);
 int licence_check_signature_ok(const conf_t *conf);
