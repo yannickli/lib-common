572504c986630338a5ac36d2c95d84f6b7adfd6e
ogu <ogu@intersec.com>
2007-07-28 01:52:17 +0200

    cosmetics

--- lib-common/property.c
+++ lib-common/property.c
@@ -223,6 +223,7 @@ void props_to_fmtv1(blob_t *out, property_array *props)
 void property_array_dup(const property_array *from, property_array *to)
 {
     int i;
+
     for (i = 0; i < from->len; i++) {
         property_t *prop = property_new();
 
