7beb7e848cdac546775b822e53994b1c1c2d0b68
Flavien Lebarbé <flavien.lebarbe@intersec.com>
2007-07-31 18:39:25 +0200

    MINGCC compilation fixes. Added pthread.h, use empty macros in threading.h for win targets.

--- compat/pthread.h
+++ compat/pthread.h
@@ -11,29 +11,13 @@
 /*                                                                        */
 /**************************************************************************/
 
-#ifndef IS_LIB_COMMON_THREADING_H
-#define IS_LIB_COMMON_THREADING_H
+#ifndef IS_COMPAT_PTHREAD_H
+#define IS_COMPAT_PTHREAD_H
 
-#include <assert.h>
-#include <pthread.h>
 
-#if !defined(NDEBUG) && !defined(__MINGW) && !defined(__MINGW32__)
-/* declare an anchor for the module/thing */
-#define THREAD_ANCHOR(name)  pthread_t name
-#define STATIC_THREAD_ANCHOR(name)  static THREAD_ANCHOR(name)
-
-/* tie the anchor to a given thread */
-#define THREAD_TIE(name)     (assert (!name), name = pthread_self())
-/* untie the anchor */
-#define THREAD_UNTIE(name)   (name = 0)
-/* use that in the begining of each function to tie to the anchor */
-#define THREAD_ASSERT(name)  (assert (name == pthread_self()))
-#else
-#define THREAD_ANCHOR(name)
-#define STATIC_THREAD_ANCHOR(name)
-#define THREAD_TIE(name)
-#define THREAD_UNTIE(name)
-#define THREAD_ASSERT(name)
+#if !defined(__MINGW) && !defined(__MINGW32__)
+#include_next <pthread.h>
 #endif
 
-#endif /* IS_LIB_COMMON_THREADING_H */
+#endif /* !IS_COMPAT_PTHREAD_H */
+
