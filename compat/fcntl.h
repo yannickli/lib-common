fbec451f0374c15a05fa31227ac00a770cd6c032
ogu <ogu@intersec.com>
2007-09-13 14:23:47 +0200

    cosmetics: untabified

--- compat/fcntl.h
+++ compat/fcntl.h
@@ -21,22 +21,22 @@
 #if defined(__MINGW) || defined(__MINGW32__)
 
 #ifndef __USE_FILE_OFFSET64
-# define F_GETLK	5	/* Get record locking info.  */
-# define F_SETLK	6	/* Set record locking info (non-blocking).  */
-# define F_SETLKW	7	/* Set record locking info (blocking).	*/
+# define F_GETLK        5       /* Get record locking info.  */
+# define F_SETLK        6       /* Set record locking info (non-blocking).  */
+# define F_SETLKW       7       /* Set record locking info (blocking).  */
 #else
-# define F_GETLK	F_GETLK64  /* Get record locking info.	*/
-# define F_SETLK	F_SETLK64  /* Set record locking info (non-blocking).*/
-# define F_SETLKW	F_SETLKW64 /* Set record locking info (blocking).  */
+# define F_GETLK        F_GETLK64  /* Get record locking info.  */
+# define F_SETLK        F_SETLK64  /* Set record locking info (non-blocking).*/
+# define F_SETLKW       F_SETLKW64 /* Set record locking info (blocking).  */
 #endif
-#define F_GETLK64	12	/* Get record locking info.  */
-#define F_SETLK64	13	/* Set record locking info (non-blocking).  */
-#define F_SETLKW64	14	/* Set record locking info (blocking).	*/
+#define F_GETLK64       12      /* Get record locking info.  */
+#define F_SETLK64       13      /* Set record locking info (non-blocking).  */
+#define F_SETLKW64      14      /* Set record locking info (blocking).  */
 
 /* For posix fcntl() and `l_type' field of a `struct flock' for lockf().  */
-#define F_RDLCK		0	/* Read lock.  */
-#define F_WRLCK		1	/* Write lock.	*/
-#define F_UNLCK		2	/* Remove lock.	 */
+#define F_RDLCK         0       /* Read lock.  */
+#define F_WRLCK         1       /* Write lock.  */
+#define F_UNLCK         2       /* Remove lock.  */
 
 /* NOTE: These declarations also appear in <unistd.h>; be sure to keep both
    files consistent.  Some systems have them there and some here, and some
@@ -46,33 +46,33 @@
    LEN is always relative to the current file position.
    The CMD argument is one of the following.  */
 
-# define F_ULOCK 0	/* Unlock a previously locked region.  */
-# define F_LOCK  1	/* Lock a region for exclusive use.  */
-# define F_TLOCK 2	/* Test and lock a region for exclusive use.  */
-# define F_TEST  3	/* Test a region for other processes locks.  */
+# define F_ULOCK 0      /* Unlock a previously locked region.  */
+# define F_LOCK  1      /* Lock a region for exclusive use.  */
+# define F_TLOCK 2      /* Test and lock a region for exclusive use.  */
+# define F_TEST  3      /* Test a region for other processes locks.  */
 
 struct flock
   {
-    short int l_type;	/* Type of lock: F_RDLCK, F_WRLCK, or F_UNLCK.	*/
-    short int l_whence;	/* Where `l_start' is relative to (like `lseek').  */
+    short int l_type;   /* Type of lock: F_RDLCK, F_WRLCK, or F_UNLCK.  */
+    short int l_whence; /* Where `l_start' is relative to (like `lseek').  */
 #ifndef __USE_FILE_OFFSET64
-    __off_t l_start;	/* Offset where the lock begins.  */
-    __off_t l_len;	/* Size of the locked area; zero means until EOF.  */
+    __off_t l_start;    /* Offset where the lock begins.  */
+    __off_t l_len;      /* Size of the locked area; zero means until EOF.  */
 #else
-    __off64_t l_start;	/* Offset where the lock begins.  */
-    __off64_t l_len;	/* Size of the locked area; zero means until EOF.  */
+    __off64_t l_start;  /* Offset where the lock begins.  */
+    __off64_t l_len;    /* Size of the locked area; zero means until EOF.  */
 #endif
-    __pid_t l_pid;	/* Process holding the lock.  */
+    __pid_t l_pid;      /* Process holding the lock.  */
   };
 
 #ifdef __USE_LARGEFILE64
 struct flock64
   {
-    short int l_type;	/* Type of lock: F_RDLCK, F_WRLCK, or F_UNLCK.	*/
-    short int l_whence;	/* Where `l_start' is relative to (like `lseek').  */
-    __off64_t l_start;	/* Offset where the lock begins.  */
-    __off64_t l_len;	/* Size of the locked area; zero means until EOF.  */
-    __pid_t l_pid;	/* Process holding the lock.  */
+    short int l_type;   /* Type of lock: F_RDLCK, F_WRLCK, or F_UNLCK.  */
+    short int l_whence; /* Where `l_start' is relative to (like `lseek').  */
+    __off64_t l_start;  /* Offset where the lock begins.  */
+    __off64_t l_len;    /* Size of the locked area; zero means until EOF.  */
+    __pid_t l_pid;      /* Process holding the lock.  */
   };
 #endif
 
