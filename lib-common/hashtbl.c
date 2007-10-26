cc8837ac4a31a7a2147adba67b84dbca8dabb2b3
Pierre Habouzit <pierre.habouzit@intersec.com>
2007-10-26 17:48:35 +0200

    Fixes, comments.
    Signed-off-by: Pierre Habouzit <pierre.habouzit@intersec.com>

--- lib-common/hashtbl.c
+++ lib-common/hashtbl.c
@@ -15,9 +15,12 @@
 #include "hashtbl.h"
 
 typedef struct hashtbl_entry {
-    uint64_t h;
+    uint64_t key;
     void *ptr;
 } hashtbl_entry;
+
+#define IS_EMPTY(ptr)  ((uintptr_t)(ptr) <= 1)
+#define INVALIDATE(t, ptr) ((t)->nr--, ptr = (void *)1)
 
 void hashtbl_wipe(hashtbl_t *t)
 {
@@ -34,29 +37,29 @@ static void hashtbl_resize(hashtbl_t *t, size_t newsize)
     t->nr   = 0;
 
     for (ssize_t i = 0; i < oldsize; i++) {
-        if ((uintmax_t)oldtab[i].ptr > 1)
-            hashtbl_insert(t, oldtab[i].h, oldtab[i].ptr);
+        if (!IS_EMPTY(oldtab[i].ptr))
+            hashtbl_insert(t, oldtab[i].key, oldtab[i].ptr);
     }
     p_delete(&oldtab);
 }
 
-void **hashtbl_find(const hashtbl_t *t, uint64_t h)
+void **hashtbl_find(const hashtbl_t *t, uint64_t key)
 {
-    size_t size = t->size;
-    size_t pos  = h % size;
+    size_t size = (size_t)t->size;
+    size_t pos  = key % size;
     hashtbl_entry *tab = t->tab;
 
     if (!t->tab)
         return NULL;
 
-    while (tab[pos].ptr && tab[pos].h != h) {
+    while (tab[pos].ptr && tab[pos].key != key) {
         if (++pos == size)
-            pos = size;
+            pos = 0;
     }
-    return (uintmax_t)tab[pos].ptr > 1 ? &tab[pos].ptr : NULL;
+    return IS_EMPTY(tab[pos].ptr) ? NULL : &tab[pos].ptr;
 }
 
-void **hashtbl_insert(hashtbl_t *t, uint64_t h, void *ptr)
+void **hashtbl_insert(hashtbl_t *t, uint64_t key, void *ptr)
 {
     size_t size, pos;
     hashtbl_entry *tab;
@@ -64,18 +67,18 @@ void **hashtbl_insert(hashtbl_t *t, uint64_t h, void *ptr)
     if (t->nr >= t->size / 2)
         hashtbl_resize(t, p_alloc_nr(t->size));
 
-    size = t->size;
-    pos  = h % size;
+    size = (size_t)t->size;
+    pos  = key % size;
     tab  = t->tab;
 
-    while ((uintmax_t)tab[pos].ptr > 1 && tab[pos].h != h) {
+    while (!IS_EMPTY(tab[pos].ptr) && tab[pos].key != key) {
         if (++pos == size)
-            pos = size;
+            pos = 0;
     }
-    if ((uintmax_t)tab[pos].ptr <= 1) {
+    if (IS_EMPTY(tab[pos].ptr)) {
         t->nr++;
         tab[pos].ptr = ptr;
-        tab[pos].h   = h;
+        tab[pos].key = key;
         return NULL;
     }
     return &tab[pos].ptr;
@@ -83,12 +86,11 @@ void **hashtbl_insert(hashtbl_t *t, uint64_t h, void *ptr)
 
 void hashtbl_remove(hashtbl_t *t, void **pp)
 {
-    assert ((uintmax_t)*pp >= 1);
+    assert (!IS_EMPTY(*pp));
     assert ((char*)*pp >= (char*)t->tab);
     assert ((char *)*pp < (char *)(t->tab + t->size));
 
-    *pp = (void *)1;
-    t->nr--;
+    INVALIDATE(t, *pp);
     if (8 * (t->nr + 16) < t->size)
         hashtbl_resize(t, 4 * (t->nr + 16));
 }
@@ -99,14 +101,13 @@ void hashtbl_map(hashtbl_t *t, void (*fn)(void **, void *), void *priv)
     hashtbl_entry *end = t->tab + t->size;
 
     while (e < end) {
-        if ((uintmax_t)e->ptr > 1) {
+        if (!IS_EMPTY(e->ptr)) {
             (*fn)(&e->ptr, priv);
             if (!e->ptr) { /* means removal */
-                e->ptr = (void *)1;
-                t->nr--;
+                INVALIDATE(t, e->ptr);
             }
         }
     }
-    if (8 * (t->nr + 16) < t->size)
-        hashtbl_resize(t, 4 * (t->nr + 16));
+    if (4 * p_alloc_nr(t->nr) < t->size)
+        hashtbl_resize(t, 2 * p_alloc_nr(t->nr));
 }
--- lib-common/hashtbl.h
+++ lib-common/hashtbl.h
@@ -16,6 +16,11 @@
 
 #include "mem.h"
 
+/*
+ * Hash tables from that module don't deal with collitions,
+ * though wrappers to deal with it using SLISTS are trivial
+ */
+
 typedef struct hashtbl_t {
     ssize_t nr, size;
     struct hashtbl_entry *tab;
@@ -24,8 +29,8 @@ typedef struct hashtbl_t {
 GENERIC_INIT(hashtbl_t, hashtbl);
 void hashtbl_wipe(hashtbl_t *t);
 
-void **hashtbl_find(const hashtbl_t *t, uint64_t h);
-void **hashtbl_insert(hashtbl_t *t, uint64_t h, void *);
+void **hashtbl_find(const hashtbl_t *t, uint64_t key);
+void **hashtbl_insert(hashtbl_t *t, uint64_t key, void *);
 void hashtbl_remove(hashtbl_t *t, void **);
 void hashtbl_map(hashtbl_t *t, void (*fn)(void **, void *), void *);
 
@@ -40,12 +45,12 @@ void hashtbl_map(hashtbl_t *t, void (*fn)(void **, void *), void *);
         hashtbl_wipe((hashtbl_t *)t);                                        \
     }                                                                        \
     \
-    static inline type **pfx##_hash_find(pfx##_hash *t, uint64_t h) {        \
-        return (type **)hashtbl_find((hashtbl_t *)t, h);                     \
+    static inline type **pfx##_hash_find(pfx##_hash *t, uint64_t key) {      \
+        return (type **)hashtbl_find((hashtbl_t *)t, key);                   \
     }                                                                        \
     static inline type **                                                    \
-    pfx##_hash_insert(pfx##_hash *t, uint64_t h, type *e) {                  \
-        return (type **)hashtbl_insert((hashtbl_t *)t, h, e);                \
+    pfx##_hash_insert(pfx##_hash *t, uint64_t key, type *e) {                \
+        return (type **)hashtbl_insert((hashtbl_t *)t, key, e);              \
     }                                                                        \
     \
     static inline void pfx##_hash_remove(pfx##_hash *t, type **e) {          \
