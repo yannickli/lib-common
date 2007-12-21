a418e9ca7c0c078814e6a8cca3d0d99bdf43fab8
Pierre Habouzit <pierre.habouzit@intersec.com>
2007-12-21 10:56:32 +0100

    Have a netinet/in.h compat wrapper that #include winsock2.h for IPPROTO_TCP.
      This removes the dependency on lib-network in the lib-inet
    
    Signed-off-by: Pierre Habouzit <pierre.habouzit@intersec.com>

---
+++ compat/netinet/in.h
@@ -0,0 +1,23 @@
+/**************************************************************************/
+/*                                                                        */
+/*  Copyright (C) 2004-2007 INTERSEC SAS                                  */
+/*                                                                        */
+/*  Should you receive a copy of this source code, you must check you     */
+/*  have a proper, written authorization of INTERSEC to hold it. If you   */
+/*  don't have such an authorization, you must DELETE all source code     */
+/*  files in your possession, and inform INTERSEC of the fact you obtain  */
+/*  these files. Should you not comply to these terms, you can be         */
+/*  prosecuted in the extent permitted by applicable law.                 */
+/*                                                                        */
+/**************************************************************************/
+
+#ifndef IS_COMPAT_NETINET_IN_H
+#define IS_COMPAT_NETINET_IN_H
+
+#if defined(__MINGW) || defined(__MINGW32__)
+#include <winsock2.h>
+#else
+#include_next <netinet/in.h>
+#endif
+
+#endif /* !IS_COMPAT_NETINET_IN_H */
