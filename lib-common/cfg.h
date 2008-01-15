614282a3f87823bc95d56e4d2c4daed45a5118e6
Pierre Habouzit <pierre.habouzit@intersec.com>
2008-01-15 16:21:04 +0100

    Initial more easily extensible configuration parser.
    Signed-off-by: Pierre Habouzit <pierre.habouzit@intersec.com>

---
+++ lib-common/cfg.h
@@ -0,0 +1,22 @@
+/**************************************************************************/
+/*                                                                        */
+/*  Copyright (C) 2004-2008 INTERSEC SAS                                  */
+/*                                                                        */
+/*  Should you receive a copy of this source code, you must check you     */
+/*  have a proper, written authorization of INTERSEC to hold it. If you   */
+/*  don't have such an authorization, you must DELETE all source code     */
+/*  files in your possession, and inform INTERSEC of the fact you obtain  */
+/*  these files. Should you not comply to these terms, you can be         */
+/*  prosecuted in the extent permitted by applicable law.                 */
+/*                                                                        */
+/**************************************************************************/
+
+#ifndef IS_LIB_COMMON_CFG_H
+#define IS_LIB_COMMON_CFG_H
+
+#include "blob.h"
+#include "string_is.h"
+
+int cfg_parse(const char *file);
+
+#endif /* IS_LIB_COMMON_CFG_H */
---
+++ lib-common/tst-cfgparser.c
@@ -0,0 +1,22 @@
+/**************************************************************************/
+/*                                                                        */
+/*  Copyright (C) 2004-2008 INTERSEC SAS                                  */
+/*                                                                        */
+/*  Should you receive a copy of this source code, you must check you     */
+/*  have a proper, written authorization of INTERSEC to hold it. If you   */
+/*  don't have such an authorization, you must DELETE all source code     */
+/*  files in your possession, and inform INTERSEC of the fact you obtain  */
+/*  these files. Should you not comply to these terms, you can be         */
+/*  prosecuted in the extent permitted by applicable law.                 */
+/*                                                                        */
+/**************************************************************************/
+
+int cfg_parse(const char *file);
+
+int main(int argc, const char **argv)
+{
+    for (argc--, argv++; argc > 0; argc--, argv++) {
+        cfg_parse(*argv);
+    }
+    return 0;
+}
