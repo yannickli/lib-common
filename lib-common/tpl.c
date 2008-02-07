8818c6b77fe6c718505181b0bf413fb7d70ef700
Pierre Habouzit <pierre.habouzit@intersec.com>
2008-02-07 18:41:37 +0100

    Content templates second gen.
    Signed-off-by: Pierre Habouzit <pierre.habouzit@intersec.com>

---
+++ lib-common/tpl.c
@@ -0,0 +1,276 @@
+/**************************************************************************/
+/*                                                                        */
+/*  Copyright (C) 2004-2008 INTERSEC SAS                                  */
+/*                                                                        */
+/*  Should you receive a copy of this source code, you must check you     */
+/*  have a proper, written authorization of INTERSEC to hold it. If you   */
+/*  don't have such an authorization, you must DELETE all source code     */
+/*  files in your possession, and inform INTERSEC of the fact you obtain  */
+/*  these files. Should you not comply to these terms, you can be         */
+/*  prosecuted in the extent permitted by applicable law.                 */
+/*                                                                        */
+/**************************************************************************/
+
+#include "string_is.h"
+#include "tpl.h"
+
+static tpl_t *tpl_new_op(tpl_op op)
+{
+    tpl_t *n = p_new(tpl_t, 1);
+    n->refcnt = 1;
+    switch (op) {
+      case TPL_OP_BLOB:
+        blob_init(&n->u.blob);
+        break;
+      default:
+        break;
+    }
+    tpl_array_init(&n->blocks);
+    return n;
+}
+tpl_t *tpl_new(void)
+{
+    return tpl_new_op(TPL_OP_BLOCK);
+}
+tpl_t *tpl_dup(const tpl_t *tpl)
+{
+    tpl_t *res = (tpl_t *)tpl;
+    res->refcnt++;
+    return res;
+}
+static void tpl_wipe(tpl_t *n)
+{
+    switch (n->op) {
+      case TPL_OP_DATA:
+        p_delete(&n->u.data.iov);
+        break;
+      case TPL_OP_BLOB:
+        blob_wipe(&n->u.blob);
+        break;
+      default:
+        break;
+    }
+    tpl_array_wipe(&n->blocks);
+}
+void tpl_delete(tpl_t **tpl)
+{
+    if (*tpl) {
+        if (--(*tpl)->refcnt > 0) {
+            *tpl = NULL;
+        } else {
+            tpl_wipe(*tpl);
+            p_delete(tpl);
+        }
+    }
+}
+
+/****************************************************************************/
+/* Build the AST                                                            */
+/****************************************************************************/
+
+#define tpl_can_append(t)  \
+        (  ((t)->op & TPL_OP_BLOCK)                                     \
+        && ((t)->op != TPL_OP_IFDEF || (t)->blocks.len < 2))
+
+void tpl_add_data(tpl_t *tpl, const byte *data, int len)
+{
+    struct tpl_data *td;
+    tpl_t *buf;
+
+    assert (tpl_can_append(tpl));
+
+    if (!tpl->blocks.len || tpl->blocks.tab[tpl->blocks.len - 1]->op != TPL_OP_DATA) {
+        tpl_array_append(&tpl->blocks, buf = tpl_new_op(TPL_OP_DATA));
+    } else {
+        buf = tpl->blocks.tab[tpl->blocks.len - 1];
+    }
+    td = &buf->u.data;
+    p_allocgrow(&td->iov, td->n + 1, &td->sz);
+    td->iov[td->n++] = (struct iovec){
+        .iov_base = (void *)data,
+        .iov_len  = len,
+    };
+}
+
+void tpl_copy_data(tpl_t *tpl, const byte *data, int len)
+{
+    tpl_t *buf;
+
+    assert (tpl_can_append(tpl));
+
+    if (!tpl->blocks.len || tpl->blocks.tab[tpl->blocks.len - 1]->op != TPL_OP_BLOB) {
+        tpl_array_append(&tpl->blocks, buf = tpl_new_op(TPL_OP_BLOB));
+    } else {
+        buf = tpl->blocks.tab[tpl->blocks.len - 1];
+    }
+    blob_append_data(&buf->u.blob, data, len);
+}
+
+void tpl_add_var(tpl_t *tpl, uint16_t array, uint16_t index)
+{
+    tpl_t *var;
+
+    assert (tpl_can_append(tpl));
+    tpl_array_append(&tpl->blocks, var = tpl_new_op(TPL_OP_VAR));
+    var->u.varidx = ((uint32_t)array << 16) | index;
+}
+
+tpl_t *tpl_add_ifdef(tpl_t *tpl, uint16_t array, uint16_t index)
+{
+    tpl_t *var;
+
+    assert (tpl_can_append(tpl));
+    tpl_array_append(&tpl->blocks, var = tpl_new_op(TPL_OP_IFDEF));
+    var->u.varidx = ((uint32_t)array << 16) | index;
+    return var;
+}
+
+tpl_t *tpl_add_apply(tpl_t *tpl, tpl_op op, tpl_apply_f *f)
+{
+    tpl_t *app;
+
+    assert (tpl_can_append(tpl));
+    tpl_array_append(&tpl->blocks, app = tpl_new_op(op));
+    app->u.f = f;
+    return app;
+}
+
+/****************************************************************************/
+/* Substitution and optimization                                            */
+/****************************************************************************/
+
+enum tplcode {
+    TPL_ERR   = -1,
+    TPL_CONST = 0,
+    TPL_VAR   = 1,
+};
+
+static int tpl_into_iovec(struct tpl_data *td, const tpl_t *tpl)
+{
+    for (int i = 0; i < tpl->blocks.len; i++) {
+        tpl_t *tmp = tpl->blocks.tab[i];
+        struct tpl_data *td2 = &tmp->u.data;
+
+        switch (tmp->op) {
+          case TPL_OP_DATA:
+            p_allocgrow(&td->iov, td->n + td2->n, &td->sz);
+            memcpy(td->iov + td->n, td2->iov, td2->n * sizeof(struct iovec));
+            td->n += td2->n;
+            break;
+
+          case TPL_OP_BLOB:
+            p_allocgrow(&td->iov, td->n + 1, &td->sz);
+            td->iov[td->n++] = (struct iovec){
+                .iov_base = (void *)tmp->u.blob.data,
+                .iov_len  = tmp->u.blob.len,
+            };
+            break;
+
+          case TPL_OP_BLOCK:
+            if (tpl_into_iovec(td, tmp) < 0)
+                return -1;
+            break;
+
+          default:
+            return -1;
+        }
+    }
+    return 0;
+}
+
+static enum tplcode tpl_combine(tpl_t *out, const tpl_t *orig, uint16_t envid,
+                                const tpl_t **vals, int nb)
+{
+    enum tplcode res = TPL_CONST;
+    tpl_t *tmp;
+
+    for (int i = 0; i < orig->blocks.len; i++) {
+        const tpl_t *tpl = orig->blocks.tab[i];
+
+      again:
+        switch (tpl->op) {
+          case TPL_OP_DATA:
+          case TPL_OP_BLOB:
+            tpl_array_append(&out->blocks, tpl_dup(tpl));
+            break;
+
+          case TPL_OP_VAR:
+            if (tpl->u.varidx >> 16 != envid) {
+                tpl_array_append(&out->blocks, tpl_dup(tpl));
+                res |= TPL_VAR;
+            } else {
+                if ((tpl->u.varidx & 0xffff) >= (uint16_t)nb)
+                    return TPL_ERR;
+                res |= tpl_combine(out, vals[tpl->u.varidx & 0xffff],
+                                   envid, vals, nb);
+            }
+            break;
+
+          case TPL_OP_BLOCK:
+            res |= tpl_combine(out, tpl, envid, vals, nb);
+            break;
+
+          case TPL_OP_IFDEF:
+            if (tpl->u.varidx >> 16 != envid) {
+                tpl_array_append(&out->blocks, tpl_dup(tpl));
+                res |= TPL_VAR;
+            } else {
+                if ((tpl->u.varidx & 0xffff) >= (uint16_t)nb || !vals[tpl->u.varidx]) {
+                    if (tpl->blocks.len > 1) {
+                        tpl = tpl->blocks.tab[1];
+                        goto again;
+                    }
+                } else {
+                    if (tpl->blocks.len > 0) {
+                        tpl = tpl->blocks.tab[0];
+                        goto again;
+                    }
+                }
+            }
+            break;
+
+          case TPL_OP_APPLY:
+            tpl_array_append(&out->blocks, tmp = tpl_new_op(TPL_OP_APPLY));
+            tmp->u.f = tpl->u.f;
+            res |= TPL_VAR | tpl_combine(tmp, tpl, envid, vals, nb);
+            break;
+
+          case TPL_OP_APPLY_PURE:
+          case TPL_OP_APPLY_PURE_ASSOC:
+            res |= tpl_combine(tmp = tpl_new(), tpl, envid, vals, nb);
+            if (res == TPL_CONST) {
+                struct tpl_data td = { .iov = NULL };
+
+                tmp->op = TPL_OP_BLOB;
+                blob_init(&tmp->u.blob);
+                tpl_into_iovec(&td, tpl);
+
+                if ((*tpl->u.f)(&tmp->u.blob, &td) < 0) {
+                    res = TPL_ERR;
+                }
+                tpl_array_wipe(&tmp->blocks);
+                tpl_array_init(&tmp->blocks);
+                p_delete(&td.iov);
+            } else {
+                tmp->op  = tpl->op;
+                tmp->u.f = tpl->u.f;
+            }
+            tpl_array_append(&out->blocks, tmp);
+            break;
+        }
+
+        if (res < 0)
+            return res;
+    }
+
+    return res;
+}
+
+tpl_t *tpl_subst(const tpl_t *tpl, uint16_t envid, const tpl_t **vals, int nb)
+{
+    tpl_t *res = tpl_new();
+    if (tpl_combine(res, tpl, envid, vals, nb) < 0) {
+        tpl_delete(&res);
+    }
+    return res;
+}
---
+++ lib-common/tpl.h
@@ -0,0 +1,74 @@
+/**************************************************************************/
+/*                                                                        */
+/*  Copyright (C) 2004-2008 INTERSEC SAS                                  */
+/*                                                                        */
+/*  Should you receive a copy of this source code, you must check you     */
+/*  have a proper, written authorization of INTERSEC to hold it. If you   */
+/*  don't have such an authorization, you must DELETE all source code     */
+/*  files in your possession, and inform INTERSEC of the fact you obtain  */
+/*  these files. Should you not comply to these terms, you can be         */
+/*  prosecuted in the extent permitted by applicable law.                 */
+/*                                                                        */
+/**************************************************************************/
+
+#ifndef IS_LIB_COMMON_TPL_H
+#define IS_LIB_COMMON_TPL_H
+
+#include "array.h"
+#include "blob.h"
+#include "ioveclist.h"
+
+typedef enum tpl_op {
+    TPL_OP_DATA = 0x00,
+    TPL_OP_BLOB,
+    TPL_OP_VAR,
+
+    TPL_OP_BLOCK = 0x10,
+    TPL_OP_IFDEF,
+    TPL_OP_APPLY,
+    TPL_OP_APPLY_PURE,
+    TPL_OP_APPLY_PURE_ASSOC,
+} tpl_op;
+
+struct tpl_data {
+    struct iovec *iov;
+    int n, sz;
+};
+
+typedef int (tpl_apply_f)(blob_t *, struct tpl_data *);
+
+ARRAY_TYPE(union tpl_t, tpl);
+typedef union tpl_t {
+    int refcnt;
+    tpl_op op;
+    union {
+        struct tpl_data data;
+        blob_t blob;
+        uint32_t varidx; /* 16 bits of env, 16 bits of index */
+        tpl_apply_f *f;
+    } u;
+    tpl_array blocks;
+} tpl_t;
+
+tpl_t *tpl_new(void);
+tpl_t *tpl_dup(const tpl_t *);
+void tpl_delete(tpl_t **);
+ARRAY_FUNCTIONS(tpl_t, tpl, tpl_delete);
+
+/****************************************************************************/
+/* Build the AST                                                            */
+/****************************************************************************/
+
+void tpl_add_data(tpl_t *tpl, const byte *data, int len);
+void tpl_copy_data(tpl_t *tpl, const byte *data, int len);
+void tpl_add_var(tpl_t *tpl, uint16_t array, uint16_t index);
+tpl_t *tpl_add_ifdef(tpl_t *tpl, uint16_t array, uint16_t index);
+tpl_t *tpl_add_apply(tpl_t *tpl, tpl_op op, tpl_apply_f *f);
+
+/****************************************************************************/
+/* Substitution and optimization                                            */
+/****************************************************************************/
+
+tpl_t *tpl_subst(const tpl_t *, uint16_t envid, const tpl_t **, int nb);
+
+#endif
