db6de2a593e5132b5ee279180a5869bfa4660a59
Pierre Habouzit <madcoder@debian.org>
2008-07-11 15:29:53 +0200

    missing file
    Signed-off-by: Pierre Habouzit <madcoder@debian.org>

---
+++ compat/syslog.h
@@ -0,0 +1,30 @@
+/**************************************************************************/
+/*                                                                        */
+/*  Copyright (C) 2004-2008 INTERSEC SAS                                  */
+/*                                                                        */
+/*  Should you receive a copy of this source code, you must check you     */
+/*  have a proper, written authorization of INTERSEC to hold it. If you   */
+/*  don't have such an authorization, you must DELETE all source code     */
+/*  files in your possession, and inform INTERSEC of the fact you obtain  */
+/*  these files. Should you not comply to these terms, you can be         */
+/*  prosecuted in the extent permitted by applicable law.                 */
+/*                                                                        */
+/**************************************************************************/
+
+#ifndef IS_COMPAT_SYSLOG_H
+#define IS_COMPAT_SYSLOG_H
+
+#ifndef MINGCC
+#include_next <syslog.h>
+#else
+#define LOG_EMERG       0       /* system is unusable */
+#define LOG_ALERT       1       /* action must be taken immediately */
+#define LOG_CRIT        2       /* critical conditions */
+#define LOG_ERR         3       /* error conditions */
+#define LOG_WARNING     4       /* warning conditions */
+#define LOG_NOTICE      5       /* normal but significant condition */
+#define LOG_INFO        6       /* informational */
+#define LOG_DEBUG       7       /* debug-level messages */
+#endif
+
+#endif
