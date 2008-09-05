510cca3c1f98e4af12ad2502169e1cb3a2f7949c
Clex <clex@intersec.com>
2008-09-05 14:01:05 +0200

    fix padlock build
    Signed-off-by: Clex <clex@intersec.com>

--- hash-padlock.c
+++ hash-padlock.c
@@ -44,14 +44,14 @@
 /*
  * PadLock detection routine
  */
-int padlock_supports( int feature )
+int padlock_supports(int feature)
 {
     static int flags = -1;
     int ebx, edx;
 
-    if( flags == -1 )
+    if (flags == -1)
     {
-        asm( "movl  %%ebx, %0           \n"     \
+        asm("movl  %%ebx, %0           \n"     \
              "movl  $0xC0000000, %%eax  \n"     \
              "cpuid                     \n"     \
              "cmpl  $0xC0000001, %%eax  \n"     \
@@ -64,21 +64,19 @@ int padlock_supports( int feature )
              "movl  %2, %%ebx           \n"
              : "=m" (ebx), "=m" (edx)
              :  "m" (ebx)
-             : "eax", "ecx", "edx" );
+             : "eax", "ecx", "edx");
 
         flags = edx;
     }
 
-    return( flags & feature );
+    return flags & feature;
 }
 
 /*
  * PadLock AES-ECB block en(de)cryption
  */
-int padlock_xcryptecb( aes_ctx *ctx,
-                       int mode,
-                       byte input[16],
-                       byte output[16] )
+int padlock_xcryptecb(aes_ctx *ctx, int mode, const byte input[16],
+                      byte output[16])
 {
     int ebx;
     uint32_t *rk;
@@ -87,13 +85,13 @@ int padlock_xcryptecb( aes_ctx *ctx,
     byte buf[256];
 
     rk  = ctx->rk;
-    blk = PADLOCK_ALIGN16( buf );
-    memcpy( blk, input, 16 );
+    blk = PADLOCK_ALIGN16(buf);
+    memcpy(blk, input, 16);
 
      ctrl = blk + 4;
-    *ctrl = 0x80 | ctx->nr | ( ( ctx->nr + ( mode^1 ) - 10 ) << 9 );
+    *ctrl = 0x80 | ctx->nr | ((ctx->nr + (mode^1) - 10) << 9);
 
-    asm( "pushfl; popfl         \n"     \
+    asm("pushfl; popfl         \n"     \
          "movl    %%ebx, %0     \n"     \
          "movl    $1, %%ecx     \n"     \
          "movl    %2, %%edx     \n"     \
@@ -104,22 +102,18 @@ int padlock_xcryptecb( aes_ctx *ctx,
          "movl    %1, %%ebx     \n"
          : "=m" (ebx)
          :  "m" (ebx), "m" (ctrl), "m" (rk), "m" (blk)
-         : "ecx", "edx", "esi", "edi" );
+         : "ecx", "edx", "esi", "edi");
 
-    memcpy( output, blk, 16 );
+    memcpy(output, blk, 16);
 
-    return( 0 );
+    return 0;
 }
 
 /*
  * PadLock AES-CBC buffer en(de)cryption
  */
-int padlock_xcryptcbc( aes_ctx *ctx,
-                       int mode,
-                       int length,
-                       byte iv[16],
-                       byte *input,
-                       byte *output )
+int padlock_xcryptcbc(aes_ctx *ctx, int mode, int length,
+                      byte iv[16], const byte *input, byte *output)
 {
     int ebx, count;
     uint32_t *rk;
@@ -127,20 +121,20 @@ int padlock_xcryptcbc( aes_ctx *ctx,
     uint32_t *ctrl;
     byte buf[256];
 
-    if( ( (long) input  & 15 ) != 0 ||
-        ( (long) output & 15 ) != 0 )
-        return( 1 );
+    if (((long) input  & 15) != 0 ||
+        ((long) output & 15) != 0)
+        return 1;
 
     rk = ctx->rk;
-    iw = PADLOCK_ALIGN16( buf );
-    memcpy( iw, iv, 16 );
+    iw = PADLOCK_ALIGN16(buf);
+    memcpy(iw, iv, 16);
 
      ctrl = iw + 4;
-    *ctrl = 0x80 | ctx->nr | ( ( ctx->nr + (mode^1) - 10 ) << 9 );
+    *ctrl = 0x80 | ctx->nr | ((ctx->nr + (mode^1) - 10) << 9);
 
     count = (length + 15) >> 4;
 
-    asm( "pushfl; popfl         \n"     \
+    asm("pushfl; popfl         \n"     \
          "movl    %%ebx, %0     \n"     \
          "movl    %2, %%ecx     \n"     \
          "movl    %3, %%edx     \n"     \
@@ -153,11 +147,11 @@ int padlock_xcryptcbc( aes_ctx *ctx,
          : "=m" (ebx)
          :  "m" (ebx), "m" (count), "m" (ctrl),
             "m"  (rk), "m" (input), "m" (output), "m" (iw)
-         : "eax", "ecx", "edx", "esi", "edi" );
+         : "eax", "ecx", "edx", "esi", "edi");
 
-    memcpy( iv, iw, 16 );
+    memcpy(iv, iw, 16);
 
-    return( 0 );
+    return 0;
 }
 
 #endif
