4e297b743effb9cdfe203668fe56229344b5ed60
Pierre Habouzit <pierre.habouzit@intersec.com>
2009-02-24 10:09:30 +0100

    Add new dlist iterators and containers.
    dlist_for_each_{,entry_}continue to start a foreach after a certain point.
    
    Also add dlist_prev_entry, dlist_first_entry and dlist_last_entry.
    
    Signed-off-by: Pierre Habouzit <pierre.habouzit@intersec.com>

--- container-dlist.h
+++ container-dlist.h
@@ -109,28 +109,37 @@ dlist_splice_tail(dlist_t *dst, dlist_t *src)
 #define dlist_entry(ptr, type, member)  container_of(ptr, type, member)
 #define dlist_entry_of(ptr, n, member)  dlist_entry(ptr, typeof(*n), member)
 
-#define dlist_next_entry(e, member) \
-    dlist_entry(e->member.next, typeof(*e), member)
+#define dlist_next_entry(e, mber)  dlist_entry(e->mber.next, typeof(*e), mber)
+#define dlist_prev_entry(e, mber)  dlist_entry(e->mber.prev, typeof(*e), mber)
+#define dlist_first_entry(l, type, mber)  dlist_entry(l->next, type, mber)
+#define dlist_last_entry(l, type, mber)   dlist_entry(l->prev, type, mber)
 
-#define __dlist_for_each(n, head, doit) \
-     for (dlist_t *n = (head)->next; \
+#define __dlist_for_each(pos, n, head, doit) \
+     for (dlist_t *n = (pos)->next; \
           n != (head) && ({ doit; prefetch(n->next); }); n = n->next)
 
-#define dlist_for_each(n, head)   \
-    __dlist_for_each(n, head, )
+#define dlist_for_each(n, head) \
+    __dlist_for_each(head, n, head, )
+#define dlist_for_each_continue(pos, n, head) \
+    __dlist_for_each(pos, n, head, )
+
 #define dlist_for_each_entry(n, head, member) \
-    __dlist_for_each(__real_##n, head, n = dlist_entry_of(__real_##n, n, member))
+    __dlist_for_each(head, __real_##n, head,                         \
+                     n = dlist_entry_of(__real_##n, n, member))
+#define dlist_for_each_entry_continue(pos, n, head, member) \
+    __dlist_for_each(&(pos)->member, __real_##n, head,               \
+                     n = dlist_entry_of(__real_##n, n, member))
 
 
 #define __dlist_for_each_safe(n, __next, head, doit) \
-    for (dlist_t *n = (head)->next, *__next = n->next;      \
-         n != (head) && ({ doit; prefetch(__next->next); });     \
+    for (dlist_t *n = (head)->next, *__next = n->next;               \
+         n != (head) && ({ doit; prefetch(__next->next); });         \
          n = __next, __next = n->next)
 
 #define dlist_for_each_safe(n, head) \
          __dlist_for_each_safe(n, __next_##n, head, )
 
 #define dlist_for_each_entry_safe(n, head, member) \
-    __dlist_for_each_safe(__real_##n, __next_##n, head,         \
+    __dlist_for_each_safe(__real_##n, __next_##n, head,              \
                           n = dlist_entry_of(__real_##n, n, member))
 
