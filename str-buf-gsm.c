9b0d4e98e2f37c94cbcd9f8784750fce77239d23
ogu <ogu@intersec.com>
2009-03-31 15:37:04 +0200

    cosmetics.
    parenthesize macro expansions
    
    Signed-off-by: Pierre Habouzit <pierre.habouzit@intersec.com>

--- str-buf-gsm.c
+++ str-buf-gsm.c
@@ -18,7 +18,7 @@
 /* Convert GSM charset to Unicode */
 static int16_t const __gsm7_to_unicode[] = {
 #define X(x)     (0x##x)
-#define UNK      -1
+#define UNK      (-1)
     /* 0x00,   0x01,   0x02,   0x03,   0x04,   0x05,   0x06,   0x07,    */
        X(40),  X(A3),  X(24),  X(A5),  X(E8),  X(E9),  X(FA),  X(EC),
     /* 0x08,   0x09,   0x0A,   0x0B,   0x0C,   0x0D,   0x0E,   0x0F,    */
@@ -91,7 +91,7 @@ static int16_t const __gsm7_to_unicode[] = {
 
 static int16_t const win1252_to_gsm7[] = {
 #define X(x)  (x)
-#define UNK    -1
+#define UNK   (-1)
 
     /*0x00,   0x01,    0x02,    0x03,    0x04,    0x05,    0x06,    0x07 */
     /*0x00,   0x01,    0x02,    0x03,    0x04,    0x05,    0x06,    0x07 */
