7dd558c7562979d1162cd34e791fe8da0597fde4
Pierre Habouzit <pierre.habouzit@intersec.com>
2009-06-23 12:31:44 +0200

    Useful t_pop_and_* macros.
    Signed-off-by: Pierre Habouzit <pierre.habouzit@intersec.com>

--- core-mem.h
+++ core-mem.h
@@ -450,6 +450,12 @@ mem_pool_t *t_pool(void) __attribute__((pure));
 #define t_pop()       mem_stack_pop(t_pool())
 #define t_rewind(p)   mem_stack_rewind(t_pool(), p)
 
+#define __t_pop_and_do(expr)    ({ t_pop(); expr; })
+#define t_pop_and_return(expr)  t_pop_and_do(return expr)
+#define t_pop_and_break()       t_pop_and_do(break)
+#define t_pop_and_continue()    t_pop_and_do(continue)
+#define t_pop_and_goto(lbl)     t_pop_and_do(goto lbl)
+
 #define t_new(type_t, n) \
     ((type_t *)imalloc((n) * sizeof(type_t), MEM_STACK))
 #define t_new_raw(type_t, n)  \
