c0752d51e8553c55b96f2e435e4877419f1081d8
Pierre Habouzit <pierre.habouzit@intersec.com>
2009-09-23 17:03:18 +0200

    add ps_initsb

--- str-stream.h
+++ str-stream.h
@@ -107,6 +107,9 @@ static inline pstream_t ps_init(const void *s, size_t len) {
 static inline pstream_t ps_initstr(const char *s) {
     return ps_initptr(s, rawmemchr(s, '\0'));
 }
+static inline pstream_t ps_initsb(const sb_t *sb) {
+    return ps_init(sb->data, sb->len);
+}
 
 static inline size_t ps_len(const pstream_t *ps) {
     return ps->s_end - ps->s;
