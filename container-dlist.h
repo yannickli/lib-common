1c75fd231a5e5126e9bde320dc1d1e1004828c9b
Pierre Habouzit <pierre.habouzit@intersec.com>
2009-10-07 17:38:16 +0200

    dlist: add new APIs.
    entries safe access (wrt the dlist_t being NULL):
      dlist_entry_safe, dlist_entry_of_safe.
    
    rev variants for for_each:
      dlist_for_each_rev{,_start,_continue}
      dlist_for_each_rev_safe{,_start,_continue}
    
    start variants for for_each, those variants restart the loop at the "pos"
    argument included. As opposed to "continue" which starts at the next (or
    previous for the rev variants) element of the list:
      dlist_for_each_start
      dlist_for_each_safe_start
      dlist_for_each_entry_start
      dlist_for_each_entry_safe_start
      dlist_for_each_rev_start
      dlist_for_each_rev_safe_start
    
    This can be used to do stuff like:
    
    {
        type_t *e;
    
        dlist_for_each_entry(e, &head, member) {
            if (e->do_something(e) < 0)
               goto undo;
        }
        return 0;
    
        dlist_for_each_rev_start(&e->member, it, &head) {
            e = dlist_entry(it, type_t, member);
            e->undo(e);
        }
        return -1;
    }
    
    Signed-off-by: Pierre Habouzit <pierre.habouzit@intersec.com>
    Cc: dev@intersec.com

--- container-dlist.h
+++ container-dlist.h
@@ -141,6 +141,11 @@ dlist_cut_at(dlist_t *src, dlist_t *e, dlist_t *dst)
 
 #define dlist_entry(ptr, type, member)  container_of(ptr, type, member)
 #define dlist_entry_of(ptr, n, member)  dlist_entry(ptr, typeof(*n), member)
+#define dlist_entry_safe(ptr, type, member) \
+    ({ typeof(ptr) *__ptr = (ptr);          \
+       __ptr ? dlist_entry(__ptr, type, member) : NULL })
+#define dlist_entry_of_safe(ptr, n, member) \
+    dlist_entry_safe(ptr, typeof(*n), member)
 
 #define dlist_next_entry(e, mber)  dlist_entry((e)->mber.next, typeof(*e), mber)
 #define dlist_prev_entry(e, mber)  dlist_entry((e)->mber.prev, typeof(*e), mber)
@@ -152,27 +157,70 @@ dlist_cut_at(dlist_t *src, dlist_t *e, dlist_t *dst)
           n != (head) && ({ doit; prefetch(n->next); }); n = n->next)
 
 #define dlist_for_each(n, head) \
-    __dlist_for_each(head, n, head, )
-#define dlist_for_each_continue(pos, n, head) \
+    __dlist_for_each((head)->next, n, head, )
+#define dlist_for_each_start(pos, n, head) \
     __dlist_for_each(pos, n, head, )
-
-#define dlist_for_each_entry(n, head, member) \
-    __dlist_for_each(head, __real_##n, head,                         \
-                     n = dlist_entry_of(__real_##n, n, member))
-#define dlist_for_each_entry_continue(pos, n, head, member) \
-    __dlist_for_each(&(pos)->member, __real_##n, head,               \
-                     n = dlist_entry_of(__real_##n, n, member))
+#define dlist_for_each_continue(pos, n, head) \
+    __dlist_for_each((pos)->next, n, head, )
 
 
-#define __dlist_for_each_safe(n, __next, head, doit) \
-    for (dlist_t *n = (head)->next, *__next = n->next;               \
+#define __dlist_for_each_safe(pos, n, __next, head, doit) \
+    for (dlist_t *n = pos, *__next = n->next;                        \
          n != (head) && ({ doit; prefetch(__next->next); });         \
          n = __next, __next = n->next)
 
 #define dlist_for_each_safe(n, head) \
-         __dlist_for_each_safe(n, __next_##n, head, )
+    __dlist_for_each_safe((head)->next, n, __next_##n, head, )
+#define dlist_for_each_safe_start(pos, n, head) \
+    __dlist_for_each_safe(pos, n, __next_##n, head, )
+#define dlist_for_each_safe_continue(pos, n, head) \
+    __dlist_for_each_safe((pos)->next, n, __next_##n, head, )
 
-#define dlist_for_each_entry_safe(n, head, member) \
-    __dlist_for_each_safe(__real_##n, __next_##n, head,              \
+
+#define __dlist_for_each_rev(pos, n, head, doit) \
+     for (dlist_t *n = (pos)->prev; \
+          n != (head) && ({ doit; prefetch(n->prev); }); n = n->prev)
+
+#define dlist_for_each_rev(n, head) \
+    __dlist_for_each_rev((head)->prev, n, head, )
+#define dlist_for_each_rev_start(pos, n, head) \
+    __dlist_for_each_rev(pos, n, head, )
+#define dlist_for_each_rev_continue(pos, n, head) \
+    __dlist_for_each_rev((pos)->prev, n, head, )
+
+
+#define __dlist_for_each_rev_safe(pos, n, __prev, head, doit) \
+    for (dlist_t *n = pos, *__prev = n->prev;                      \
+         n != (head) && ({ doit; prefetch(__prev->prev); });       \
+         n = __prev, __prev = n->prev)
+
+#define dlist_for_each_rev_safe(n, head) \
+    __dlist_for_each_rev_safe((head)->prev, n, __prev_##n, head, )
+#define dlist_for_each_rev_safe_start(pos, n, head) \
+    __dlist_for_each_rev_safe(pos, n, __prev_##n, head, )
+#define dlist_for_each_rev_safe_continue(pos, n, head) \
+    __dlist_for_each_rev_safe((pos)->prev, n, __prev_##n, head, )
+
+
+#define __dlist_for_each_entry(pos, n, head, member) \
+    __dlist_for_each(pos, __real_##n, head,                        \
+                     n = dlist_entry_of(__real_##n, n, member))
+
+#define dlist_for_each_entry(n, head, member) \
+    __dlist_for_each_entry((head)->next, n, head, member)
+#define dlist_for_each_entry_start(pos, n, head, member) \
+    __dlist_for_each_entry(&(pos)->member, n, head, member)
+#define dlist_for_each_entry_continue(pos, n, head, member) \
+    __dlist_for_each_entry((pos)->member.next, n, head, member)
+
+
+#define __dlist_for_each_entry_safe(pos, n, head, member) \
+    __dlist_for_each_safe(pos, __real_##n, __next_##n, head,  \
                           n = dlist_entry_of(__real_##n, n, member))
 
+#define dlist_for_each_entry_safe(n, head, member) \
+    __dlist_for_each_safe((head)->next, n, head, member)
+#define dlist_for_each_entry_safe_start(pos, n, head, member) \
+    __dlist_for_each_entry(&(pos)->member, n, head, member)
+#define dlist_for_each_entry_safe_continue(pos, n, head, member) \
+    __dlist_for_each_entry((pos)->member.next, n, head, member)
