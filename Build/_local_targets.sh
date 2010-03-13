40c6fed7e3d2f9aa5c7ed506404440dc76cb6a3e
Pierre Habouzit <pierre.habouzit@intersec.com>
2010-03-14 00:18:36 +0100

    update copyrights
    Signed-off-by: Pierre Habouzit <pierre.habouzit@intersec.com>

--- Build/_local_targets.sh
+++ Build/_local_targets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 ##########################################################################
 #                                                                        #
-#  Copyright (C) 2004-2009 INTERSEC SAS                                  #
+#  Copyright (C) 2004-2010 INTERSEC SAS                                  #
 #                                                                        #
 #  Should you receive a copy of this source code, you must check you     #
 #  have a proper, written authorization of INTERSEC to hold it. If you   #
