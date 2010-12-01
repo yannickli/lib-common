750dcb8093d79c9880113df04c0b6405e3f1bc63
Pierre Habouzit <pierre.habouzit@intersec.com>
2010-12-02 00:10:47 +0100

    be more c++ compliant
    Signed-off-by: Pierre Habouzit <pierre.habouzit@intersec.com>

--- el.h
+++ el.h
@@ -65,13 +65,13 @@ el_t el_signal_register_d(int signo, el_signal_f *, el_data_t);
 el_t el_child_register_d(pid_t pid, el_child_f *, el_data_t);
 
 static inline el_t el_before_register(el_cb_f *f, void *ptr) {
-    return el_before_register_d(f, (el_data_t)ptr);
+    return el_before_register_d(f, (el_data_t){ ptr });
 }
 static inline el_t el_signal_register(int signo, el_signal_f *f, void *ptr) {
-    return el_signal_register_d(signo, f, (el_data_t)ptr);
+    return el_signal_register_d(signo, f, (el_data_t){ ptr });
 }
 static inline el_t el_child_register(pid_t pid, el_child_f *f, void *ptr) {
-    return el_child_register_d(pid, f, (el_data_t)ptr);
+    return el_child_register_d(pid, f, (el_data_t){ ptr });
 }
 
 void el_before_set_hook(el_t, el_cb_f *);
@@ -89,7 +89,7 @@ pid_t el_child_getpid(el_t);
 /*----- proxy related -----*/
 el_t el_proxy_register_d(el_proxy_f *, el_data_t);
 static inline el_t el_proxy_register(el_proxy_f *f, void *ptr) {
-    return el_proxy_register_d(f, (el_data_t)ptr);
+    return el_proxy_register_d(f, (el_data_t){ ptr });
 }
 void el_proxy_set_hook(el_t, el_proxy_f *);
 el_data_t el_proxy_unregister(el_t *);
@@ -105,7 +105,7 @@ void el_stopper_unregister(void);
 /*----- fd related -----*/
 el_t el_fd_register_d(int fd, short events, el_fd_f *, el_data_t);
 static inline el_t el_fd_register(int fd, short events, el_fd_f *f, void *ptr) {
-    return el_fd_register_d(fd, events, f, (el_data_t)ptr);
+    return el_fd_register_d(fd, events, f, (el_data_t){ ptr });
 }
 void el_fd_set_hook(el_t, el_fd_f *);
 el_data_t el_fd_unregister(el_t *, bool do_close);
@@ -153,7 +153,7 @@ el_t el_timer_register_d(int next, int repeat, int flags, el_cb_f *, el_data_t);
 static inline
 el_t el_timer_register(int next, int repeat, int flags, el_cb_f *f, void *ptr)
 {
-    return el_timer_register_d(next, repeat, flags, f, (el_data_t)ptr);
+    return el_timer_register_d(next, repeat, flags, f, (el_data_t){ ptr });
 }
 /** \brief restart a single shot timer.
  *
