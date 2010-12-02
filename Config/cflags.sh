e0c71a02a7815210894afee7811e9af0da238baa
Pierre Habouzit <pierre.habouzit@intersec.com>
2010-12-02 17:58:37 +0100

    add some c++ flags to check for coding rules
    Signed-off-by: Pierre Habouzit <pierre.habouzit@intersec.com>

--- Config/cflags.sh
+++ Config/cflags.sh
@@ -122,4 +122,11 @@ if ! is_cpp; then
     echo -Wdeclaration-after-statement
 fi
 
+if is_cpp; then
+    echo -Wnon-virtual-dtor
+    echo -Weffc++
+    echo -Wold-style-cast
+    echo -Woverloaded-virtual
+fi
+
 echo -D_GNU_SOURCE $(getconf LFS_CFLAGS)
