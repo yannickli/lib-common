19d2717c87ff446650f4012a9431fe305248e016
Florent Bruneau <florent.bruneau@intersec.com>
2011-05-19 12:10:23 +0200

    qkv: wah: simple implementation of WAH.
    Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

---
+++ wah.h
@@ -0,0 +1,207 @@
+/**************************************************************************/
+/*                                                                        */
+/*  Copyright (C) 2004-2011 INTERSEC SAS                                  */
+/*                                                                        */
+/*  Should you receive a copy of this source code, you must check you     */
+/*  have a proper, written authorization of INTERSEC to hold it. If you   */
+/*  don't have such an authorization, you must DELETE all source code     */
+/*  files in your possession, and inform INTERSEC of the fact you obtain  */
+/*  these files. Should you not comply to these terms, you can be         */
+/*  prosecuted in the extent permitted by applicable law.                 */
+/*                                                                        */
+/**************************************************************************/
+
+#ifndef QKV_WAH_H
+#define QKV_WAH_H
+
+typedef struct wah_header_t {
+#if __BYTE_ORDER == __BIG_ENDIAN
+    unsigned  bit   : 1;
+    unsigned  words : 31;
+#else
+    unsigned  words : 31;
+    unsigned  bit   : 1;
+#endif
+} wah_header_t;
+
+typedef union wah_word_t {
+    wah_header_t head;
+    uint32_t     count;
+    uint32_t     literal;
+} wah_word_t;
+qvector_t(wah, wah_word_t);
+
+typedef struct wah_t {
+    uint64_t  len;
+    uint64_t  active;
+
+    wah_header_t first_run_head;
+    uint32_t     first_run_len;
+
+    int       previous_run_pos;
+    int       last_run_pos;
+    qv_t(wah) data;
+    uint32_t  pending;
+} wah_t;
+#define WAH_BIT_IN_WORD  bitsizeof(wah_word_t)
+
+static ALWAYS_INLINE
+wah_header_t *wah_last_run_header(wah_t *map)
+{
+    if (map->last_run_pos < 0) {
+        return &map->first_run_head;
+    } else {
+        return &map->data.tab[map->last_run_pos].head;
+    }
+}
+
+static ALWAYS_INLINE
+uint32_t *wah_last_run_count(wah_t *map)
+{
+    if (map->last_run_pos < 0) {
+        return &map->first_run_len;
+    } else {
+        return &map->data.tab[map->last_run_pos].count;
+    }
+}
+
+static ALWAYS_INLINE
+void wah_append_header(wah_t *map, wah_header_t head)
+{
+    wah_word_t word;
+    word.head = head;
+    qv_append(wah, &map->data, word);
+    word.count = 0;
+    qv_append(wah, &map->data, word);
+}
+
+static ALWAYS_INLINE
+void wah_append_literal(wah_t *map, uint32_t val)
+{
+    wah_word_t word;
+    word.literal = val;
+    qv_append(wah, &map->data, word);
+}
+
+static inline
+void wah_flatten_last_run(wah_t *map)
+{
+    wah_header_t *head;
+    if (map->last_run_pos < 0) {
+        return;
+    }
+    assert (*wah_last_run_count(map) == 0);
+    head = wah_last_run_header(map);
+    assert (head->words == 1);
+    assert (map->data.len == map->last_run_pos + 2);
+    map->data.len -= 2;
+    wah_append_literal(map, head->bit ? UINT32_MAX : 0);
+
+    if (map->previous_run_pos < 0) {
+        map->first_run_len++;
+    } else {
+        map->data.tab[map->previous_run_pos].count++;
+    }
+    map->last_run_pos     = map->previous_run_pos;
+    map->previous_run_pos = -2;
+}
+
+static inline
+void wah_push_pending(wah_t *map, uint32_t words)
+{
+    const bool is_trivial = map->pending == UINT32_MAX || map->pending == 0;
+    if (!is_trivial) {
+        if (wah_last_run_header(map)->words < 2) {
+            wah_flatten_last_run(map);
+        }
+        *wah_last_run_count(map) += 1;
+        wah_append_literal(map, map->pending);
+    } else {
+        wah_header_t *head = wah_last_run_header(map);
+        if (*wah_last_run_count(map) == 0
+        && (!head->bit == !map->pending || head->words == 0)) {
+            /* Merge with previous */
+            head->words += words;
+            head->bit    = !map->pending;
+        } else {
+            /* Create a new run */
+            wah_header_t new_head;
+            if (head->words < 2) {
+                wah_flatten_last_run(map);
+            }
+            new_head.bit          = !!map->pending;
+            new_head.words        = words;
+            map->previous_run_pos = map->last_run_pos;
+            map->last_run_pos     = map->data.len;
+            wah_append_header(map, new_head);
+        }
+    }
+    map->pending = 0;
+}
+
+static inline
+void wah_add0s(wah_t *map, uint64_t count)
+{
+    uint32_t remain = map->len % WAH_BIT_IN_WORD;
+    if (count == 0) {
+        return;
+    }
+    if (remain > 0) {
+        if (count < remain) {
+            map->len += count;
+            return;
+        }
+        count    -= remain;
+        map->len += remain;
+        wah_push_pending(map, 1);
+    }
+
+    if (count == 0) {
+        return;
+    }
+    if (count >= WAH_BIT_IN_WORD) {
+        wah_push_pending(map, count / WAH_BIT_IN_WORD);
+    }
+    map->len += count;
+}
+
+static inline
+void wah_add1s(wah_t *map, uint64_t count)
+{
+    uint64_t remain = map->len % WAH_BIT_IN_WORD;
+    if (count == 0) {
+        return;
+    }
+    if (remain > 0) {
+        if (count < remain) {
+            uint32_t mask = UINT32_MAX >> (32 - count);
+            mask <<= (32 - remain);
+            map->pending |= mask;
+            map->len     += count;
+            map->active  += count;
+            return;
+        } else {
+            uint32_t mask = UINT32_MAX << (32 - remain);
+            map->pending |= mask;
+            map->len     += remain;
+            map->active  += remain;
+            count -= remain;
+            wah_push_pending(map, 1);
+        }
+    }
+    if (count == 0) {
+        return;
+    }
+    if (count >= WAH_BIT_IN_WORD) {
+        map->pending = UINT32_MAX;
+        wah_push_pending(map, count / WAH_BIT_IN_WORD);
+        map->pending = 0;
+    }
+    if (count % WAH_BIT_IN_WORD != 0) {
+        map->pending = UINT32_MAX >> (32 - (count % WAH_BIT_IN_WORD));
+    }
+    map->len    += count;
+    map->active += count;
+}
+
+#endif
