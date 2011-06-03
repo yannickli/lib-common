1a50402d85bf7763a425cf427daa61bcdbb01643
Pierre Habouzit <pierre.habouzit@intersec.com>
2011-06-03 11:43:26 +0200

    atomic: fix wrong cast for atomic_bool_cas
    This returns a bool, not a type_t *
    
    Signed-off-by: Pierre Habouzit <pierre.habouzit@intersec.com>

--- core-atomic.h
+++ core-atomic.h
@@ -41,7 +41,7 @@ typedef int spinlock_t;
 #define atomic_get_and_sub(ptr, v)  ((typeof(*(ptr)))__sync_fetch_and_sub(ptr, v))
 
 #define atomic_xchg(ptr, v)         ((typeof(*(ptr)))__sync_lock_test_and_set(ptr, v))
-#define atomic_bool_cas(ptr, b, a)  ((typeof(*(ptr)))__sync_bool_compare_and_swap(ptr, b, a))
+#define atomic_bool_cas(ptr, b, a)  __sync_bool_compare_and_swap(ptr, b, a)
 #define atomic_val_cas(ptr, b, a)   ((typeof(*(ptr)))__sync_val_compare_and_swap(ptr, b, a))
 
 #define spin_trylock(ptr)  (!__sync_lock_test_and_set(ptr, 1))
