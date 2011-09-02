9849f62525323be9411b9dddc1b4a2d6fadeaf3c
Pierre Habouzit <pierre.habouzit@intersec.com>
2011-09-02 16:01:57 +0200

    Improve str-ctype-maker even more.
    Signed-off-by: Pierre Habouzit <pierre.habouzit@intersec.com>

--- tools/str-ctype-maker.c
+++ tools/str-ctype-maker.c
@@ -17,25 +17,27 @@
 int main(int argc, char **argv)
 {
     SB_1k(buf);
+    SB_1k(buf_del);
 
     if (argc <= 2) {
         puts("usage: str-ctype-maker varname < chars | range >...");
         puts("");
-        puts("  chars: s:<c-quoted list of chars>");
-        puts("  range: r:code1[-code2] (code can be decimal, hexa or octal)");
+        puts("  chars: [-]s:<c-quoted list of chars>");
+        puts("  range: [-]r:code1[-code2] (code can be decimal, hexa or octal)");
         return 1;
     }
 
     sb_init(&buf);
     for (int i = 2; i < argc; i++) {
         const char *s = argv[i];
+        sb_t *sb = (argv[i][0] == '-') ? &buf_del : &buf;
 
-        if (strstart(s, "s:", &s)) {
-            sb_adds_unquoted(&buf, argv[2] + 2);
+        if (strstart(s, "-s:", &s) || strstart(s, "s:", &s)) {
+            sb_adds_unquoted(sb, s);
             continue;
         }
 
-        if (strstart(s, "r:", &s)) {
+        if (strstart(s, "-r:", &s) || strstart(s, "r:", &s)) {
             long l, r;
 
             errno = 0;
@@ -56,7 +58,7 @@ int main(int argc, char **argv)
             if ((uint8_t)r != r || (uint8_t)l != l || l > r)
                 goto range_error;
             for (int c = l; c <= r; c++)
-                sb_addc(&buf, c);
+                sb_addc(sb, c);
             continue;
 
           range_error:
@@ -67,15 +69,18 @@ int main(int argc, char **argv)
     }
 
     {
-        ctype_desc_t d;
+        ctype_desc_t d, e;
 
         ctype_desc_build2(&d, buf.data, buf.len);
+        ctype_desc_build2(&e, buf_del.data, buf_del.len);
 
         printf("ctype_desc_t const %s = { {\n", argv[1]);
         printf("    0x%08x, 0x%08x, 0x%08x, 0x%08x,\n",
-               d.tab[0], d.tab[1], d.tab[2], d.tab[3]);
+               d.tab[0] & ~e.tab[0], d.tab[1] & ~e.tab[1],
+               d.tab[2] & ~e.tab[2], d.tab[3] & ~e.tab[3]);
         printf("    0x%08x, 0x%08x, 0x%08x, 0x%08x,\n",
-               d.tab[4], d.tab[5], d.tab[6], d.tab[7]);
+               d.tab[4] & ~e.tab[4], d.tab[5] & ~e.tab[5],
+               d.tab[6] & ~e.tab[6], d.tab[7] & ~e.tab[7]);
         puts("} };");
     }
 
