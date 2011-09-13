81d8bd4905aced9d4108c4da65b4fed4395f4098
Florent Bruneau <florent.bruneau@intersec.com>
2011-09-13 11:36:32 +0200

    build: add support for a NOCOMPRESS parameter.
    Sometimes we want to disable debug-section compression because some tools
    don't support compressed sections (eg pahole, valgrind, some versions of
    gdb, cf thread on corp.sw.general for details).
    
    Change-Id: I94ef0bce06ba0233a3e8f0fc4ad7cdbc3b6fb45e
    Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>
    Reviewed-by: Pierre Habouzit <pierre.habouzit@intersec.com>

--- Config/cflags.mk
+++ Config/cflags.mk
@@ -11,37 +11,21 @@
 #                                                                        #
 ##########################################################################
 
-LDFLAGS := -Wl,--as-needed
+CFLAGS :=
+CPPFLAGS :=
+LDFLAGS :=
+
+GCCVERSION := $(shell $(CC) -dumpversion)
+GCCMAJOR   := $(word 1,$(subst ., ,$(GCCVERSION)))
+GCCMINOR   := $(word 2,$(subst ., ,$(GCCVERSION)))
+GCC_PREREQ=$(shell test $(GCCMAJOR) -lt $1 || test $(GCCMAJOR) = $1 -a $(GCCMINOR) -lt $2 || echo 1)
+
+LDFLAGS += -Wl,--as-needed
 ifeq (,$(NOCOMPRESS))
 ifneq (,$(shell ld --help | grep compress-debug-sections))
     LDFLAGS += -Wl,--compress-debug-sections=zlib
 endif
 endif
-
-$!clang-flags.mk: $(var/toolsdir)/* $(var/cfgdir)/*.mk
-	echo -n "CLANGFLAGS := "                                  >  $@+
-	$(var/cfgdir)/cflags.sh "clang" "rewrite" | tr '\n' ' '   >> $@+
-	echo                                                      >> $@+
-	echo -n "CLANGXXFLAGS := "                                >> $@+
-	$(var/cfgdir)/cflags.sh "clang++" "rewrite" | tr '\n' ' ' >> $@+
-	echo                                                      >> $@+
-	mv $@+ $@
-
-$!cc-$(CC)-flags.mk: $(var/toolsdir)/* $(var/cfgdir)/*.mk
-	echo -n "CFLAGS := "                          >  $@+
-	$(var/cfgdir)/cflags.sh "$(CC)" | tr '\n' ' ' >> $@+
-	echo                                          >> $@+
-	mv $@+ $@
-
-$!cxx-$(CXX)-flags.mk: $(var/toolsdir)/* $(var/cfgdir)/*.mk
-	echo -n "CXXFLAGS := "                         >  $@+
-	$(var/cfgdir)/cflags.sh "$(CXX)" | tr '\n' ' ' >> $@+
-	echo                                           >> $@+
-	mv $@+ $@
-
--include $!clang-flags.mk
--include $!cc-$(CC)-flags.mk
--include $!cxx-$(CXX)-flags.mk
 
 CFLAGS       += -I$/lib-common/compat -I$/
 CXXFLAGS     += -I$/lib-common/compat -I$/
