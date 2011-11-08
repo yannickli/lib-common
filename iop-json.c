yannickli/lib-common

commit 84b43416bae0a570ccf3d258f5100ae17d9dc1cd

iop-json: support double values such as .42
update the last token ll->last by using TK() so that when we find a dot,
it does not wrongly believe the last token was an IDENT or a STRING.
('id.x' would mean that id is an union, but 'id: .x' is completely
different)

Closes: #6326
Change-Id: Ie1f2f06f2de2528a8db087e3d62522bd2d6514b2

iop-json.c
@@ -574,14 +574,14 @@ static int iop_json_lex(iop_json_lex_t *ll)
       case '{': case '}':
       case '[': case ']':
       case '@':
-                return EATC();
+                return TK(EATC());
 
       case 'a' ... 'z': case 'A' ... 'Z':
                 return iop_json_lex_token(ll);
 
       case '.':
                 if (ll->last == IOP_JSON_IDENT || ll->last == IOP_JSON_STRING)
-                    return EATC();
+                    return TK(EATC());
                 /* FALLTHROUGH */
       case '-': case '+':
       case '0' ... '9':

ztst-iop.c
@@ -306,7 +306,7 @@ static void test_json(void)
 //"            l: {us: \"union value normal\"},\n"
 "            foo: {us: \"union value to skip\"},\n"
 "            bar.us: \"union value to skip\",\n"
-"            \"m\": 10.42\n,"
+"            \"m\": .42\n,"
 "            \"n\": true\n"
 "        };\n"
     ;

commit 778543f33e60b606624629df5e9d8b99b6372dbf

iop-json: fix regression on union extended syntax parsing
84b43416bae0a570ccf3d258f5100ae17d9dc1cd broke the parsing of the union
extended syntax inside an array. Moreover '=' and ',' tokens wasn't
correctly handled in the patch.

This patch finish the job.

Closes: #6326
Change-Id: I0705e7358edf5c7d765439c2d0b71ebb1ff7f059

iop-json.c
@@ -568,8 +568,8 @@ static int iop_json_lex(iop_json_lex_t *ll)
     STORECTX();
     switch (READC()) {
         int c;
-      case '=': SKIP(1); return ':';
-      case ';': SKIP(1); return ',';
+      case '=': SKIP(1); return TK(':');
+      case ';': SKIP(1); return TK(',');
       case ':': case ',':
       case '{': case '}':
       case '[': case ']':
@@ -580,8 +580,11 @@ static int iop_json_lex(iop_json_lex_t *ll)
                 return iop_json_lex_token(ll);
 
       case '.':
-                if (ll->last == IOP_JSON_IDENT || ll->last == IOP_JSON_STRING)
+                if (ll->last == IOP_JSON_IDENT || ll->last == IOP_JSON_STRING
+                ||  ll->last == '[' || ll->last == ',')
+                {
                     return TK(EATC());
+                }
                 /* FALLTHROUGH */
       case '-': case '+':
       case '0' ... '9':

zchk-iop.c
@@ -619,6 +619,8 @@ Z_GROUP_EXPORT(iop)
             "    l.us: \"union value\",\n"
             "    foo: {us: \"union value to skip\"},\n"
             "    bar.us: \"union value to skip\",\n"
+            "    arraytoSkip: [ .blah: \"skip\", .foo: 42, 32; \"skipme\";\n"
+            "                   { foo: 42 } ];"
             "    \"m\": .42\n,"
             "    \"n\": true\n"
             "};\n"
@@ -726,7 +728,6 @@ Z_GROUP_EXPORT(iop)
 
         const iop_struct_t *st_sa, *st_sf;
 
-        Z_TODO("json_sf test is broken!");
         if ((dso = iop_dso_open(path.s)) == NULL)
             Z_SKIP("unable to load zchk-tstiop-plugin, TOOLS repo?");
 