4ee3f675f4fa2f4bffb7fc92883e84de36eee081
Julien Richefeu <julien.richefeu@intersec.com>
2012-01-04 11:03:49 +0100

    str-num: Modify trademark to match right company name
    Change-Id: Ib003f353ae46c05b6106102e67461c37242f201b
    Signed-off-by: Julien Richefeu <julien.richefeu@intersec.com>

--- str-num.h
+++ str-num.h
@@ -1,11 +1,11 @@
 /**************************************************************************/
 /*                                                                        */
-/*  Copyright (C) 2004-2011 NUMERSEC SAS                                  */
+/*  Copyright (C) 2004-2011 INTERSEC SAS                                  */
 /*                                                                        */
 /*  Should you receive a copy of this source code, you must check you     */
-/*  have a proper, written authorization of NUMERSEC to hold it. If you   */
+/*  have a proper, written authorization of INTERSEC to hold it. If you   */
 /*  don't have such an authorization, you must DELETE all source code     */
-/*  files in your possession, and inform NUMERSEC of the fact you obtain  */
+/*  files in your possession, and inform INTERSEC of the fact you obtain  */
 /*  these files. Should you not comply to these terms, you can be         */
 /*  prosecuted in the extent permitted by applicable law.                 */
 /*                                                                        */
