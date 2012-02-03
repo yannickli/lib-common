yannickli/lib-common

commit cdf624c2d65bd921dabd66b988b771eb96e94657

httpd: add notification callbacks for connections/disconnections/query termination
on_accept is called after the httpd has been spawned in order to ensure we
have access to informations such as the configuration or the file
descriptor (useful if you want to log the remote address).

on_status is called each time a query's lifecycle ends. We ensure the callback
has access to description of the query (such as the method, the path, ...)
by setting the q->qinfo while parsing.

Refs: #6751
Change-Id: If53624be8c05dea9ed6ff630ee7449590b80e906
Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

http.c
@@ -575,8 +575,14 @@ void httpd_reply_chunk_done_(httpd_query_t *q, outbuf_t *ob)
                      ob->length - q->chunk_prev_length);
 }
 
+
+__attribute__((format(printf, 4, 0)))
+static void httpd_notify_status(httpd_t *w, httpd_query_t *q, int handler,
+                              const char *fmt, va_list va);
+
 void httpd_reply_done(httpd_query_t *q)
 {
+    va_list va;
     outbuf_t *ob = httpd_get_ob(q);
 
     assert (q->hdrs_done && !q->answered && !q->chunk_started);
@@ -587,6 +593,7 @@ void httpd_reply_done(httpd_query_t *q)
                            ob->length - q->chunk_prev_length);
         q->clength_hack = false;
     }
+    httpd_notify_status(q->owner, q, HTTPD_QUERY_STATUS_ANSWERED, "", va);
     httpd_mark_query_answered(q);
 }
 
@@ -648,6 +655,10 @@ void httpd_reject_(httpd_query_t *q, int code, const char *fmt, ...)
     ob_addvf(ob, fmt, ap);
     va_end(ap);
     ob_adds(ob, "</p></body></html>\r\n");
+
+    va_start(ap, fmt);
+    httpd_notify_status(q->owner, q, HTTPD_QUERY_STATUS_ANSWERED, fmt, ap);
+    va_end(ap);
     httpd_reply_done(q);
 }
 
@@ -656,6 +667,7 @@ void httpd_reject_unauthorized(httpd_query_t *q, lstr_t auth_realm)
     const lstr_t body = LSTR_IMMED_V("<html><body>"
                                      "<h1>401 - Authentication required</h1>"
                                      "</body></html>\r\n");
+    va_list va;
     outbuf_t *ob;
 
     if (q->answered || q->hdrs_started)
@@ -667,6 +679,8 @@ void httpd_reject_unauthorized(httpd_query_t *q, lstr_t auth_realm)
             LSTR_FMT_ARG(auth_realm));
     httpd_reply_hdrs_done(q, body.len, false);
     ob_add(ob, body.s, body.len);
+
+    httpd_notify_status(q->owner, q, HTTP_CODE_UNAUTHORIZED, "", va);
     httpd_reply_done(q);
 }
 
@@ -781,6 +795,19 @@ httpd_trigger_resolve(httpd_trigger_node_t *n, httpd_qinfo_t *req)
 /* }}} */
 /* HTTPD Parser {{{ */
 
+__attribute__((format(printf, 4, 0)))
+static void httpd_notify_status(httpd_t *w, httpd_query_t *q, int handler,
+                              const char *fmt, va_list va)
+{
+    if (!q->status_sent) {
+        q->status_sent = true;
+
+        if (w->on_status) {
+            (*w->on_status)(w, q, handler, fmt, va);
+        }
+    }
+}
+
 static void httpd_set_mask(httpd_t *w)
 {
     int mask = POLLIN;
@@ -894,6 +921,7 @@ static int httpd_parse_idle(httpd_t *w, pstream_t *ps)
         cb = httpd_trigger_resolve(&w->cfg->roots[req.method], &req);
     q = httpd_query_create(w, cb);
     q->http_version = req.http_version;
+    q->qinfo        = &req;
     buf = __ps_get_ps_upto(ps, p + 2);
     __ps_skip_upto(ps, p + 4);
     switch (req.http_version) {
@@ -1196,6 +1224,17 @@ static httpd_t *httpd_init(httpd_t *w)
 
 static void httpd_wipe(httpd_t *w)
 {
+    if (w->on_status) {
+        va_list va;
+
+        dlist_for_each_safe (it, &w->query_list) {
+            httpd_notify_status(w, dlist_entry(it, httpd_query_t, query_link),
+                              HTTPD_QUERY_STATUS_CANCEL, "Query cancelled", va);
+        }
+    }
+    if (w->on_disconnect) {
+        (*w->on_disconnect)(w);
+    }
     el_fd_unregister(&w->ev, true);
     sb_wipe(&w->ibuf);
     ob_wipe(&w->ob);
@@ -1440,6 +1479,9 @@ httpd_t *httpd_spawn(int fd, httpd_cfg_t *cfg)
     w->max_queries = cfg->max_queries;
     el_fd_watch_activity(w->ev, POLLINOUT, w->cfg->noact_delay);
     dlist_add_tail(&cfg->httpd_list, &w->httpd_link);
+    if (w->on_accept) {
+        (*w->on_accept)(w);
+    }
     return w;
 }
 

http.h
@@ -213,22 +213,51 @@ typedef struct httpd_trigger_t      httpd_trigger_t;
 qm_kvec_t(http_path, lstr_t, httpd_trigger_node_t *,
           qhash_lstr_hash, qhash_lstr_equal);
 
+enum httpd_query_status {
+    HTTPD_QUERY_STATUS_CANCEL,
+    HTTPD_QUERY_STATUS_ANSWERED,
+};
+
+/** Type of HTTP server.
+ *
+ * The httpd structure contains callbacks that let you be notified when a
+ * connection is established and when a query is processed.
+ *
+ * - httpd_t#on_accept is called whenever a new connection has been accepted.
+ *   The object passed as argument is the fully-filled connection handler
+ *   (including its configuration and its file descriptor).
+ * - httpd_t#on_status is called whenever the final state of a query is
+ *   reached. This state can be:
+ *     - the query has been answered, then the query object is passed and the
+ *     status is ANSWERED.
+ *     - the query has been detached, then the query object is passed and the
+ *     status is set to CANCEL. Query are detached when the httpd_t get
+ *     disconnected while still having pending queries.
+ * - httpd_t#on_disconnect is called whenever the connection get closed.
+ */
 #define HTTPD_FIELDS(pfx) \
-    OBJECT_FIELDS(pfx);                                    \
-    dlist_t            httpd_link;                         \
-    httpd_cfg_t       *cfg;                                \
-    el_t               ev;                                 \
-    sb_t               ibuf;                               \
-                                                           \
-    flag_t             connection_close   : 1;             \
-    uint8_t            state;                              \
-    uint16_t           queries;                            \
-    uint16_t           queries_done;                       \
-    unsigned           max_queries;                        \
-    unsigned           chunk_length;                       \
-                                                           \
-    dlist_t            query_list;                         \
-    outbuf_t           ob
+    OBJECT_FIELDS(pfx);                                                      \
+    dlist_t            httpd_link;                                           \
+    httpd_cfg_t       *cfg;                                                  \
+    el_t               ev;                                                   \
+    sb_t               ibuf;                                                 \
+                                                                             \
+    flag_t             connection_close   : 1;                               \
+    uint8_t            state;                                                \
+    uint16_t           queries;                                              \
+    uint16_t           queries_done;                                         \
+    unsigned           max_queries;                                          \
+    unsigned           chunk_length;                                         \
+                                                                             \
+    dlist_t            query_list;                                           \
+    outbuf_t           ob;                                                   \
+                                                                             \
+    void             (*on_accept)(httpd_t *w);                               \
+    void             (*on_disconnect)(httpd_t *w);                           \
+                                                                             \
+    __attribute__((format(printf, 4, 0)))                                    \
+    void             (*on_status)(httpd_t *w, const struct httpd_query_t *q, \
+                                  int status, const char *fmt, va_list va);
 
 #define HTTPD_METHODS(type_t) \
     OBJECT_METHODS(type_t)
@@ -449,6 +478,7 @@ struct httpd_qinfo_t {
     flag_t              answered      : 1;                          \
     flag_t              chunked       : 1;                          \
     flag_t              conn_close    : 1;                          \
+    flag_t              status_sent   : 1;                          \
                                                                     \
     uint16_t            answer_code;                                \
     uint16_t            http_version;                               \

commit 178242ebe9896ebd61a038a97546c4bcd5ae9b81

http: fix regression introduced by cdf624c2d65b.
In order to be able to access the details of the query within the
notification callbacks, q->qinfo is set to a pointer to httpd_qinfo_t
structure allocated on the stack. Since the pointer was left untouched at
the end of the function, this led to freeing a pointer on the stack when
the httpd_query_t is release.

Change-Id: Ib32e0e01f11b44f46c4ddfff263a116d77fccfa8
Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

http.c
@@ -1040,10 +1040,16 @@ static int httpd_parse_idle(httpd_t *w, pstream_t *ps)
         httpd_do_any(w, q, &req);
         break;
     }
+    if (q->qinfo == &req) {
+        q->qinfo = NULL;
+    }
     httpd_query_reply_100continue_(q);
     return PARSE_OK;
 
   unrecoverable_error:
+    if (q->qinfo == &req) {
+        q->qinfo = NULL;
+    }
     w->connection_close = true;
     httpd_query_done(w, q);
     return PARSE_OK;