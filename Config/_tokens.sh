641394ab5a2addca7729af7cf2c5608aef53b6f2
Sébastien Thouément <sebastien.thouement@intersec.com>
2012-04-11 11:14:17 +0200

    mmsx: 2012 copyright update
    Change-Id: I876ef6db04eab38a771c90398997d372cd153c8a
    Signed-off-by: Sébastien Thouément <sebastien.thouement@intersec.com>

--- Config/_tokens.sh
+++ Config/_tokens.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -e
 ##########################################################################
 #                                                                        #
-#  Copyright (C) 2004-2011 INTERSEC SAS                                  #
+#  Copyright (C) 2004-2012 INTERSEC SAS                                  #
 #                                                                        #
 #  Should you receive a copy of this source code, you must check you     #
 #  have a proper, written authorization of INTERSEC to hold it. If you   #
--- Config/cflags.mk
+++ Config/cflags.mk
@@ -1,6 +1,6 @@
 ##########################################################################
 #                                                                        #
-#  Copyright (C) 2004-2011 INTERSEC SAS                                  #
+#  Copyright (C) 2004-2012 INTERSEC SAS                                  #
 #                                                                        #
 #  Should you receive a copy of this source code, you must check you     #
 #  have a proper, written authorization of INTERSEC to hold it. If you   #
