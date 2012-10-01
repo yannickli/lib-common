52062cd2e9f50d5411c25918384b82bcbcd6e9cc
Jean-Philippe Fourès <jean-philippe.foures@intersec.com>
2012-10-01 11:11:22 +0200

    gsm: Add check on convertion from GSM to UTF-8 to GSM
    Change-Id: Ieff8ee70c9281baf11fc42a4870f225a509dadb3

--- str-buf-gsm.c
+++ str-buf-gsm.c
@@ -12,6 +12,7 @@
 /**************************************************************************/
 
 #include "core.h"
+#include "z.h"
 
 /* TODO: support other shift pages than latin1 ! */
 
@@ -665,3 +666,51 @@ int sb_conv_from_gsm7(sb_t *out, const void *_src, int gsmlen, int udhlen)
   done:
     return c ? -1 : 0;
 }
+
+/* {{{ Convertion checks */
+
+Z_GROUP_EXPORT(conv)
+{
+    sb_t tmp, out;
+
+    Z_TEST(sb_conv_gsm, "sb conv from/to gsm") {
+#define T(input, expected, description)      \
+        ({  lstr_t exp = LSTR_IMMED(expected);                               \
+            lstr_t in  = LSTR_IMMED(input);                                  \
+                                                                             \
+            sb_reset(&tmp);                                                  \
+            sb_reset(&out);                                                  \
+            sb_conv_from_gsm(&tmp, in.s, in.len);                            \
+            sb_conv_to_gsm(&out, tmp.data, tmp.len);                         \
+            Z_ASSERT_LSTREQUAL(exp, LSTR_SB_V(&out), description);           \
+        })
+
+        sb_init(&tmp);
+        sb_init(&out);
+
+        T("\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
+          "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1c\x1d\x1e\x1f"
+          "\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f"
+          "\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f"
+          "\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f"
+          "\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f"
+          "\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f"
+          "\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f",
+          "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
+          "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1c\x1d\x1e\x1f"
+          "\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f"
+          "\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f"
+          "\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f"
+          "\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f"
+          "\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f"
+          "\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f",
+          "UTF-8 from default table GSM conversion");
+
+        sb_wipe(&tmp);
+        sb_wipe(&out);
+
+#undef T
+    } Z_TEST_END
+} Z_GROUP_END
+
+/* }}} */
