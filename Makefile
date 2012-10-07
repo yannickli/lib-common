e96a1f4ee9f3161c7cc6fa10dccaf1605dc2e266
Florent Bruneau <florent.bruneau@intersec.com>
2012-10-07 22:59:21 +0200

    sort: add generic uniq/bisect/contains implementations.
    This will ensure people won't reinvent the wheel each time they need a
    binary search on complex structures.
    
    The patch also adds some tests that check both the optimized and the
    generic implementation and makes sure the bisection have sane invariants
    on properly prepared arrays (sorted + uniqed).
    
    Change-Id: I13990f77cfdee8f0c271d0006f21e4fa09090ae9
    Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

--- Makefile
+++ Makefile
@@ -118,7 +118,7 @@ libcommon_SOURCES = \
 	qps-hat.c \
 	qps-bitmap.c \
 	\
-	sort-32-64.c \
+	sort.blk \
 	\
 	str.c \
 	str-buf-gsm.c \
--- sort.h
+++ sort.h
@@ -16,6 +16,7 @@
 
 #include "core.h"
 
+/* 32-64 optimized versions */
 void    dsort32(uint32_t base[], size_t n);
 void    dsort64(uint64_t base[], size_t n);
 size_t  uniq32(uint32_t data[], size_t len);
@@ -24,5 +25,24 @@ size_t  bisect32(uint32_t what, const uint32_t data[], size_t len);
 size_t  bisect64(uint64_t what, const uint64_t data[], size_t len);
 bool    contains32(uint32_t what, const uint32_t data[], size_t len);
 bool    contains64(uint64_t what, const uint64_t data[], size_t len);
+
+/* Generic implementations */
+typedef int (cmp_r_t)(const void *a, const void *b, void *arg);
+
+size_t  uniq(void *data, size_t size, size_t nmemb, cmp_r_t *cmp, void *arg);
+size_t  bisect(const void *what, const void *data, size_t size,
+               size_t nmemb, cmp_r_t *cmp, void *arg);
+bool    contains(const void *what, const void *data, size_t size,
+                 size_t nmemb, cmp_r_t *cmp, void *arg);
+
+#ifdef __has_blocks
+typedef int (BLOCK_CARET cmp_b)(const void *a, const void *b);
+
+size_t  uniq_blk(void *data, size_t size, size_t nmemb, cmp_b cmp);
+size_t  bisect_blk(const void *what, const void *data, size_t size,
+                   size_t nmemb, cmp_b cmp);
+bool    contains_blk(const void *what, const void *data, size_t size,
+                     size_t nmemb, cmp_b cmp);
+#endif
 
 #endif
