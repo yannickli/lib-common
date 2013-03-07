yannickli/lib-common

commit dd927d5eca6cda14d0d33bc3e5718e9950d441bf

sb: have t_SB* macros
so that people stop using the SB* ones.

Change-Id: I94bac3d0223f30f0595d79fc7c2f24f4fa1dc136
Signed-off-by: Pierre Habouzit <pierre.habouzit@intersec.com>

str-buf.h
@@ -65,6 +65,7 @@ typedef struct sb_t {
     unsigned int skip : 30;
 #ifdef __cplusplus
     inline sb_t();
+    inline sb_t(void *buf, int len, int size, int pool);
     inline ~sb_t();
 
   private:
@@ -108,18 +109,18 @@ sb_init_full(sb_t *sb, void *buf, int blen, int bsize, int mem_pool)
     sb_init_full(sb, memset(alloca(sz), 0, 1), 0, (sz), MEM_STATIC)
 
 #ifdef __cplusplus
-#define SB(name, sz)                                    \
-    STATIC_ASSERT((sz) < (64 << 10));                   \
-    sb_t name;                                          \
-    sb_inita(&name, sz)
+#define SB(name, sz)    sb_t name(alloca(sz), 0, sz, MEM_STATIC)
+#define t_SB(name, sz)  sb_t name(t_new_raw(char, sz), 0, sz, MEM_STACK)
 #else
-#define SB(name, sz) \
-    sb_t name = {                                       \
-        .data = memset(alloca(sz), 0, 1),               \
-        .size = (STATIC_ASSERT((sz) < (64 << 10)), sz), \
-    }
+#define SB_INIT(buf, length, sz, pool) \
+    {   .data = (((char *)(buf))[length] = '\0', (buf)), \
+        .len  = length, .size = sz, .mem_pool = pool }
+#define SB(name, sz)    sb_t name = SB_INIT(alloca(sz), 0, sz, MEM_STATIC)
+#define t_SB(name, sz)  sb_t name = SB_INIT(t_new_raw(char, sz), 0, sz, MEM_STACK)
 #endif
 
+#define t_SB_1k(name)  t_SB(name, 1 << 10)
+#define t_SB_8k(name)  t_SB(name, 8 << 10)
 #define SB_1k(name)    SB(name, 1 << 10)
 #define SB_8k(name)    SB(name, 8 << 10)
 
@@ -145,6 +146,15 @@ sb_t::sb_t() :
     mem_pool(MEM_STATIC),
     skip(0)
 {
+}
+sb_t::sb_t(void *buf, int len_, int size_, int pool) :
+    data(static_cast<char *>(buf)),
+    len(len_),
+    size(size_),
+    mem_pool(pool),
+    skip(0)
+{
+    buf[len] = '\0';
 }
 sb_t::~sb_t() { sb_wipe(this); }
 #endif

commit 0fab871488667f8a26ed9eff7891a6ad81e506ee

str-buf: fix regression in SB() introduced in dd927d5.
The sb_t was built using a SB_INIT(buf, ...) macro, that did, in order to
ensure the first byte is set at 0, (buf[length] = 0, buf). As a
consequence, buf was evaluated twice...

buf is 'alloca(sz)', as a consequence:
* we allocated twice the expected memory amount
* we initialized the first byte of one of the two buffers and returned the
  other one, which means the sb had a pointer to the buffer that was not
  initialized.

The first point is quite funny (well...), the second broke an invariant of
the sb (the first unused byte must be 0) which lead to a build failure on
the buildbot:
http://buildbot.corp/builders/nightly/builds/16058/steps/test/logs/stdio

Change-Id: I6514d092d74c2fc985e07ae2e24b457c5b5b356d
Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

str-buf.h
@@ -114,12 +114,12 @@ sb_init_full(sb_t *sb, void *buf, int blen, int bsize, int mem_pool)
 #define SB(name, sz)    sb_t name(alloca(sz), 0, sz, MEM_STATIC)
 #define t_SB(name, sz)  sb_t name(t_new_raw(char, sz), 0, sz, MEM_STACK)
 #else
-#define SB_INIT(buf, length, sz, pool) \
-    {   .data = (((char *)(buf))[length] = '\0', (buf)), \
-        .len  = length, .size = sz, .mem_pool = pool }
+#define SB_INIT(buf, sz, pool)                                               \
+    {   .data = memset(buf, 0, 1),                                           \
+        .size = sz, .mem_pool = pool }
 #define SB(name, sz)    sb_t name __attribute__((cleanup(sb_wipe))) \
-                                  = SB_INIT(alloca(sz), 0, sz, MEM_STATIC)
-#define t_SB(name, sz)  sb_t name = SB_INIT(t_new_raw(char, sz), 0, sz, MEM_STACK)
+                                  = SB_INIT(alloca(sz), sz, MEM_STATIC)
+#define t_SB(name, sz)  sb_t name = SB_INIT(t_new_raw(char, sz), sz, MEM_STACK)
 #endif
 
 #define t_SB_1k(name)  t_SB(name, 1 << 10)