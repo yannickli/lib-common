4340f1698e4cdefb241cd2d6ec1d2504622aa21f
Nicolas Barbey <nicolas.barbey@intersec.com>
2013-03-21 10:08:35 +0100

    iop: Handle character string literal prefix in JSON IOP files.
    The special c"x" sequence is detected and x is converted to an
    int64_t. x can be a single character or an escape sequence (including
    octal, hexadecimal and unicode). Simple character, octal, hexadecimal,
    unicode and \t are tested in zchk-iop.
    
    Closes: #13292
    Version: 2011.2
    Change-Id: I0284c89068827e6e71fd7f8cdfd2e1649bdf30f4
    Signed-off-by: Nicolas Barbey <nicolas.barbey@intersec.com>

--- zchk-iop.c
+++ zchk-iop.c
@@ -582,6 +582,11 @@ Z_GROUP_EXPORT(iop)
             .l = IOP_UNION(tstiop__my_union_a, ub, 42),
             .m = 3.14159265,
             .n = true,
+            .o = '.',
+            .p = '!',
+            .q = '*',
+            .r = '+',
+            .s = '\t',
         };
 
         tstiop__my_struct_a__t sa2 = {
@@ -600,6 +605,11 @@ Z_GROUP_EXPORT(iop)
             .l = IOP_UNION(tstiop__my_union_a, ub, 42),
             .m = 3.14159265,
             .n = true,
+            .o = '.',
+            .p = '!',
+            .q = '*',
+            .r = '+',
+            .s = '\t',
         };
 
         const char json_sa[] =
@@ -621,8 +631,13 @@ Z_GROUP_EXPORT(iop)
             "    bar.us: \"union value to skip\",\n"
             "    arraytoSkip: [ .blah: \"skip\", .foo: 42, 32; \"skipme\";\n"
             "                   { foo: 42 } ];"
-            "    \"m\": .42\n,"
-            "    \"n\": true\n"
+            "    \"m\": .42,\n"
+            "    \"n\": true,\n"
+            "    \"o\": c\'.\',\n"
+            "    \"p\": c\'\\041\',\n"
+            "    \"q\": c\'\\x2A\',\n"
+            "    \"r\": c\'\\u002B\',\n"
+            "    \"s\": c\'\\t\'\n"
             "};\n"
             ;
 
@@ -644,7 +659,12 @@ Z_GROUP_EXPORT(iop)
             "    foo: {us: \"union value to skip\"},\n"
             "    bar.us: \"union value to skip\",\n"
             "    \"m\": 0.42\n,"
-            "    \"n\": true\n"
+            "    \"n\": true,\n"
+            "    \"o\": c\'.\',\n"
+            "    \"p\": c\'\\041\',\n"
+            "    \"q\": c\'\\x2A\',\n"
+            "    \"r\": c\'\\u002B\',\n"
+            "    \"s\": c\'\\t\'\n"
             "};\n"
             ;
 
@@ -664,6 +684,11 @@ Z_GROUP_EXPORT(iop)
             .l = IOP_UNION(tstiop__my_union_a, us, LSTR_IMMED("union value")),
             .m = 0.42,
             .n = true,
+            .o = '.',
+            .p = '!',
+            .q = '*',
+            .r = '+',
+            .s = '\t',
         };
 
         const char json_sf[] =
