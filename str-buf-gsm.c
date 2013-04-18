5b83d4bcef410db76fae03abd063cba2b4efb3f9
Xavier Chantry <xavier.chantry@intersec.com>
2013-04-18 14:48:54 +0200

    str-buf-gsm: add tests for sb_conv_to_gsm_hex
    note: a test for conversion with invalid characters has been added in
    2012.4, it will require an update for the new macro
    
    Change-Id: Id73ddf93bab7493c625fce28d6609f01c61b676b

--- str-buf-gsm.c
+++ str-buf-gsm.c
@@ -679,44 +679,84 @@ int sb_conv_from_gsm7(sb_t *out, const void *_src, int gsmlen, int udhlen)
 Z_GROUP_EXPORT(conv)
 {
     sb_t tmp, out;
+    lstr_t default_tab = LSTR_IMMED(
+        "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
+        "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1c\x1d\x1e\x1f"
+        "\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f"
+        "\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f"
+        "\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f"
+        "\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f"
+        "\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f"
+        "\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f");
+    lstr_t extended_tab = LSTR_IMMED(
+        "\x1b\x14\x1b\x28\x1b\x29\x1b\x2f\x1b\x3c\x1b\x3d\x1b\x3e\x1b\x40\x1b\x65");
 
     Z_TEST(sb_conv_gsm, "sb conv from/to gsm") {
-#define T(input, expected, description)      \
-        ({  lstr_t exp = LSTR_IMMED(expected);                               \
-            lstr_t in  = LSTR_IMMED(input);                                  \
-                                                                             \
+#define TL(input, expected, desc)       \
+        ({  lstr_t in   = input;                                             \
+            lstr_t exp  = expected;                                          \
             sb_reset(&tmp);                                                  \
             sb_reset(&out);                                                  \
             sb_conv_from_gsm(&tmp, in.s, in.len);                            \
             sb_conv_to_gsm(&out, tmp.data, tmp.len);                         \
-            Z_ASSERT_LSTREQUAL(exp, LSTR_SB_V(&out), description);           \
+            Z_ASSERT_LSTREQUAL(exp, LSTR_SB_V(&out), desc);                  \
+        })
+#define TLHEX(input, expected, desc)    \
+        ({  lstr_t in   = input;                                             \
+            lstr_t exp  = expected;                                          \
+            SB_1k(in_hex); SB_1k(exp_hex);                                   \
+            sb_add_hex(&in_hex, in.s, in.len);                               \
+            sb_add_hex(&exp_hex, exp.s, exp.len);                            \
+            sb_reset(&tmp);                                                  \
+            sb_reset(&out);                                                  \
+            sb_conv_from_gsm_hex(&tmp, in_hex.data, in_hex.len);             \
+            sb_conv_to_gsm_hex(&out, tmp.data, tmp.len);                     \
+            Z_ASSERT_LSTREQUAL(LSTR_SB_V(&exp_hex), LSTR_SB_V(&out), desc);  \
         })
 
         sb_init(&tmp);
         sb_init(&out);
 
-        T("\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
-          "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1c\x1d\x1e\x1f"
-          "\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f"
-          "\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f"
-          "\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f"
-          "\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f"
-          "\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f"
-          "\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f",
-          "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
-          "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1c\x1d\x1e\x1f"
-          "\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f"
-          "\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f"
-          "\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f"
-          "\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f"
-          "\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f"
-          "\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f",
-          "UTF-8 from default table GSM conversion");
+#if 0
+        /* behavior in 2012.4 */
+        TL(LSTR_IMMED("\x80\x20\x81\x20\x82"),
+           LSTR_IMMED("\x2e\x20\x2e\x20\x2e"),
+           "conversion with invalid characters");
+#endif
+
+        for (int i = 0; i < default_tab.len; i++) {
+            TL(LSTR_INIT_V(default_tab.s, i),
+               LSTR_INIT_V(default_tab.s, i),
+               "test default table with various lengths");
+            TLHEX(LSTR_INIT_V(default_tab.s, i),
+                  LSTR_INIT_V(default_tab.s, i),
+                  "test default table with various lengths (hex)");
+        }
+        for (int i = 0; i < extended_tab.len; i += 2) {
+            TL(LSTR_INIT_V(extended_tab.s, i),
+               LSTR_INIT_V(extended_tab.s, i),
+               "test extension table with various lengths");
+            TLHEX(LSTR_INIT_V(extended_tab.s, i),
+                  LSTR_INIT_V(extended_tab.s, i),
+                  "test extension table with various lengths (hex)");
+        }
+
+        {
+            lstr_t str = LSTR_IMMED(
+                "coucou random:\"jk6q?#hU*1/m.VVteU[i4S|\\\"@>'wrTFuV[Csrvi<^|%/1>|"
+                "'9kpfG76aY5)gWN!+1D8aj-j|)'3'\"ZO:F#XL7n2=DpIEtU5%H8UICK.F\"&2HBOi6ZLZ[|ptN-z");
+            sb_wipe(&tmp);
+            sb_reset(&out);
+            sb_conv_to_gsm_hex(&tmp, str.s, str.len);
+            sb_conv_from_gsm_hex(&out, tmp.data, tmp.len);
+            Z_ASSERT_LSTREQUAL(str, LSTR_SB_V(&out), "emi teaser crash");
+        }
 
         sb_wipe(&tmp);
         sb_wipe(&out);
 
-#undef T
+#undef TL
+#undef TLHEX
     } Z_TEST_END
 } Z_GROUP_END
 
