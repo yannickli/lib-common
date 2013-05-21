7c5c52fc26e27a9f05407b299c2ebc07ef1d249f
Romain Le Godais <romain.legodais@intersec.com>
2013-05-21 14:28:42 +0200

    iop: Simplify a bit class packing/unpacking code.
    Since I0121d81 (in tools:iopc), the size of field of type class is
    sizeof(void *), so we don't need all these tests anymore.
    
    Refs: #14357
    Change-Id: Iada2a277579c23af022d49ac5383577c20834a29

--- Build/configure.inc.sh
+++ Build/configure.inc.sh
@@ -98,7 +98,7 @@ ruby_var() {
 }
 
 check_iopc() {
-    IOPC_VER=2.9.0
+    IOPC_VER=2.9.3
     if ! prereq "$IOPC_VER" "$(iopc --version)"; then
         warn "iopc version $IOPC_VER required, update your tools"
     fi
