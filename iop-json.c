yannickli/lib-common

commit 4bdf220981aff06907cb9bd261b83584ead7499e

iop: Add json junpack_ptr* functions.
Implementation still needs to be done, but here are the declaration of the
json unpack functions that will be used to unpack classes.

Also add helpers in IOP_GENERIC/IOP_CLASS.

Refs: #14737
Change-Id: Iad52259e94eec48c9ce3b88704b3a35580b5bed2

iop-json.c
@@ -1340,6 +1340,12 @@ static int unpack_struct(iop_json_lex_t *ll, const iop_struct_t *desc,
     return 0;
 }
 
+/* If "desc" is a structure or a union, "value" is a pointer on the structure
+ * to fill.
+ * If "desc" is a class, "value" is a double-pointer on the structure to fill.
+ * It will be (re)allocated when the size of the real class to unpack will be
+ * known.
+ */
 int iop_junpack(iop_json_lex_t *ll, const iop_struct_t *desc, void *value,
                 bool single_value)
 {
@@ -1404,49 +1410,76 @@ int iop_junpack(iop_json_lex_t *ll, const iop_struct_t *desc, void *value,
     }
 }
 
-int t_iop_junpack_ps(pstream_t *ps, const iop_struct_t *desc, void *v,
-                     int flags, sb_t *errb)
+int iop_junpack_ptr(iop_json_lex_t *ll, const iop_struct_t *st, void **out,
+                    bool single_value)
 {
-    iop_json_lex_t  jll;
-    int res;
-
-    iop_jlex_init(t_pool(), &jll);
-    iop_jlex_attach(&jll, ps);
-    jll.flags = flags;
-
-    if ((res = iop_junpack(&jll, desc, v, true)) < 0) {
-        if (errb) {
-            iop_jlex_write_error(&jll, errb);
-        }
+    if (iop_struct_is_class(st)) {
+        /* "out" will be (re)allocated after, when the real packed class type
+         * will be known. */
+        return iop_junpack(ll, st, out, single_value);
     }
 
-    iop_jlex_wipe(&jll);
-
-    return res;
-}
-
-int t_iop_junpack_file(const char *filename, const iop_struct_t *st,
-                       void *out, int flags, sb_t *errb)
-{
-    int res;
-    pstream_t ps;
-    lstr_t file = LSTR_NULL_V;
-
-    if (lstr_init_from_file(&file, filename, PROT_READ, MAP_SHARED) < 0) {
-        if (errb) {
-            sb_addf(errb, "cannot read file %s: %m", filename);
-        }
-        res = IOP_JERR_INVALID_FILE;
-        goto end;
+    if (*out) {
+        *out = ll->mp->realloc(ll->mp, *out, 0, st->size, MEM_RAW);
+    } else {
+        *out = ll->mp->malloc(ll->mp, st->size, MEM_RAW);
     }
-
-    ps = ps_initlstr(&file);
-    res = t_iop_junpack_ps(&ps, st, out, flags, errb);
-
- end:
-    lstr_wipe(&file);
-    return res;
+    return iop_junpack(ll, st, *out, single_value);
 }
+
+#define CREATE_JUNPACK_PS(_fun, _data_type, _fun_to_call)  \
+int _fun(pstream_t *ps, const iop_struct_t *desc, _data_type v,              \
+         int flags, sb_t *errb)                                              \
+{                                                                            \
+    iop_json_lex_t  jll;                                                     \
+    int res;                                                                 \
+                                                                             \
+    iop_jlex_init(t_pool(), &jll);                                           \
+    iop_jlex_attach(&jll, ps);                                               \
+    jll.flags = flags;                                                       \
+                                                                             \
+    if ((res = iop_junpack(&jll, desc, v, true)) < 0) {                      \
+        if (errb) {                                                          \
+            iop_jlex_write_error(&jll, errb);                                \
+        }                                                                    \
+    }                                                                        \
+                                                                             \
+    iop_jlex_wipe(&jll);                                                     \
+                                                                             \
+    return res;                                                              \
+}
+
+CREATE_JUNPACK_PS(t_iop_junpack_ps,     void *,  iop_junpack)
+CREATE_JUNPACK_PS(t_iop_junpack_ptr_ps, void **, iop_junpack_ptr)
+#undef CREATE_JUNPACK_PS
+
+#define CREATE_JUNPACK_FILE(_fun, _data_type, _fun_to_call)  \
+int _fun(const char *filename, const iop_struct_t *st, _data_type out,       \
+         int flags, sb_t *errb)                                              \
+{                                                                            \
+    int res;                                                                 \
+    pstream_t ps;                                                            \
+    lstr_t file = LSTR_NULL_V;                                               \
+                                                                             \
+    if (lstr_init_from_file(&file, filename, PROT_READ, MAP_SHARED) < 0) {   \
+        if (errb) {                                                          \
+            sb_addf(errb, "cannot read file %s: %m", filename);              \
+        }                                                                    \
+        res = IOP_JERR_INVALID_FILE;                                         \
+        goto end;                                                            \
+    }                                                                        \
+                                                                             \
+    ps = ps_initlstr(&file);                                                 \
+    res = _fun_to_call(&ps, st, out, flags, errb);                           \
+                                                                             \
+ end:                                                                        \
+    lstr_wipe(&file);                                                        \
+    return res;                                                              \
+}
+
+CREATE_JUNPACK_FILE(t_iop_junpack_file,     void *,  t_iop_junpack_ps)
+CREATE_JUNPACK_FILE(t_iop_junpack_ptr_file, void **, t_iop_junpack_ptr_ps)
+#undef CREATE_JUNPACK_FILE
 
 /*-}}}-*/
 /* {{{ jpack */

iop-json.h
@@ -160,6 +160,9 @@ static inline void iop_jlex_set_flags(iop_json_lex_t *ll, int flags)
  * If you want to set some special flags, you have to set it using
  * iop_jlex_set_flags() before calling this this function.
  *
+ * This function cannot be used to unpack a class; use `iop_junpack_ptr`
+ * instead.
+ *
  * Prefer the generated version instead of this low-level API (see IOP_GENERIC
  * in iop-macros.h).
  *
@@ -182,6 +185,22 @@ __must_check__
 int iop_junpack(iop_json_lex_t *ll, const iop_struct_t *st, void *out,
                 bool single_value);
 
+/** Convert IOP-JSon to an IOP C structure.
+ *
+ * This function acts exactly as `iop_junpack` but allocates (or reallocates)
+ * the destination structure.
+ *
+ * This function MUST be used to unpack a class instead of `iop_junpack`,
+ * because the size of a class is not known before unpacking it (this could be
+ * a child).
+ *
+ * Prefer the generated version instead of this low-level API
+ * (see IOP_GENERIC/IOP_CLASS in iop-macros.h).
+ */
+__must_check__
+int iop_junpack_ptr(iop_json_lex_t *ll, const iop_struct_t *st, void **out,
+                    bool single_value);
+
 /** Convert IOP-JSon to an IOP C structure using the t_pool().
  *
  * This function allow to unpack an IOP structure encoded in JSon format in
@@ -195,6 +214,9 @@ int iop_junpack(iop_json_lex_t *ll, const iop_struct_t *st, void *out,
  * </code>
  *
  * Note that the parameter `single_value` is set to true.
+ *
+ * This function cannot be used to unpack a class; use `t_iop_junpack_ptr_ps`
+ * instead.
  *
  * Prefer the generated version instead of this low-level API (see IOP_GENERIC
  * in iop-macros.h).
@@ -212,10 +234,29 @@ __must_check__
 int t_iop_junpack_ps(pstream_t *ps, const iop_struct_t *st, void *out,
                      int flags, sb_t *errb);
 
+/** Convert IOP-JSon to an IOP C structure using the t_pool().
+ *
+ * This function acts exactly as `t_iop_junpack_ps` but allocates
+ * (or reallocates) the destination structure.
+ *
+ * This function MUST be used to unpack a class instead of `t_iop_junpack_ps`,
+ * because the size of a class is not known before unpacking it (this could be
+ * a child).
+ *
+ * Prefer the generated version instead of this low-level API
+ * (see IOP_GENERIC/IOP_CLASS in iop-macros.h).
+ */
+__must_check__
+int t_iop_junpack_ptr_ps(pstream_t *ps, const iop_struct_t *st, void **out,
+                         int flags, sb_t *errb);
+
 /** Convert an IOP-JSon structure contained in a file to an IOP C structure.
  *
  * This function read a file containing an IOP-JSon structure and set the
  * fields in an IOP C structure.
+ *
+ * This function cannot be used to unpack a class; use `t_iop_junpack_ptr_file`
+ * instead.
  *
  * Prefer the generated version instead of this low-level API (see IOP_GENERIC
  * in iop-macros.h).
@@ -233,6 +274,22 @@ int t_iop_junpack_ps(pstream_t *ps, const iop_struct_t *st, void *out,
 __must_check__
 int t_iop_junpack_file(const char *filename, const iop_struct_t *st,
                        void *out, int flags, sb_t *errb);
+
+/** Convert an IOP-JSon structure contained in a file to an IOP C structure.
+ *
+ * This function acts exactly as `t_iop_junpack_file` but allocates
+ * (or reallocates) the destination structure.
+ *
+ * This function MUST be used to unpack a class instead of
+ * `t_iop_junpack_file`, because the size of a class is not known before
+ * unpacking it (this could be a child).
+ *
+ * Prefer the generated version instead of this low-level API
+ * (see IOP_GENERIC/IOP_CLASS in iop-macros.h).
+ */
+__must_check__
+int t_iop_junpack_ptr_file(const char *filename, const iop_struct_t *st,
+                           void **out, int flags, sb_t *errb);
 
 /** Print a textual error after iop_junpack() failure.
  *

iop-macros.h
@@ -453,6 +453,28 @@
         return iop_sb_jpack(sb, &pfx##__s, v, flags);                        \
     }
 
+#define IOP_GENERIC_JSON_UNPACK(pfx)  \
+    __must_check__                                                           \
+    static inline int pfx##__junpack_ptr(iop_json_lex_t *ll, pfx##__t **v,   \
+                                         bool single)                        \
+    {                                                                        \
+        return iop_junpack_ptr(ll, &pfx##__s, (void **)v, single);           \
+    }                                                                        \
+    __must_check__                                                           \
+    static inline int t_##pfx##__junpack_ptr_ps(pstream_t *ps, pfx##__t **v, \
+                                                int flags, sb_t *errb)       \
+    {                                                                        \
+        return t_iop_junpack_ptr_ps(ps, &pfx##__s, (void **)v, flags, errb); \
+    }                                                                        \
+    __must_check__                                                           \
+    static inline int t_##pfx##__junpack_ptr_file(const char *filename,      \
+                                                  pfx##__t **v, int flags,   \
+                                                  sb_t *errb)                \
+    {                                                                        \
+        return t_iop_junpack_ptr_file(filename, &pfx##__s, (void **)v,       \
+                                      flags, errb);                          \
+    }
+
 #define IOP_GENERIC_JSON_UNPACK_STRUCT_UNION(pfx)  \
     __must_check__                                                           \
     static inline int pfx##__junpack(iop_json_lex_t *ll, pfx##__t *v,        \
@@ -529,6 +551,7 @@
     IOP_GENERIC_BINARY_UNPACK_STRUCT_UNION(pfx)                              \
     /* ---- JSon ---- */                                                     \
     IOP_GENERIC_JSON_PACK(pfx)                                               \
+    IOP_GENERIC_JSON_UNPACK(pfx)                                             \
     IOP_GENERIC_JSON_UNPACK_STRUCT_UNION(pfx)                                \
     /* ---- XML ---- */                                                      \
     IOP_GENERIC_XML_PACK(pfx)                                                \
@@ -544,6 +567,7 @@
     IOP_GENERIC_BINARY_UNPACK(pfx)                                           \
     /* ---- JSon ---- */                                                     \
     IOP_GENERIC_JSON_PACK(pfx)                                               \
+    IOP_GENERIC_JSON_UNPACK(pfx)                                             \
     /* ---- XML ---- */                                                      \
     IOP_GENERIC_XML_PACK(pfx)
 

commit e1beba870f5bcac135d4a4bf442db5c862084475

iop: Fix t_iop_junpack_ptr_ps.
This fixes a stupid bug introduced in 4bdf22098: iop_junpack was called
instead of iop_junpack_ptr in t_iop_junpack_ptr_ps.

Refs: #14737
Change-Id: Ibd4a2771c7393b9043a947414a3ab0f32d684f8a

iop-json.c
@@ -1608,7 +1608,7 @@ int _fun(pstream_t *ps, const iop_struct_t *desc, _data_type v,              \
     iop_jlex_attach(&jll, ps);                                               \
     jll.flags = flags;                                                       \
                                                                              \
-    if ((res = iop_junpack(&jll, desc, v, true)) < 0) {                      \
+    if ((res = _fun_to_call(&jll, desc, v, true)) < 0) {                     \
         if (errb) {                                                          \
             iop_jlex_write_error(&jll, errb);                                \
         }                                                                    \