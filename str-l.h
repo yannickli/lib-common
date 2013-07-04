yannickli/lib-common

commit 8d7dbe5853c7fa309b22d00bab3e2a3a820f2483

lstr_t: Support negative values for string length.
Many times, we read this kind of code: lstr_dups(str, strlen(str)).
This is boring to write, so support negative values for len. In that case,
it will automatically use strlen(str).

Change-Id: I8b4dd0f692a39e01b85063e5bed2b7f2157646b6

str-l.h
@@ -286,6 +286,8 @@ static inline lstr_t lstr_dups(const char *s, int len)
 {
     if (!s)
         return LSTR_NULL_V;
+    if (len < 0)
+        len = strlen(s);
     return lstr_init_(p_dupz(s, len), len, MEM_LIBC);
 }
 
@@ -318,6 +320,8 @@ static inline lstr_t mp_lstr_dups(mem_pool_t *mp, const char *s, int len)
 {
     if (!s)
         return LSTR_NULL_V;
+    if (len < 0)
+        len = strlen(s);
     return lstr_init_(mp_dupz(mp, s, len), len, MEM_OTHER);
 }
 
@@ -354,6 +358,8 @@ static inline void lstr_copyc(lstr_t *dst, const lstr_t s)
 static inline void lstr_copys(lstr_t *dst, const char *s, int len)
 {
     if (s) {
+        if (len < 0)
+            len = strlen(s);
         lstr_copy_(NULL, dst, p_dupz(s, len), len, MEM_LIBC);
     } else {
         lstr_copy_(NULL, dst, NULL, 0, MEM_STATIC);
@@ -377,6 +383,8 @@ static inline
 void mp_lstr_copys(mem_pool_t *mp, lstr_t *dst, const char *s, int len)
 {
     if (s) {
+        if (len < 0)
+            len = strlen(s);
         lstr_copy_(mp, dst, mp_dupz(mp, s, len), len, MEM_OTHER);
     } else {
         lstr_copy_(NULL, dst, NULL, 0, MEM_STATIC);
@@ -411,6 +419,8 @@ static inline lstr_t t_lstr_dups(const void *s, int len)
 {
     if (!s)
         return LSTR_NULL_V;
+    if (len < 0)
+        len = strlen(s);
     return lstr_init_(t_dupz(s, len), len, MEM_STACK);
 }
 
@@ -419,6 +429,8 @@ static inline lstr_t t_lstr_dups(const void *s, int len)
 static inline void t_lstr_copys(lstr_t *dst, const void *s, int len)
 {
     if (s) {
+        if (len < 0)
+            len = strlen(s);
         lstr_copy_(NULL, dst, t_dupz(s, len), len, MEM_STACK);
     } else {
         lstr_copy_(NULL, dst, NULL, 0, MEM_STATIC);

commit 468db8260f105519c97dd215370018149715ba52

str-l: Fix mmsx c++ files compil; change prototypes.
mmsx compilation of c++ files was broken by 8d7dbe5853c, because the
compiler was looking for a function "strlen(const void*)" which does not
exist.

To fix that, change the prototype of these functions, that were broken
anyway.  We are now consistent with lstr_dups, mp_lstr_dups, lstr_copys
and mp_lstr_copys.

Note that a small patch will be needed in qts to make it compile with this
patch.

Change-Id: Idd132da53d905aa6d868bca1f64e461d21874441

str-l.h
@@ -415,7 +415,7 @@ static inline lstr_t t_lstr_dup(const lstr_t s)
 
 /** \brief returns a duplicated lstr from the mem stack.
  */
-static inline lstr_t t_lstr_dups(const void *s, int len)
+static inline lstr_t t_lstr_dups(const char *s, int len)
 {
     if (!s)
         return LSTR_NULL_V;
@@ -426,7 +426,7 @@ static inline lstr_t t_lstr_dups(const void *s, int len)
 
 /** \brief sets \v dst to a mem stack allocated copy of its arguments.
  */
-static inline void t_lstr_copys(lstr_t *dst, const void *s, int len)
+static inline void t_lstr_copys(lstr_t *dst, const char *s, int len)
 {
     if (s) {
         if (len < 0)