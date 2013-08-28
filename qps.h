yannickli/lib-common

commit ce21fcdf7ad003e96a913e42e4ca59fe0f6ddbac

mem: add new "mem_tool" helpers and get rid of core-mem-valgrind.
The new mem_tool helpers wraps the call to valgrind and Asan helpers
allowing easy instrumentation of both in the same way. Existing code has
been migrated in order to use the new directives instead of valgrind's
specific ones.

Change-Id: I87fc44b461bff68edd7df3ce01f29f737955f95b
Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

qps.blk
@@ -14,10 +14,6 @@
 #include <sys/wait.h>
 #include <sys/resource.h>
 #include <zlib.h>
-#include <lib-common/core-mem-valgrind.h>
-#ifndef VALGRIND_MAKE_MEM_DEFINED_IF_ADDRESSABLE
-#  define VALGRIND_MAKE_MEM_DEFINED_IF_ADDRESSABLE(...) ((void)0)
-#endif
 #include <lib-common/unix.h>
 #include <lib-common/qlzo.h>
 #include <lib-common/el.h>
@@ -60,14 +56,18 @@ static struct {
         }                                                                    \
     } while (0)
 
-#ifndef NDEBUG
+#if !defined(NDEBUG) && !defined(__has_asan)
 # define VG_DO(what, map, pg, n) \
     ({  qps_pg_t _pg = (pg); size_t _n = (n);                              \
         uint8_t *_start = map[_pg & 0xffff].data;                          \
         uint8_t *_end   = _start + _n * QPS_PAGE_SIZE;                     \
         assert (map);                                                      \
         assert (_start);                                                   \
-        VALGRIND_MAKE_MEM_##what(_start, _end - _start);                   \
+        if (what < 0) {                                                    \
+            mem_tool_disallow_memory(_start, _end - _start);               \
+        } else {                                                           \
+            mem_tool_allow_memory(_start, _end - _start, what);            \
+        }                                                                  \
     })
 #define VG_DO_PG(what, qps, pg, n) \
     ({  qps_pg_t __pg = (pg);                                              \
@@ -78,13 +78,13 @@ static struct {
 # define VG_DO_PG(...)  ((void)0)
 #endif
 
-#define PG_HIDE(qps, pg, n)    VG_DO_PG(NOACCESS, qps, pg, n)
-#define PG_UNDEF(qps, pg, n)   VG_DO_PG(UNDEFINED, qps, pg, n)
-#define PG_DEF(qps, pg, n)     VG_DO_PG(DEFINED, qps, pg, n)
+#define PG_HIDE(qps, pg, n)    VG_DO_PG(-1, qps, pg, n)
+#define PG_UNDEF(qps, pg, n)   VG_DO_PG(0, qps, pg, n)
+#define PG_DEF(qps, pg, n)     VG_DO_PG(1, qps, pg, n)
 
-#define MAP_HIDE(map, pg, n)    VG_DO(NOACCESS, map, pg, n)
-#define MAP_UNDEF(map, pg, n)   VG_DO(UNDEFINED, map, pg, n)
-#define MAP_DEF(map, pg, n)     VG_DO(DEFINED, map, pg, n)
+#define MAP_HIDE(map, pg, n)    VG_DO(-1, map, pg, n)
+#define MAP_UNDEF(map, pg, n)   VG_DO(0, map, pg, n)
+#define MAP_DEF(map, pg, n)     VG_DO(1, map, pg, n)
 
 
 /**
@@ -1728,7 +1728,7 @@ static ALWAYS_INLINE void qps_zero_helper(uint8_t *p, uint8_t *end)
 {
     uint8_t *q = p;
 
-    VALGRIND_MAKE_MEM_DEFINED_IF_ADDRESSABLE(p, end - p);
+    mem_tool_allow_memory_if_addressable(p, end - p, true);
     if (end - p >= 64) {
         while ((uintptr_t)q & 7) {
             if (*q++)
@@ -1991,8 +1991,8 @@ static void qps_free_int(qps_t *qps, void *ptr)
     blk = container_of(ptr, qps_mhdr_t, data);
     bsz = qps_m_blk_size(blk);
 #if QPS_USE_REDZONES
-    VALGRIND_FREELIKE_BLOCK(ptr, 8);
-    VALGRIND_MAKE_MEM_DEFINED(blk, QPS_MBLK_HDRSZ + bsz);
+    mem_tool_freelike(ptr, bsz, 8);
+    mem_tool_allow_memory(blk, QPS_MBLK_HDRSZ + bsz, true);
 #endif
 
     if (!qps_is_ro(qps, map)) {
@@ -2066,7 +2066,7 @@ static void *qps_alloc_int(qps_t *qps, uint32_t id, size_t asked)
     qps_map_of(blk)->hdr.allocated += size + QPS_MBLK_HDRSZ;
     *qps_handle_slot(qps, id) = res = qps_encode(blk->data);
 #if QPS_USE_REDZONES
-    VALGRIND_MALLOCLIKE_BLOCK(blk->data, asked, 8, 0);
+    mem_tool_malloclike(blk->data, asked, 8, false);
 #endif
     return blk->data;
 }
@@ -2112,10 +2112,10 @@ static void *qps_realloc_int(qps_t *qps, uint32_t id, void *ptr, size_t asked)
                 goto split;
             }
 #if QPS_USE_REDZONES
-            VALGRIND_FREELIKE_BLOCK(blk->data, 8);
-            VALGRIND_MAKE_MEM_DEFINED(blk, QPS_MBLK_HDRSZ + osz);
-            VALGRIND_MALLOCLIKE_BLOCK(blk->data, asked, 8, 0);
-            VALGRIND_MAKE_MEM_DEFINED(blk->data, asked);
+            mem_tool_freelike(blk->data, osz, 8);
+            mem_tool_allow_memory(blk, QPS_MBLK_HDRSZ + osz, true);
+            mem_tool_malloclike(blk->data, asked, 8, false);
+            mem_tool_allow_memory(blk, asked, true);
 #endif
             return blk->data;
         }
@@ -2142,9 +2142,9 @@ static void *qps_realloc_int(qps_t *qps, uint32_t id, void *ptr, size_t asked)
         blk->flags += nsz;
         qps_map_of(blk)->hdr.allocated += nsz - osz;
 #if QPS_USE_REDZONES
-        VALGRIND_FREELIKE_BLOCK(blk->data, 8);
-        VALGRIND_MALLOCLIKE_BLOCK(blk->data, asked, 8, 0);
-        VALGRIND_MAKE_MEM_DEFINED(blk->data, osz);
+        mem_tool_freelike(blk->data, osz, 8);
+        mem_tool_malloclike(blk->data, asked, 8, false);
+        mem_tool_allow_memory(blk->data, osz, true);
 #endif
         return blk->data;
     }

arith-bithacks.c
@@ -13,7 +13,6 @@
 
 #include "z.h"
 #include "arith.h"
-#include "core-mem-valgrind.h"
 
 #define BIT(x, n)   (((x) >> (n)) & 1)
 #define BC4(x)      BIT(x, 0) + BIT(x, 1) + BIT(x, 2) + BIT(x, 3)
@@ -31,7 +30,7 @@ MAKE_U64_ACCESSIBLE(const uint64_t *pos, size_t start_bit, size_t len)
     size_t vpos = 1 + ((start_bit + len) / 8);
 
     if (vpos < 8) {
-        VALGRIND_MAKE_MEM_DEFINED((const byte *)pos + vpos, 8 - vpos);
+        mem_tool_allow_memory((const byte *)pos + vpos, 8 - vpos, true);
     }
 }
 #else

core-mem-fifo.c
@@ -11,7 +11,6 @@
 /*                                                                        */
 /**************************************************************************/
 
-#include "core-mem-valgrind.h"
 #include "core.h"
 
 typedef struct mem_page_t {
@@ -45,16 +44,6 @@ typedef struct mem_fifo_pool_t {
     mem_pool_t **owner;
 } mem_fifo_pool_t;
 
-
-static ALWAYS_INLINE void blk_unprotect(mem_block_t *blk)
-{
-    (void)VALGRIND_MAKE_MEM_DEFINED(blk, sizeof(*blk));
-}
-
-static ALWAYS_INLINE void blk_protect(mem_block_t *blk)
-{
-    (void)VALGRIND_MAKE_MEM_NOACCESS(blk, sizeof(*blk));
-}
 
 static ALWAYS_INLINE mem_page_t *pageof(mem_block_t *blk)
 {
@@ -92,9 +81,9 @@ static mem_page_t *mem_page_new(mem_fifo_pool_t *mfp, uint32_t minsize)
 
 static void mem_page_reset(mem_page_t *page)
 {
-    VALGRIND_MAKE_MEM_UNDEFINED(page->area, page->used_size);
+    mem_tool_allow_memory(page->area, page->used_size, false);
     p_clear(page->area, page->used_size);
-    VALGRIND_MAKE_MEM_NOACCESS(page->start, page->size);
+    mem_tool_disallow_memory(page->area, page->size);
 
     page->used_size   = 0;
     page->used_blocks = 0;
@@ -108,6 +97,7 @@ static void mem_page_delete(mem_fifo_pool_t *mfp, mem_page_t **pagep)
     if (page) {
         mfp->nb_pages--;
         mfp->map_size -= page->size;
+        mem_tool_allow_memory(page, page->size + sizeof(mem_page_t), false);
         munmap(page, page->size + sizeof(mem_page_t));
     }
     *pagep = NULL;
@@ -118,18 +108,12 @@ static uint32_t mem_page_size_left(mem_page_t *page)
     return (page->size - page->used_size);
 }
 
-#if __GNUC_PREREQ(4, 6) && !__VALGRIND_PREREQ(3, 7)
-#  pragma GCC diagnostic push
-#  pragma GCC diagnostic ignored "-Wunused-but-set-variable"
-#endif
 static void *mfp_alloc(mem_pool_t *_mfp, size_t size, mem_flags_t flags)
 {
     mem_fifo_pool_t *mfp = container_of(_mfp, mem_fifo_pool_t, funcs);
     mem_block_t *blk;
     mem_page_t *page;
-#ifndef NDEBUG
     size_t req_size = size;
-#endif
 
     /* Must round size up to keep proper alignment */
     size = ROUND_UP((unsigned)size + sizeof(mem_block_t), 8);
@@ -143,11 +127,11 @@ static void *mfp_alloc(mem_pool_t *_mfp, size_t size, mem_flags_t flags)
     }
 
     blk = (mem_block_t *)(page->area + page->used_size);
-    blk_unprotect(blk);
-    VALGRIND_MALLOCLIKE_BLOCK(blk->area, req_size, 0, true);
+    mem_tool_allow_memory(blk, sizeof(*blk), true);
+    mem_tool_malloclike(blk->area, req_size, 0, true);
     blk->page_offs = (uintptr_t)blk - (uintptr_t)page;
     blk->blk_size  = size;
-    blk_protect(blk);
+    mem_tool_disallow_memory(blk, sizeof(*blk));
 
     mfp->occupied   += size;
     page->used_size += size;
@@ -165,11 +149,11 @@ static void mfp_free(mem_pool_t *_mfp, void *mem, mem_flags_t flags)
         return;
 
     blk  = container_of(mem, mem_block_t, area);
-    blk_unprotect(blk);
+    mem_tool_allow_memory(blk, sizeof(*blk), true);
     page = pageof(blk);
     mfp->occupied -= blk->blk_size;
-    VALGRIND_FREELIKE_BLOCK(mem, 0);
-    blk_protect(blk);
+    mem_tool_freelike(mem, blk->blk_size, 0);
+    mem_tool_disallow_memory(blk, sizeof(*blk));
 
     if (--page->used_blocks > 0)
         return;
@@ -217,7 +201,7 @@ static void *mfp_realloc(mem_pool_t *_mfp, void *mem, size_t oldsize, size_t siz
         return mfp_alloc(_mfp, size, flags);
 
     blk  = container_of(mem, mem_block_t, area);
-    blk_unprotect(blk);
+    mem_tool_allow_memory(blk, sizeof(*blk), true);
     page = pageof(blk);
 
     alloced_size = blk->blk_size - sizeof(*blk);
@@ -225,9 +209,9 @@ static void *mfp_realloc(mem_pool_t *_mfp, void *mem, size_t oldsize, size_t siz
         oldsize = alloced_size;
     assert (oldsize <= alloced_size);
     if (req_size <= alloced_size) {
-        VALGRIND_FREELIKE_BLOCK(mem, 0);
-        VALGRIND_MALLOCLIKE_BLOCK(mem, req_size, 0, false);
-        VALGRIND_MAKE_MEM_DEFINED(mem, MIN(req_size, oldsize));
+        mem_tool_freelike(mem, oldsize, 0);
+        mem_tool_malloclike(mem, req_size, 0, false);
+        mem_tool_allow_memory(mem, MIN(req_size, oldsize), true);
         if (!(flags & MEM_RAW) && oldsize < req_size)
             memset(blk->area + oldsize, 0, req_size - oldsize);
     } else
@@ -243,9 +227,9 @@ static void *mfp_realloc(mem_pool_t *_mfp, void *mem, size_t oldsize, size_t siz
 
         mfp->occupied   += diff;
         page->used_size += diff;
-        VALGRIND_FREELIKE_BLOCK(mem, 0);
-        VALGRIND_MALLOCLIKE_BLOCK(mem, req_size, 0, false);
-        VALGRIND_MAKE_MEM_DEFINED(mem, MIN(req_size, oldsize));
+        mem_tool_freelike(mem, oldsize, 0);
+        mem_tool_malloclike(mem, req_size, 0, false);
+        mem_tool_allow_memory(mem, MIN(req_size, oldsize), true);
     } else {
         void *old = mem;
 
@@ -255,12 +239,9 @@ static void *mfp_realloc(mem_pool_t *_mfp, void *mem, size_t oldsize, size_t siz
         return mem;
     }
 
-    blk_protect(blk);
+    mem_tool_disallow_memory(blk, sizeof(*blk));
     return mem;
 }
-#if __GNUC_PREREQ(4, 6) && !__VALGRIND_PREREQ(3, 7)
-#  pragma GCC diagnostic pop
-#endif
 
 static mem_pool_t const mem_fifo_pool_funcs = {
     .malloc  = &mfp_alloc,

core-mem-ring.c
@@ -11,7 +11,6 @@
 /*                                                                        */
 /**************************************************************************/
 
-#include "core-mem-valgrind.h"
 #include "arith.h"
 #include "container.h"
 #include "thr.h"
@@ -180,7 +179,7 @@ static void *rp_reserve(ring_pool_t *rp, size_t size, ring_blk_t **blkp)
     } else {
         *blkp = rp->cblk;
     }
-    (void)VALGRIND_MAKE_MEM_UNDEFINED(res, size);
+    mem_tool_allow_memory(res, size, false);
     if (unlikely(rp->alloc_sz + size < rp->alloc_sz)
     ||  unlikely(rp->alloc_nb == UINT32_MAX))
     {
@@ -222,7 +221,7 @@ static void *rp_realloc(mem_pool_t *_rp, void *mem,
         if (mem == rp->last) {
             rp->pos = (byte *)mem + size;
         }
-        (void)VALGRIND_MAKE_MEM_NOACCESS((byte *)mem + size, oldsize - size);
+        mem_tool_disallow_memory((byte *)mem + size, oldsize - size);
         return size ? mem : NULL;
     }
 
@@ -232,13 +231,13 @@ static void *rp_realloc(mem_pool_t *_rp, void *mem,
     {
         rp->pos = (byte *)rp->last + size;
         rp->alloc_sz  += size - oldsize;
-        (void)VALGRIND_MAKE_MEM_DEFINED(mem, size);
+        mem_tool_allow_memory(mem, size, true);
         res = mem;
     } else {
         res = rp_alloc(_rp, size, flags | MEM_RAW);
         if (mem != NULL) {
             memcpy(res, mem, oldsize);
-            (void)VALGRIND_MAKE_MEM_UNDEFINED(mem, oldsize);
+            mem_tool_allow_memory(mem, oldsize, false);
         }
     }
     if (!(flags & MEM_RAW))
@@ -262,11 +261,11 @@ mem_ring_protect(const ring_pool_t *rp, const ring_blk_t *blk,
     const byte *start = _start;
 
     while (!blk_contains(blk, end)) {
-        (void)VALGRIND_MAKE_MEM_NOACCESS(start, blk->area + blk->size - start);
+        mem_tool_disallow_memory(start, blk->area + blk->size - start);
         blk   = dlist_next_entry(blk, blist);
         start = blk->start;
     }
-    (void)VALGRIND_MAKE_MEM_NOACCESS(start, end - start);
+    mem_tool_disallow_memory(start, end - start);
 }
 #else
 #define mem_ring_protect(...)  ((void)0)
@@ -294,7 +293,7 @@ static ALWAYS_INLINE
 void frame_unregister(frame_t *frame)
 {
     dlist_remove(&frame->flist);
-    (void)VALGRIND_MAKE_MEM_NOACCESS(frame, sizeof(frame));
+    mem_tool_disallow_memory(frame, sizeof(frame));
 }
 
 static ALWAYS_INLINE
@@ -339,7 +338,7 @@ mem_pool_t *mem_ring_pool_new(int initialsize)
 
     /* Makes the first frame */
     blk = blk_create(rp, sizeof(frame_t));
-    (void)VALGRIND_MAKE_MEM_UNDEFINED(blk->area, sizeof(frame_t));
+    mem_tool_allow_memory(blk->area, sizeof(frame_t), false);
     ring_setup_frame(rp, blk, (frame_t *)blk->area);
 
     return &rp->funcs;

core-mem-stack.c
@@ -11,7 +11,6 @@
 /*                                                                        */
 /**************************************************************************/
 
-#include "core-mem-valgrind.h"
 #include "container.h"
 #include "thr.h"
 
@@ -109,13 +108,13 @@ static void *sp_reserve(mem_stack_pool_t *sp, size_t asked, size_t alignment,
         *blkp = blk;
         res   = blk->area;
         res   = (uint8_t *)ROUND_UP((uintptr_t)res, alignment);
-        (void)VALGRIND_MAKE_MEM_NOACCESS(blk->area, res - blk->area);
+        mem_tool_disallow_memory(blk->area, res - blk->area);
     } else {
         *blkp = frame->blk;
-        (void)VALGRIND_MAKE_MEM_NOACCESS(frame->pos, res - frame->pos);
+        mem_tool_disallow_memory(frame->pos, res - frame->pos);
     }
-    (void)VALGRIND_MAKE_MEM_UNDEFINED(res, asked);
-    (void)VALGRIND_MAKE_MEM_NOACCESS(res + asked, size - asked);
+    mem_tool_allow_memory(res, asked, false);
+    mem_tool_disallow_memory(res + asked, size - asked);
 
     /* compute a progressively forgotten mean of the allocation size.
      *
@@ -166,7 +165,7 @@ static void *sp_alloc_aligned(mem_pool_t *_sp, size_t size, size_t alignment,
 #ifndef NDEBUG
     res += alignment;
     ((void **)res)[-1] = sp->stack;
-    (void)VALGRIND_MAKE_MEM_NOACCESS(res - alignment, alignment);
+    mem_tool_disallow_memory(res - alignment, alignment);
 #endif
     return frame->last = res;
 }
@@ -195,10 +194,12 @@ static void *sp_realloc_aligned(mem_pool_t *_sp, void *mem,
 
     if (frame->prev & 1)
         e_panic("allocation performed on a sealed stack");
-    (void)VALGRIND_MAKE_MEM_DEFINED((byte *)mem - sizeof(void *), sizeof(void *));
-    if (mem != NULL && unlikely(((void **)mem)[-1] != sp->stack))
-        e_panic("%p wasn't allocated in that frame, realloc is forbidden", mem);
-    (void)VALGRIND_MAKE_MEM_NOACCESS((byte *)mem - sizeof(void *), sizeof(void *));
+    if (mem != NULL) {
+        mem_tool_allow_memory((byte *)mem - sizeof(void *), sizeof(void *), true);
+        if (unlikely(((void **)mem)[-1] != sp->stack))
+            e_panic("%p wasn't allocated in that frame, realloc is forbidden", mem);
+        mem_tool_disallow_memory((byte *)mem - sizeof(void *), sizeof(void *));
+    }
     if (unlikely(oldsize == MEM_UNKNOWN))
         e_panic("stack pools do not support reallocs with unknown old size");
 #endif
@@ -207,7 +208,7 @@ static void *sp_realloc_aligned(mem_pool_t *_sp, void *mem,
         if (mem == frame->last) {
             sp->stack->pos = (uint8_t *)mem + size;
         }
-        (void)VALGRIND_MAKE_MEM_NOACCESS((uint8_t *)mem + asked, oldsize - asked);
+        mem_tool_disallow_memory((byte *)mem + asked, oldsize - asked);
         return size ? mem : NULL;
     }
 
@@ -216,16 +217,16 @@ static void *sp_realloc_aligned(mem_pool_t *_sp, void *mem,
     {
         sp->stack->pos = frame->last + size;
         sp->alloc_sz  += size - oldsize;
-        (void)VALGRIND_MAKE_MEM_UNDEFINED(mem + oldsize, asked - oldsize);
+        mem_tool_allow_memory((byte *)mem + oldsize, asked - oldsize, false);
         res = mem;
     } else {
         res = sp_alloc_aligned(_sp, size, alignment, flags | MEM_RAW);
         if (mem) {
             memcpy(res, mem, oldsize);
-            (void)VALGRIND_MAKE_MEM_NOACCESS(mem, oldsize);
+            mem_tool_disallow_memory(mem, oldsize);
         }
     }
-    (void)VALGRIND_MAKE_MEM_NOACCESS(res + asked, size - asked);
+    mem_tool_disallow_memory(res + asked, size - asked);
 
     if (!(flags & MEM_RAW))
         p_clear(res + oldsize, asked - oldsize);
@@ -295,9 +296,9 @@ void mem_stack_protect(mem_stack_pool_t *sp)
     mem_stack_blk_t *blk = sp->stack->blk;
     size_t remainsz = frame_end(sp->stack) - sp->stack->pos;
 
-    (void)VALGRIND_MAKE_MEM_NOACCESS(sp->stack->pos, remainsz);
+    mem_tool_disallow_memory(sp->stack->pos, remainsz);
     dlist_for_each_entry_continue(blk, blk, &sp->blk_list, blk_list) {
-        (void)(VALGRIND_MAKE_MEM_NOACCESS(blk->start, blk->size));
+        mem_tool_disallow_memory(blk->start, blk->size);
     }
 }
 #endif

commit eb8b65b00eb1b44fd9f88b141f778d3ce269fe0c

qps: fix mem_tool helper & manage qps_realloc false report case
1) ce21fcdf7ad003e96a913e42e4ca59fe0f6ddbac introduced a regression on
VALGRIND_MAKE_MEM_DEFINED case (blk instead of blk->data).

2) When using QPS_USE_REDZONES, a rare case (triggered by unlucky QRRD 3.0)
of qps_realloc can report a false error due to rounding size. In order to
properly manage this case, we add a new 'only QPS_USE_REDZONES mode' flag in
qps_mhdr_t struct: rz_alloc_size, which store the requested allocation size,
and so can do finer memory perm flagging (especially for realloc and rounded
alloc size case).

Change-Id: I254ded341286bcbb8da2de68ed4d9d44f38f64eb
Signed-off-by: Richard Beneyton <richard.beneyton@intersec.com>

qps.blk
@@ -36,8 +36,6 @@ static struct {
 #  define TRACE_ALLOC(...)  ((void)0)
 #endif
 
-#define QPS_USE_REDZONES 0
-
 //#define QPS_CHECK_INVARIANTS
 #ifdef QPS_CHECK_INVARIANTS
 # define QPS_CHECK(expr)         assert(expr)
@@ -164,6 +162,7 @@ struct qps_pghdr_t {
 struct qps_mhdr_t {
 #if QPS_USE_REDZONES
     uint64_t rz_after_block;
+    uint64_t rz_alloc_size;
 #endif
     uint32_t flags;
     uint32_t handle;
@@ -466,6 +465,9 @@ static void qps_m_blk_set_prev_free(qps_mhdr_t *blk, qps_mhdr_t *prev)
     qps_mhdr_t **ptr = (qps_mhdr_t **)blk - 1;
 
     blk->flags |= QPS_BLK_PREV_FREE;
+#if QPS_USE_REDZONES
+    mem_tool_allow_memory(ptr, sizeof(qps_mhdr_t *), false);
+#endif
     *ptr = prev;
 }
 
@@ -478,6 +480,9 @@ static void qps_m_blk_insert(qps_t *qps, qps_mhdr_t *blk, size_t n)
 {
     uint32_t l1, l2;
 
+#if QPS_USE_REDZONES
+    mem_tool_allow_memory(blk, sizeof(qps_mhdr_t), false);
+#endif
     blk->flags = n | QPS_BLK_PREV_USED | QPS_BLK_FREE;
     qps_m_insert_search(n, &l1, &l2);
     if ((blk->free.next = qps->m.blks[l1][l2])) {
@@ -508,6 +513,9 @@ static size_t qps_m_blk_remove(qps_t *qps, qps_mhdr_t *blk)
             RST_BIT(&qps->m.l1_bitmap, l1);
     }
 
+#if QPS_USE_REDZONES
+    mem_tool_disallow_memory(blk, sizeof(qps_mhdr_t));
+#endif
     return bsz;
 }
 
@@ -525,6 +533,49 @@ static void qps_m_register_map(qps_t *qps, qps_map_t *map)
     e_blk->flags = 0 | QPS_BLK_USED;
 
     qps_m_blk_insert(qps, f_blk, (uint8_t *)e_blk - f_blk->data);
+}
+
+static void qps_m_malloclike_map(qps_t *qps, qps_map_t *map)
+{
+#if QPS_USE_REDZONES
+    void       *map_end = &map[QPS_MAP_PAGES];
+    qps_mhdr_t *blk     = (qps_mhdr_t *)&map[1];
+    qps_mhdr_t *blk_end = container_of(map_end, qps_mhdr_t, data);
+
+    blk = qps_m_blk_next(blk, QPS_MBLK_HDRSZ);
+    if (qps_is_ro(qps, map)) {
+        while (blk < blk_end) {
+            size_t      bsz = qps_m_blk_size(blk);
+            qps_mhdr_t *nxt = qps_m_blk_next(blk, bsz);
+            qps_ptr_t  *hptr, bptr;
+            void       *ptr;
+
+            if (bsz && !(blk->flags & QPS_BLK_FREE)) {
+                hptr = qps_handle_slot(qps, blk->handle);
+                bptr = qps_encode(blk->data);
+                if (hptr->pgno == bptr.pgno && hptr->addr == bptr.addr) {
+                    ptr = (uint8_t *)qps_pg_deref(qps, hptr->pgno) + hptr->addr;
+                    mem_tool_malloclike(ptr, blk->rz_alloc_size, 8, false);
+                    mem_tool_allow_memory(ptr, blk->rz_alloc_size, true);
+                }
+            }
+            blk = nxt;
+        }
+    } else {
+        while (blk < blk_end) {
+            size_t      bsz = qps_m_blk_size(blk);
+            qps_mhdr_t *nxt = qps_m_blk_next(blk, bsz);
+
+            if (bsz && !(blk->flags & QPS_BLK_FREE)) {
+                assert (blk->rz_alloc_size <= bsz);
+                mem_tool_malloclike(blk->data, blk->rz_alloc_size, 8, false);
+                e_info("malloclike(h:%d, sz:%zd)", blk->handle, bsz);
+                mem_tool_allow_memory(blk->data, blk->rz_alloc_size, true);
+            }
+            blk = nxt;
+        }
+    }
+#endif
 }
 
 static int qps_m_check_map(qps_t *qps, qps_map_t *map, bool fatal)
@@ -1363,7 +1414,7 @@ static int qps_load_meta(qps_t *qps, sb_t *out, bool ro)
     t_scope;
     struct qps_meta *meta;
     size_t meta_size;
-    qps_map_t *map;
+    qps_map_t *map = NULL;
     uint32_t *h_u32, *u32, *uend;
     uint32_t h_len;
     int ret_val = 0;
@@ -1467,6 +1518,7 @@ static int qps_load_meta(qps_t *qps, sb_t *out, bool ro)
             ret = qps_pg_check_hdrs_aux(qps, m->hdr.mapno << 16, false);
         } else {
             ret = qps_m_check_map(qps, m, false);
+            qps_m_malloclike_map(qps, map);
         }
         if (ret < 0) {
             ret_val = -1;
@@ -1782,6 +1834,20 @@ size_t qps_sizeof(qps_t *qps, const void *ptr)
     return qps_m_blk_size(container_of((void *)ptr, qps_mhdr_t, data));
 }
 
+/* only used by qps_handle_deref to tag memory */
+void qps_handle_allow_memory(qps_t *qps, qps_handle_t id, qps_ptr_t *ptr)
+{
+#if QPS_USE_REDZONES
+    void *res = (uint8_t *)qps_pg_deref(qps, ptr->pgno) + ptr->addr;
+    qps_mhdr_t *blk  = container_of(res, qps_mhdr_t, data);
+
+    assert (blk->handle == id);
+    assert (blk->rz_alloc_size);
+
+    mem_tool_allow_memory(res, blk->rz_alloc_size, true);
+#endif
+}
+
 /* }}} */
 /* public: paged allocation {{{ */
 
@@ -1991,13 +2057,15 @@ static void qps_free_int(qps_t *qps, void *ptr)
     blk = container_of(ptr, qps_mhdr_t, data);
     bsz = qps_m_blk_size(blk);
 #if QPS_USE_REDZONES
-    mem_tool_freelike(ptr, bsz, 8);
-    mem_tool_allow_memory(blk, QPS_MBLK_HDRSZ + bsz, true);
+    mem_tool_freelike(ptr, blk->rz_alloc_size, 8);
 #endif
 
     if (!qps_is_ro(qps, map)) {
         qps_mhdr_t *tmp = qps_m_blk_next(blk, bsz);
 
+#if QPS_USE_REDZONES
+        blk->rz_alloc_size = 0;
+#endif
         map->hdr.allocated -= bsz + QPS_MBLK_HDRSZ;
         if (tmp->flags & QPS_BLK_FREE) {
             bsz += qps_m_blk_remove(qps, tmp) + QPS_MBLK_HDRSZ;
@@ -2054,7 +2122,12 @@ static void *qps_alloc_int(qps_t *qps, uint32_t id, size_t asked)
     if (tmp >= sizeof(qps_mhdr_t)) {
         split = qps_m_blk_next(blk, size);
         qps_m_blk_insert(qps, split, tmp - QPS_MBLK_HDRSZ);
+        assert (qps_m_blk_get_prev(next) == split);
         qps_m_blk_set_prev_free(next, split);
+#if QPS_USE_REDZONES
+        split->rz_alloc_size = 0;
+#endif
+        assert (!(blk->flags & QPS_BLK_PREV_FREE));
         blk->flags = size;
     } else {
         next->flags &= ~QPS_BLK_PREV_FREE;
@@ -2067,6 +2140,9 @@ static void *qps_alloc_int(qps_t *qps, uint32_t id, size_t asked)
     *qps_handle_slot(qps, id) = res = qps_encode(blk->data);
 #if QPS_USE_REDZONES
     mem_tool_malloclike(blk->data, asked, 8, false);
+    mem_tool_allow_memory(blk->data, asked, false);
+    mem_tool_allow_memory(&blk->rz_alloc_size, 8, true);
+    blk->rz_alloc_size = asked;
 #endif
     return blk->data;
 }
@@ -2113,9 +2189,9 @@ static void *qps_realloc_int(qps_t *qps, uint32_t id, void *ptr, size_t asked)
             }
 #if QPS_USE_REDZONES
             mem_tool_freelike(blk->data, osz, 8);
-            mem_tool_allow_memory(blk, QPS_MBLK_HDRSZ + osz, true);
             mem_tool_malloclike(blk->data, asked, 8, false);
-            mem_tool_allow_memory(blk, asked, true);
+            mem_tool_allow_memory(blk->data, asked, true);
+            blk->rz_alloc_size = asked;
 #endif
             return blk->data;
         }
@@ -2145,6 +2221,7 @@ static void *qps_realloc_int(qps_t *qps, uint32_t id, void *ptr, size_t asked)
         mem_tool_freelike(blk->data, osz, 8);
         mem_tool_malloclike(blk->data, asked, 8, false);
         mem_tool_allow_memory(blk->data, osz, true);
+        blk->rz_alloc_size = asked;
 #endif
         return blk->data;
     }
@@ -2156,6 +2233,35 @@ static void *qps_realloc_int(qps_t *qps, uint32_t id, void *ptr, size_t asked)
         if (likely(dst)) {
             size_t sz = qps_sizeof(qps, ptr);
 
+#if QPS_USE_REDZONES
+            if (!qps_map_is_pg(map)) {
+                /* The 'asked' value of the previous alloc of ptr is lost
+                 * during alloc. Only rounded upper value have been used to
+                 * do the alloc, and should be sz if all went well. As we will
+                 * copy the entire data[] array, we might possibly read data
+                 * after 'asked' bytes, whihc is tagged as unaddressable.
+                 * Copying this extra data is fine, and we should not report
+                 * any memory error here. To avoid this cas, we have stored
+                 * this 'asked' value in the rz_alloc_size field (only in this
+                 * mode of course). Then, we can mark the trailing data
+                 * ]asked, sz] as defined, just for the memcpy call */
+                qps_mhdr_t *blko = container_of(ptr, qps_mhdr_t, data);
+                qps_mhdr_t *blkn = container_of(dst, qps_mhdr_t, data);
+
+                assert (blko->rz_alloc_size <= sz);
+                assert (blkn->rz_alloc_size == nsz);
+                if (asked != nsz) {
+                    /* reajust this size as we know original asked value */
+                    mem_tool_freelike(blkn->data, nsz, 8);
+                    mem_tool_malloclike(blkn->data, asked, 8, false);
+                    mem_tool_allow_memory(blkn->data, asked, true);
+                    blkn->rz_alloc_size = asked;
+                }
+                memcpy(dst, ptr, MIN(asked, blko->rz_alloc_size));
+                qps_free_int(qps, ptr);
+                return dst;
+            }
+#endif
             if (sz > nsz)
                 sz = nsz;
             memcpy(dst, ptr, sz);
@@ -2241,12 +2347,16 @@ void qps_free(qps_t *qps, uint32_t id)
 void *qps_w_deref_(qps_t *qps, uint32_t h, void *pptr)
 {
     void     *rptr = NULL;
-    size_t    sz   = qps_m_blk_size(container_of(pptr, qps_mhdr_t, data));
+    qps_mhdr_t *blk = container_of(pptr, qps_mhdr_t, data);
+    size_t    sz   = qps_m_blk_size(blk);
 
     qps_m_check_maps(qps);
     TRACE_ALLOC("frag", "w_deref(%p, %d, "QPS_PTR_FMT") = ...",
                 qps, h, QPS_PTR_ARG(qps_encode(pptr)));
     rptr = qps_alloc_int(qps, h, sz);
+#if QPS_USE_REDZONES
+    sz = MIN(sz, blk->rz_alloc_size);
+#endif
     memcpy(rptr, pptr, sz);
     qps_free_ro(qps_map_of(pptr), sz);
     TRACE_ALLOC("frag", "w_deref(%p, %d, "QPS_PTR_FMT") = "QPS_PTR_FMT,

qps.h
@@ -22,6 +22,11 @@
 __attribute__((noreturn))
 extern void qps_enospc(const char *what);
 
+/* define this flag to 1 to allow valgrind/asan to potentially detect incorect
+ * QPS API usage (WARNING: the QPS spool storage format is not compatible with
+ * the standard one) */
+#define QPS_USE_REDZONES 0
+
 #define qps_io_wrap(what, ...) \
     ({  typeof(what(__VA_ARGS__)) _res = what(__VA_ARGS__);  \
         if (unlikely(_res < 0))                              \
@@ -416,12 +421,17 @@ qps_ptr_t *qps_handle_slot(qps_t *qps, qps_handle_t id)
     return &qps->handles[id / QPS_HANDLES_COUNT][id % QPS_HANDLES_COUNT];
 }
 
+void qps_handle_allow_memory(qps_t *qps, qps_handle_t id, qps_ptr_t *ptr);
+
 static ALWAYS_INLINE
 void *qps_handle_deref(qps_t *qps, qps_handle_t id)
 {
     qps_ptr_t *ptr = qps_handle_slot(qps, id);
 
     assert ((ptr->addr & ~QPS_PAGE_MASK) == 0);
+#if QPS_USE_REDZONES
+    qps_handle_allow_memory(qps, id, ptr);
+#endif
     return (uint8_t *)qps_pg_deref(qps, ptr->pgno) + ptr->addr;
 }
 