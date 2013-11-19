d98fcdb19f31f760d9e8b9ca80e098ebecc37c9a
Florent Bruneau <florent.bruneau@intersec.com>
2013-11-19 14:31:48 +0100

    Update generated IOP for iopc 2.9.15.
    Change-Id: Ia9073c2d2ab3e085aa8c3de6ea1b27315a358218
    Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

--- ic-t.iop.h
+++ ic-t.iop.h
@@ -2,23 +2,23 @@
 #ifndef IOP_HEADER_GUARD_ic_TYPES_H
 #define IOP_HEADER_GUARD_ic_TYPES_H
 
-#include <lib-common/iop.h>
+#include "ic-tdef.iop.h"
 
-typedef struct ic__simple_hdr__t {
+struct ic__simple_hdr__t {
     lstr_t           login;
     lstr_t           password;
     lstr_t           kind;
     int32_t  payload;
-} ic__simple_hdr__t;
+};
 extern iop_struct_t const ic__simple_hdr__s;
 IOP_GENERIC(ic__simple_hdr);
 
-typedef struct ic__hdr__t {
+struct ic__hdr__t {
     uint16_t iop_tag;
     union {
         struct ic__simple_hdr__t simple;
     };
-} ic__hdr__t;
+};
 extern iop_struct_t const ic__hdr__s;
 IOP_GENERIC(ic__hdr);
 /*----- XXX private data, do not use directly -{{{-*/
