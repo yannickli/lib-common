db724cc9228616e38aaf96b53d403ad5987def1a
Florent Bruneau <florent.bruneau@intersec.com>
2014-02-16 12:23:18 +0100

    Support build in both platform and mmsx.
    Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

--- Config/rules.mk
+++ Config/rules.mk
@@ -69,14 +69,14 @@ ext/gen/iop = $(call fun/patsubst-filt,%.iop,%.iop.c,$1) \
 define ext/expand/iop
 $3.h: $3.c
 $3.json: $3.c
-$~$3.dep: IOPINCPATH_=$l $/lib-inet $/qrrd/iop $($(1DV)_IOPINCPATH) $($1_IOPINCPATH) $($3_IOPINCPATH)
+$~$3.dep: IOPINCPATH_=$l $(var/platform)/lib-inet $(var/platform)/qrrd/iop $($(1DV)_IOPINCPATH) $($1_IOPINCPATH) $($3_IOPINCPATH)
 $~$3.dep: IOPCLASSRANGE_=$(firstword $($(1DV)_IOPCLASSRANGE) $($1_IOPCLASSRANGE) $($3_IOPCLASSRANGE))
 $~$3.dep: $3 $(IOPC)
 	$(msg/COMPILE.iop) $3
 	$(RM) $$@
 	$(IOPC) -3 --c-resolve-includes --Wextra -l c,json -d$~$$<.dep -I$$(call fun/join,:,$$(IOPINCPATH_)) \
 		$$(if $$(IOPCLASSRANGE_),--class-id-range $$(IOPCLASSRANGE_)) $$<
-$3.c: IOPINCPATH_=$l $/lib-inet $/qrrd/iop $($(1DV)_IOPINCPATH) $($1_IOPINCPATH) $($3_IOPINCPATH)
+$3.c: IOPINCPATH_=$l $(var/platform)/lib-inet $(var/platform)/qrrd/iop $($(1DV)_IOPINCPATH) $($1_IOPINCPATH) $($3_IOPINCPATH)
 $3.c: IOPCLASSRANGE_=$(firstword $($(1DV)_IOPCLASSRANGE) $($1_IOPCLASSRANGE) $($3_IOPCLASSRANGE))
 $3.c: $3 $(IOPC)
 	$(msg/COMPILE.iop) $3
