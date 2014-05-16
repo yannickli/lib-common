be56284b2528222f7a1fde1b11519146a7e276a8
Florent Bruneau <florent.bruneau@intersec.com>
2014-05-16 22:40:28 +0200

    Depends on iopc 3.0.11.
    Change-Id: I7f61b5ff41ff9e8b5a42f6d4946f8f34125aaa9a
    Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

--- Build/configure.inc.sh
+++ Build/configure.inc.sh
@@ -98,7 +98,7 @@ ruby_var() {
 }
 
 check_iopc() {
-    IOPC_VER=3.0.10
+    IOPC_VER=3.0.11
     if ! prereq "$IOPC_VER" "$(iopc --version)"; then
         warn "iopc version $IOPC_VER required, update your tools"
     fi
