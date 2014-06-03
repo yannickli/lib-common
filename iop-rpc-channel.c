yannickli/lib-common

commit fe8c9d42f0966544bc02413ac16cd53377940b74

ichannel: Delete "autodel" ichannels in ic_disconnect.
This patch comes to fix the asan build failures of platform
zchk-bigdata.py observed here for example:

 https://git.corp/buildbot/builders/asan/builds/403/steps/test/logs/stdio

What happened is that the platform EP was calling ic_disconnect itself on
some server peer ichannels. The platform layer is considering that if such
an ichannel is called in its on_event callback with the DISCONNECTED
event, then it will be automatically deleted by the ichannel layer, and
does NOT delete it to avoid a double-free.

The problem is that when ic_disconnect was called from outside the
ichannel layer, it was not deleting itself, and an ichannel was leaked.

Change-Id: I88b86c38b2ec71b3c77a0f19ccee85cbb371c52d

iop-rpc-channel.c
@@ -1138,13 +1138,22 @@ void ic_disconnect(ichannel_t *ic)
     el_timer_unregister(&ic->timer);
     el_timer_unregister(&ic->wa_soft_timer);
     ic_drop_id(ic);
-    ic_choose_id(ic);
-    qv_clear(iovec, &ic->iov);
-    ic->is_closing = false;
-    ic->wpos = 0;
-    ic->iov_total_len = 0;
-    sb_reset(&ic->rbuf);
-    ic->queuable = false;
+
+    if (ic->is_closing) {
+        return;
+    }
+
+    if (ic->is_spawned && !ic->no_autodel) {
+        ic_delete(&ic);
+    } else {
+        ic_choose_id(ic);
+        qv_clear(iovec, &ic->iov);
+        ic->is_closing = false;
+        ic->wpos = 0;
+        ic->iov_total_len = 0;
+        sb_reset(&ic->rbuf);
+        ic->queuable = false;
+    }
 }
 
 void ic_wipe(ichannel_t *ic)

commit 503ba98229cbcc39fb2e658843103ee96d7f985f

ichannel: Fix ic_disconnect following fe8c9d42f096.
fe8c9d42f096 introduced a regression in ic_disconnect: it is important to
execute the lines from "ic_choose_id(ic);" to "ic->queuable = false;" even
when is_closing is true.

This is because is_closing can be set to true by someone else than
ic_wipe, and in these cases it is important to leave the ichannel in a
state where ic_connect can be called.

This caused a segfault in the zplatform python tests on master branch
(only observed on my machine, not on the buildbots).

Change-Id: I56114fe43ea6b02b1aa4d4b73f2234bf5e0cdaa2

iop-rpc-channel.c
@@ -1139,11 +1139,7 @@ void ic_disconnect(ichannel_t *ic)
     el_timer_unregister(&ic->wa_soft_timer);
     ic_drop_id(ic);
 
-    if (ic->is_closing) {
-        return;
-    }
-
-    if (ic->is_spawned && !ic->no_autodel) {
+    if (!ic->is_closing && ic->is_spawned && !ic->no_autodel) {
         ic_delete(&ic);
     } else {
         ic_choose_id(ic);