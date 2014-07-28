bac753d67e7ccb5f716add500981bf4703a4ad41
Camille Gillot <camille.gillot@intersec.com>
2014-07-28 12:53:18 +0200

    lib-common: Add python utility to read mem-bench logs.
    The mem_stats.py script is invoked passing the log file as first argument.
    It currently plots the used memory/allocated memory graph.
    
    Change-Id: Ia868cf02fc0f506192592275c017df60111d8cb7

---
+++ mem_stats.py
@@ -0,0 +1,58 @@
+##########################################################################
+#                                                                        #
+#  Copyright (C) 2004-2014 INTERSEC SA                                   #
+#                                                                        #
+#  Should you receive a copy of this source code, you must check you     #
+#  have a proper, written authorization of INTERSEC to hold it. If you   #
+#  don't have such an authorization, you must DELETE all source code     #
+#  files in your possession, and inform INTERSEC of the fact you obtain  #
+#  these files. Should you not comply to these terms, you can be         #
+#  prosecuted in the extent permitted by applicable law.                 #
+#                                                                        #
+##########################################################################
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+columns = [
+    # alloc block
+    "alloc_nb", "alloc_slow",
+    "alloc_nbtimer", "alloc_min", "alloc_max",
+    "alloc_tot",
+    # realloc block
+    "realloc_nb", "realloc_slow",
+    "realloc_nbtimer", "realloc_min", "realloc_max",
+    "realloc_tot",
+    # free block
+    "free_nb", "free_slow",
+    "free_nbtimer", "free_min", "free_max",
+    "free_tot",
+    # memory block
+    "total_allocated", "total_requested",
+    "max_allocated", "max_unused", "max_used",
+    "malloc_calls", "current_used", "current_allocated"
+]
+POSITION = { s: i for (i, s) in enumerate(columns) }
+NUM_COLUMNS = len(columns)
+
+filename = sys.argv[1]
+if __name__ == "__main__":
+    data = np.loadtxt(filename, delimiter=',')
+    print data.shape
+    print NUM_COLUMNS
+
+    # adding "time"
+    time = np.array(xrange(len(data[:,0])))
+
+    # generate figure
+    fig, ax1 = plt.subplots()
+    ax1.plot(time, data[:,POSITION["current_used"]] / (1024 * 1024))
+    ax1.plot(time, data[:,POSITION["current_allocated"]] / (1024 * 1024))
+    ax1.set_xlabel('saves')
+    ax1.set_ylabel('memory (MB)')
+    ax1.legend(('Used memory', 'Available memory'), loc=0)
+    ax2 = ax1.twinx()
+    ax2.plot(time, data[:,POSITION["malloc_calls"]], 'r')
+    ax2.set_ylabel('malloc calls', color='r')
+    plt.show()
+
