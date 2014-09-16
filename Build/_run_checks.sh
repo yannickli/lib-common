yannickli/lib-common

commit 587f7ae4e11555de16d338c57d88242a6b9ade78

Build: add ability to run test with testem
About testem "Unit testing in Javascript can be tedious and painful, but
Testem makes it so easy that you will actually want to write tests."

To run testem in ci mode, a configuration file is required. His name is
testem.json. So, if an testem conf file is included in ZFile it will be
handled by  the "make check" command.

Note that I did not find another solution to launch testem without
to go in the configuration file directory.

Change-Id: Icad9d6fbc94ab7ba3d279c908be7680f02897852
Refs: #26534

Build/_run_checks.sh
@@ -91,7 +91,7 @@ for TAG in ${TAGS[@]}
 do
      BEHAVE_FLAGS="${BEHAVE_FLAGS} --tags=-$TAG"
 done
-export BEHAVE_FLAGS=$BEHAVE_FLAGS
+export BEHAVE_FLAGS="$BEHAVE_FLAGS --tags=-web --format z --no-summary"
 
 while read t; do
     say_color info "starting suite $t..."
@@ -99,17 +99,26 @@ while read t; do
     start=$(date '+%s')
     case ./"$t" in
         */behave)
-            res="$pybin -m z $BEHAVE_FLAGS --format z --no-summary --tags=-web $(dirname "./$t")/ci/features"
+            $pybin -m z $BEHAVE_FLAGS  $(dirname "./$t")/ci/features
+            res=$?
             ;;
         *.py)
-            res="$pybin ./$t"
+            $pybin ./$t
+            res=$?
+            ;;
+        *testem.json)
+            cd $(dirname "./$t")
+            ztestem
+            res=$?
+            cd - &>/dev/null
             ;;
         *)
-            res="./$t"
+            ./$t
+            res=$?
             ;;
     esac
 
-    if $res ; then
+    if [ $res -eq 0 ] ; then
         end=$(date '+%s')
         say_color pass "done ($((end - start)) seconds)"
     else

Build/_list_checks.sh
@@ -50,7 +50,7 @@ dump_zf()
                         echo "${f#$CURDIR/}"
                         continue
                         ;;
-                    *.py)
+                    *.py|*testem.json)
                         test -f "$f" || continue
                         ;;
                     *)

commit e2889b6c6d990414e71351bb18362d68bbe18d89

Build: fix no exit immediately when a test suite fails
Since 587f7ae4e11555de16d338c57d88242a6b9ade78, the make check on mmsx
is interrupted if a test suite raises an error.

The set option -e exit immediately if a command exits with
a non-zero status. So we remove it to control the test suite status and
run all tests.

Change-Id: I781a641f0336c2d77d5fb9ee436908857df0f61b
Refs: #26534

Build/_run_checks.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 ##########################################################################
 #                                                                        #
 #  Copyright (C) 2004-2014 INTERSEC SA                                   #