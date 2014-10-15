0ee55bc49ff02992b538ec2a17a8805a654ca40c
Romain Le Godais <romain.legodais@intersec.com>
2014-10-15 18:38:48 +0200

    core-macros: Use !! in unlikely.
    I don't know why this was not done like fir likely, but without this patch
    it is not possible to write code like:
    
     if (unlikely(ptr))
    
    (with ptr a pointer), so we usually write:
    
     if (unlikely(ptr != NULL))
    
    which is unusual for us.
    
    Change-Id: I131248c3ca4e0301e378100d2ae6eee63a7d1791

--- core-macros.h
+++ core-macros.h
@@ -143,7 +143,7 @@
 
 #ifdef __GNUC__
 # define likely(expr)     __builtin_expect(!!(expr), 1)
-# define unlikely(expr)   __builtin_expect((expr), 0)
+# define unlikely(expr)   __builtin_expect(!!(expr), 0)
 #else
 # define likely(expr)     (expr)
 # define unlikely(expr)   (expr)
