yannickli/lib-common

commit 7e64b309998a26b4f2933f34847614b562b4baea

iopc: Allow constraints merging.
Example:
        @max(9)
        typedef Digit uint;

        struct Test {
            @max(4)
            Digit result;
        };
In this case result would be restrict to min(4, 9) == 4.

I have changed the attribute @max, @min, @maxOccurs, @minOccurs,
@minLength and @maxLength.

Change-Id: I09475866e40b24a2f3dcf30466f2dc4d9e6a5f69
Refs: #27468

iopc/iopc-lang-c.blk
@@ -12,6 +12,8 @@
 /**************************************************************************/
 
 #include "iopc.h"
+#include <float.h>
+#include <math.h>
 
 static qv_t(str) pp_g;
 struct iopc_do_c_globs iopc_do_c_g;
@@ -1566,47 +1568,129 @@ iopc_dump_allow(sb_t *sb, const iopc_attr_t *attr, iopc_field_t *f)
     return 0;
 }
 
+static int64_t merge_int64(iopc_attr_t *attr) {
+    int64_t ret;
+
+    switch (attr->desc->id) {
+      case IOPC_ATTR_MIN_OCCURS:
+      case IOPC_ATTR_MIN_LENGTH:
+      case IOPC_ATTR_MIN:
+        ret = INT64_MIN;
+        qv_for_each_ptr(iopc_arg, arg, &attr->args) {
+            ret = MAX(ret, IOPC_ATTR_GET_ARG_V(int64_t, arg));
+        }
+        break;
+
+      case IOPC_ATTR_MAX_OCCURS:
+      case IOPC_ATTR_MAX_LENGTH:
+      case IOPC_ATTR_MAX:
+        ret = INT64_MAX;
+        qv_for_each_ptr(iopc_arg, arg, &attr->args) {
+            ret = MIN(ret, IOPC_ATTR_GET_ARG_V(int64_t, arg));
+        }
+        break;
+
+     case IOPC_ATTR_LENGTH:
+        assert (attr->args.len == 1);
+        ret = attr->args.tab[0].v.i64;
+        break;
+
+      default:
+        e_panic("attribute type not handled in `int64`");
+    }
+    return ret;
+}
+
+static uint64_t merge_uint64(iopc_attr_t *attr) {
+    uint64_t ret;
+
+    switch (attr->desc->id) {
+      case IOPC_ATTR_MIN:
+        ret = 0;
+        qv_for_each_ptr(iopc_arg, arg, &attr->args) {
+            ret = MAX(ret, IOPC_ATTR_GET_ARG_V(uint64_t, arg));
+        }
+        break;
+
+      case IOPC_ATTR_MAX:
+        ret = UINT64_MAX;
+        qv_for_each_ptr(iopc_arg, arg, &attr->args) {
+            ret = MIN(ret, IOPC_ATTR_GET_ARG_V(uint64_t, arg));
+        }
+        break;
+
+      default:
+        e_panic("attribute type not handled in `uint64`");
+    }
+    return ret;
+}
+
+static double merge_double(iopc_attr_t *attr) {
+    double ret;
+
+    switch (attr->desc->id) {
+      case IOPC_ATTR_MIN:
+        ret = FLT_MIN;
+        qv_for_each_ptr(iopc_arg, arg, &attr->args) {
+            ret = MAX(ret, IOPC_ATTR_GET_ARG_V(double, arg));
+        }
+        break;
+
+      case IOPC_ATTR_MAX:
+        ret = FLT_MAX;
+        qv_for_each_ptr(iopc_arg, arg, &attr->args) {
+            ret = MIN(ret, IOPC_ATTR_GET_ARG_V(double, arg));
+        }
+        break;
+
+      default:
+        e_panic("attribute type not handled in `double`");
+    }
+    return ret;
+}
+
 static void
 iopc_dump_minmax(sb_t *sb, int cmp, iopc_attr_t *attr, iopc_field_t *f)
 {
-    iopc_arg_t *arg = &attr->args.tab[0];
 
 #define GEN_TEST(fmt1, fmt2, type) \
-    sb_addf(sb,                                                             \
-            LVL2 "if (val %c " fmt1 ") {\n"                                 \
-            LVL3 "if (n > 1) {\n"                                           \
-            \
-            LVL4 "iop_set_err(\"violation of constraint %%s (" fmt2 ") "    \
-                 "on field %%s[%%d]: val=" fmt2 "\",\n"                     \
-            LVL4 "            \"%*pM\", (" type ")" fmt1 ", \"%s\", j, "    \
-                 "(" type ")val);\n"                                        \
-            \
-            LVL3 "} else {\n"                                               \
-            \
-            LVL4 "iop_set_err(\"violation of constraint %%s (" fmt2 ") "    \
-                 "on field %%s: val=" fmt2 "\",\n"                          \
-            LVL4 "            \"%*pM\", (" type ")" fmt1 ", \"%s\", "       \
-                 "(" type ")val);\n"                                        \
-            \
-            LVL3 "}\n"                                                      \
-            \
-            LVL3 "return -1;\n"                                             \
-            LVL2 "}\n",                                                     \
-            cmp, v, LSTR_FMT_ARG(attr->desc->name), v, f->name,             \
-            LSTR_FMT_ARG(attr->desc->name), v, f->name);
+    do {                                                                     \
+        sb_addf(sb,                                                          \
+                LVL2 "if (val %c " fmt1 ") {\n"                              \
+                LVL3 "if (n > 1) {\n"                                        \
+                                                                             \
+                LVL4 "iop_set_err(\"violation of constraint %%s (" fmt2 ") " \
+                     "on field %%s[%%d]: val=" fmt2 "\",\n"                  \
+                LVL4 "            \"%*pM\", (" type ")" fmt1 ", \"%s\", j, " \
+                     "(" type ")val);\n"                                     \
+                                                                             \
+                LVL3 "} else {\n"                                            \
+                                                                             \
+                LVL4 "iop_set_err(\"violation of constraint %%s (" fmt2 ") " \
+                     "on field %%s: val=" fmt2 "\",\n"                       \
+                LVL4 "            \"%*pM\", (" type ")" fmt1 ", \"%s\", "    \
+                     "(" type ")val);\n"                                     \
+                                                                             \
+                LVL3 "}\n"                                                   \
+                                                                             \
+                LVL3 "return -1;\n"                                          \
+                LVL2 "}\n",                                                  \
+                cmp, v, LSTR_FMT_ARG(attr->desc->name), v, f->name,          \
+                LSTR_FMT_ARG(attr->desc->name), v, f->name);                 \
+    } while (0)
 
     assert (iopc_g.v2);
     if (f->kind == IOP_T_DOUBLE) {
-        double v = IOPC_ATTR_GET_ARG_V(double, arg);
+        double v = merge_double(attr);
 
         GEN_TEST("%.17e", "%%.17e", "double");
     } else
     if (iopc_field_is_signed(f)) {
-        int64_t v = IOPC_ATTR_GET_ARG_V(int64_t, arg);
+        int64_t v = merge_int64(attr);
 
         GEN_TEST("%jdLL", "%%jd", "int64_t");
     } else {
-        uint64_t v = IOPC_ATTR_GET_ARG_V(uint64_t, arg);
+        uint64_t v = merge_uint64(attr);
 
         GEN_TEST("%juULL", "%%ju", "uint64_t");
     }
@@ -1678,14 +1762,12 @@ static void iopc_dump_field_attr(iopc_attr_t *attr, unsigned type,
       case IOPC_ATTR_MIN_LENGTH:
       case IOPC_ATTR_MAX_LENGTH:
       case IOPC_ATTR_LENGTH:
-        assert (attr->args.tab[0].type == ITOK_INTEGER);
-        assert (attr->args.len == 1);
         sb_addf(sb,
                 LVL1 "{\n"
                 LVL1 "    .type = %d,\n"
                 LVL1 "    .args = (iop_field_attr_arg_t[]){ { .v.i64 = %jdLL } },\n"
                 LVL1 "},\n",
-                type, attr->args.tab[0].v.i64);
+                type, merge_int64(attr));
         break;
 
       case IOPC_ATTR_PATTERN:
@@ -1703,14 +1785,13 @@ static void iopc_dump_field_attr(iopc_attr_t *attr, unsigned type,
 
       case IOPC_ATTR_MIN:
       case IOPC_ATTR_MAX:
-        assert (attr->args.len == 1);
         if (f->kind == IOP_T_DOUBLE) {
             sb_addf(sb,
                     LVL1 "{\n"
                     LVL1 "    .type = %d,\n"
                     LVL1 "    .args = (iop_field_attr_arg_t[]){ { .v.d = %.17e } },\n"
                     LVL1 "},\n",
-                    type, IOPC_ATTR_GET_ARG_V(double, &attr->args.tab[0]));
+                    type, merge_double(attr));
         } else
         if (iopc_field_is_signed(f)) {
             sb_addf(sb,
@@ -1718,14 +1799,14 @@ static void iopc_dump_field_attr(iopc_attr_t *attr, unsigned type,
                     LVL1 "    .type = %d,\n"
                     LVL1 "    .args = (iop_field_attr_arg_t[]){ { .v.i64 = %jdLL } },\n"
                     LVL1 "},\n",
-                    type, IOPC_ATTR_GET_ARG_V(int64_t, &attr->args.tab[0]));
+                    type, merge_int64(attr));
         } else {
             sb_addf(sb,
                     LVL1 "{\n"
                     LVL1 "    .type = %d,\n"
                     LVL1 "    .args = (iop_field_attr_arg_t[]){ { .v.i64 = %juULL } },\n"
                     LVL1 "},\n",
-                    type, IOPC_ATTR_GET_ARG_V(uint64_t, &attr->args.tab[0]));
+                    type, merge_uint64(attr));
         }
         break;
 

iopc/testsuite/attrs_multi_constraints.iop
@@ -1,0 +1,35 @@
+package attrs_multi_constraints;
+
+@min(3)
+typedef ulong Min3;
+@min(5)
+typedef Min3 Min5;
+
+@max(42) @min(5)
+typedef uint Digit;
+
+struct Test {
+    @max(21) @min(7)
+    Digit a;
+    @max(43) @min(0)
+    Digit b;
+    Min5 c;
+};
+
+@minLength(5) @maxLength(128)
+typedef string ExStr;
+
+struct StrTest {
+    @maxLength(21) @minLength(7)
+    ExStr a;
+    @length(42)
+    string b;
+};
+
+@maxOccurs(42) @minOccurs(5)
+typedef int[] ExTab;
+
+struct TabTest {
+    @maxOccurs(21) @minOccurs(7)
+    ExTab a;
+};

iopc/testsuite/reference_attrs_multi_constraints.c
@@ -1,0 +1,383 @@
+/***** THIS FILE IS AUTOGENERATED DO NOT MODIFY DIRECTLY ! *****/
+#if !defined(linux) && !defined(__APPLE__)
+#  warning "attrs_multi_constraints.iop assumed linux alignments"
+#endif
+#ifndef __x86_64__
+#  warning "attrs_multi_constraints.iop assumed x86_64 alignments"
+#endif
+
+#include "attrs_multi_constraints.iop.h"
+
+/* Structure attrs_multi_constraints.Test {{{ */
+
+static int attrs_multi_constraints__test__a__check(const void *ptr, int n)
+{
+    for (int j = 0; j < n; j++) {
+        uint32_t  val = IOP_FIELD(uint32_t , ptr, j);
+
+        if (val > 21ULL) {
+            if (n > 1) {
+                iop_set_err("violation of constraint %s (%ju) on field %s[%d]: val=%ju",
+                            "max", (uint64_t)21ULL, "a", j, (uint64_t)val);
+            } else {
+                iop_set_err("violation of constraint %s (%ju) on field %s: val=%ju",
+                            "max", (uint64_t)21ULL, "a", (uint64_t)val);
+            }
+            return -1;
+        }
+        if (val < 7ULL) {
+            if (n > 1) {
+                iop_set_err("violation of constraint %s (%ju) on field %s[%d]: val=%ju",
+                            "min", (uint64_t)7ULL, "a", j, (uint64_t)val);
+            } else {
+                iop_set_err("violation of constraint %s (%ju) on field %s: val=%ju",
+                            "min", (uint64_t)7ULL, "a", (uint64_t)val);
+            }
+            return -1;
+        }
+    }
+    return 0;
+}
+static iop_field_attr_t const attrs_multi_constraints__test__a__attrs[] = {
+    {
+        .type = 4,
+        .args = (iop_field_attr_arg_t[]){ { .v.i64 = 21ULL } },
+    },
+    {
+        .type = 3,
+        .args = (iop_field_attr_arg_t[]){ { .v.i64 = 7ULL } },
+    },
+};
+static int attrs_multi_constraints__test__b__check(const void *ptr, int n)
+{
+    for (int j = 0; j < n; j++) {
+        uint32_t  val = IOP_FIELD(uint32_t , ptr, j);
+
+        if (val > 42ULL) {
+            if (n > 1) {
+                iop_set_err("violation of constraint %s (%ju) on field %s[%d]: val=%ju",
+                            "max", (uint64_t)42ULL, "b", j, (uint64_t)val);
+            } else {
+                iop_set_err("violation of constraint %s (%ju) on field %s: val=%ju",
+                            "max", (uint64_t)42ULL, "b", (uint64_t)val);
+            }
+            return -1;
+        }
+        if (val < 5ULL) {
+            if (n > 1) {
+                iop_set_err("violation of constraint %s (%ju) on field %s[%d]: val=%ju",
+                            "min", (uint64_t)5ULL, "b", j, (uint64_t)val);
+            } else {
+                iop_set_err("violation of constraint %s (%ju) on field %s: val=%ju",
+                            "min", (uint64_t)5ULL, "b", (uint64_t)val);
+            }
+            return -1;
+        }
+    }
+    return 0;
+}
+static iop_field_attr_t const attrs_multi_constraints__test__b__attrs[] = {
+    {
+        .type = 4,
+        .args = (iop_field_attr_arg_t[]){ { .v.i64 = 42ULL } },
+    },
+    {
+        .type = 3,
+        .args = (iop_field_attr_arg_t[]){ { .v.i64 = 5ULL } },
+    },
+};
+static int attrs_multi_constraints__test__c__check(const void *ptr, int n)
+{
+    for (int j = 0; j < n; j++) {
+        uint64_t  val = IOP_FIELD(uint64_t , ptr, j);
+
+        if (val < 5ULL) {
+            if (n > 1) {
+                iop_set_err("violation of constraint %s (%ju) on field %s[%d]: val=%ju",
+                            "min", (uint64_t)5ULL, "c", j, (uint64_t)val);
+            } else {
+                iop_set_err("violation of constraint %s (%ju) on field %s: val=%ju",
+                            "min", (uint64_t)5ULL, "c", (uint64_t)val);
+            }
+            return -1;
+        }
+    }
+    return 0;
+}
+static iop_field_attr_t const attrs_multi_constraints__test__c__attrs[] = {
+    {
+        .type = 3,
+        .args = (iop_field_attr_arg_t[]){ { .v.i64 = 5ULL } },
+    },
+};
+static iop_field_attrs_t const attrs_multi_constraints__test__desc_fields_attrs[] = {
+    {
+        .flags             = 24,
+        .attrs_len         = 2,
+        .check_constraints = &attrs_multi_constraints__test__a__check,
+        .attrs             = attrs_multi_constraints__test__a__attrs,
+    },
+    {
+        .flags             = 24,
+        .attrs_len         = 2,
+        .check_constraints = &attrs_multi_constraints__test__b__check,
+        .attrs             = attrs_multi_constraints__test__b__attrs,
+    },
+    {
+        .flags             = 8,
+        .attrs_len         = 1,
+        .check_constraints = &attrs_multi_constraints__test__c__check,
+        .attrs             = attrs_multi_constraints__test__c__attrs,
+    },
+};
+static iop_field_t const attrs_multi_constraints__test__desc_fields[] = {
+    {
+        .name      = LSTR_IMMED("a"),
+        .tag       = 1,
+        .tag_len   = 0,
+        .repeat    = IOP_R_REQUIRED,
+        .type      = IOP_T_U32,
+        .data_offs = offsetof(attrs_multi_constraints__test__t, a),
+        .flags     = 1,
+        .size      = fieldsizeof(attrs_multi_constraints__test__t, a),
+    },
+    {
+        .name      = LSTR_IMMED("b"),
+        .tag       = 2,
+        .tag_len   = 0,
+        .repeat    = IOP_R_REQUIRED,
+        .type      = IOP_T_U32,
+        .data_offs = offsetof(attrs_multi_constraints__test__t, b),
+        .flags     = 1,
+        .size      = fieldsizeof(attrs_multi_constraints__test__t, b),
+    },
+    {
+        .name      = LSTR_IMMED("c"),
+        .tag       = 3,
+        .tag_len   = 0,
+        .repeat    = IOP_R_REQUIRED,
+        .type      = IOP_T_U64,
+        .data_offs = offsetof(attrs_multi_constraints__test__t, c),
+        .flags     = 1,
+        .size      = fieldsizeof(attrs_multi_constraints__test__t, c),
+    },
+};
+static int const iop__ranges__1[] = {
+    0, 1,
+    3,
+};
+const iop_struct_t attrs_multi_constraints__test__s = {
+    .fullname   = LSTR_IMMED("attrs_multi_constraints.Test"),
+    .fields     = attrs_multi_constraints__test__desc_fields,
+    .ranges     = iop__ranges__1,
+    .fields_len = countof(attrs_multi_constraints__test__desc_fields),
+    .ranges_len = countof(iop__ranges__1) / 2,
+    .size       = sizeof(attrs_multi_constraints__test__t),
+    .flags      = 3,
+    .fields_attrs = attrs_multi_constraints__test__desc_fields_attrs,
+};
+
+/* }}} */
+/* Structure attrs_multi_constraints.StrTest {{{ */
+
+static int attrs_multi_constraints__str_test__a__check(const void *ptr, int n)
+{
+    for (int j = 0; j < n; j++) {
+        lstr_t    val = IOP_FIELD(lstr_t   , ptr, j);
+
+        if (val.len > 21) {
+            iop_set_err("violation of constraint %s (%d) on field %s: length=%d",
+                        "maxLength", 21, "a", val.len);
+            return -1;
+        }
+        if (val.len < 7) {
+            iop_set_err("violation of constraint %s (%d) on field %s: length=%d",
+                        "minLength", 7, "a", val.len);
+            return -1;
+        }
+    }
+    return 0;
+}
+static iop_field_attr_t const attrs_multi_constraints__str_test__a__attrs[] = {
+    {
+        .type = 8,
+        .args = (iop_field_attr_arg_t[]){ { .v.i64 = 21LL } },
+    },
+    {
+        .type = 7,
+        .args = (iop_field_attr_arg_t[]){ { .v.i64 = 7LL } },
+    },
+};
+static int attrs_multi_constraints__str_test__b__check(const void *ptr, int n)
+{
+    for (int j = 0; j < n; j++) {
+        lstr_t    val = IOP_FIELD(lstr_t   , ptr, j);
+
+        if (val.len != 42) {
+            iop_set_err("violation of constraint %s (%d) on field %s: length=%d",
+                        "length", 42, "b", val.len);
+            return -1;
+        }
+    }
+    return 0;
+}
+static iop_field_attr_t const attrs_multi_constraints__str_test__b__attrs[] = {
+    {
+        .type = 7,
+        .args = (iop_field_attr_arg_t[]){ { .v.i64 = 42LL } },
+    },
+    {
+        .type = 8,
+        .args = (iop_field_attr_arg_t[]){ { .v.i64 = 42LL } },
+    },
+};
+static iop_field_attrs_t const attrs_multi_constraints__str_test__desc_fields_attrs[] = {
+    {
+        .flags             = 384,
+        .attrs_len         = 2,
+        .check_constraints = &attrs_multi_constraints__str_test__a__check,
+        .attrs             = attrs_multi_constraints__str_test__a__attrs,
+    },
+    {
+        .flags             = 384,
+        .attrs_len         = 2,
+        .check_constraints = &attrs_multi_constraints__str_test__b__check,
+        .attrs             = attrs_multi_constraints__str_test__b__attrs,
+    },
+};
+static iop_field_t const attrs_multi_constraints__str_test__desc_fields[] = {
+    {
+        .name      = LSTR_IMMED("a"),
+        .tag       = 1,
+        .tag_len   = 0,
+        .repeat    = IOP_R_REQUIRED,
+        .type      = IOP_T_STRING,
+        .data_offs = offsetof(attrs_multi_constraints__str_test__t, a),
+        .flags     = 1,
+        .size      = fieldsizeof(attrs_multi_constraints__str_test__t, a),
+    },
+    {
+        .name      = LSTR_IMMED("b"),
+        .tag       = 2,
+        .tag_len   = 0,
+        .repeat    = IOP_R_REQUIRED,
+        .type      = IOP_T_STRING,
+        .data_offs = offsetof(attrs_multi_constraints__str_test__t, b),
+        .flags     = 1,
+        .size      = fieldsizeof(attrs_multi_constraints__str_test__t, b),
+    },
+};
+static int const iop__ranges__2[] = {
+    0, 1,
+    2,
+};
+const iop_struct_t attrs_multi_constraints__str_test__s = {
+    .fullname   = LSTR_IMMED("attrs_multi_constraints.StrTest"),
+    .fields     = attrs_multi_constraints__str_test__desc_fields,
+    .ranges     = iop__ranges__2,
+    .fields_len = countof(attrs_multi_constraints__str_test__desc_fields),
+    .ranges_len = countof(iop__ranges__2) / 2,
+    .size       = sizeof(attrs_multi_constraints__str_test__t),
+    .flags      = 3,
+    .fields_attrs = attrs_multi_constraints__str_test__desc_fields_attrs,
+};
+
+/* }}} */
+/* Structure attrs_multi_constraints.TabTest {{{ */
+
+static int attrs_multi_constraints__tab_test__a__check(const void *ptr, int n)
+{
+    if (n > 21) {
+        iop_set_err("violation of constraint %s (%d) on field %s: length=%d",
+                    "maxOccurs", 21, "a", n);
+        return -1;
+    }
+    if (n < 7) {
+        iop_set_err("violation of constraint %s (%d) on field %s: length=%d",
+                    "minOccurs", 7, "a", n);
+        return -1;
+    }
+    return 0;
+}
+static iop_field_attr_t const attrs_multi_constraints__tab_test__a__attrs[] = {
+    {
+        .type = 1,
+        .args = (iop_field_attr_arg_t[]){ { .v.i64 = 21LL } },
+    },
+    {
+        .type = 0,
+        .args = (iop_field_attr_arg_t[]){ { .v.i64 = 7LL } },
+    },
+};
+static iop_field_attrs_t const attrs_multi_constraints__tab_test__desc_fields_attrs[] = {
+    {
+        .flags             = 3,
+        .attrs_len         = 2,
+        .check_constraints = &attrs_multi_constraints__tab_test__a__check,
+        .attrs             = attrs_multi_constraints__tab_test__a__attrs,
+    },
+};
+static iop_field_t const attrs_multi_constraints__tab_test__desc_fields[] = {
+    {
+        .name      = LSTR_IMMED("a"),
+        .tag       = 1,
+        .tag_len   = 0,
+        .repeat    = IOP_R_REPEATED,
+        .type      = IOP_T_I32,
+        .data_offs = offsetof(attrs_multi_constraints__tab_test__t, a),
+        .flags     = 3,
+        .size      = fieldsizeof(attrs_multi_constraints__tab_test__t, a.tab[0]),
+    },
+};
+static int const iop__ranges__3[] = {
+    0, 1,
+    1,
+};
+const iop_struct_t attrs_multi_constraints__tab_test__s = {
+    .fullname   = LSTR_IMMED("attrs_multi_constraints.TabTest"),
+    .fields     = attrs_multi_constraints__tab_test__desc_fields,
+    .ranges     = iop__ranges__3,
+    .fields_len = countof(attrs_multi_constraints__tab_test__desc_fields),
+    .ranges_len = countof(iop__ranges__3) / 2,
+    .size       = sizeof(attrs_multi_constraints__tab_test__t),
+    .flags      = 3,
+    .fields_attrs = attrs_multi_constraints__tab_test__desc_fields_attrs,
+};
+
+/* }}} */
+/* Package attrs_multi_constraints {{{ */
+
+static const iop_pkg_t *const attrs_multi_constraints__deps[] = {
+    NULL,
+};
+
+static const iop_enum_t *const attrs_multi_constraints__enums[] = {
+    NULL,
+};
+
+static const iop_struct_t *const attrs_multi_constraints__structs[] = {
+    &attrs_multi_constraints__test__s,
+    &attrs_multi_constraints__str_test__s,
+    &attrs_multi_constraints__tab_test__s,
+    NULL,
+};
+
+static const iop_iface_t *const attrs_multi_constraints__ifaces[] = {
+    NULL,
+};
+
+static const iop_mod_t *const attrs_multi_constraints__mods[] = {
+    NULL,
+};
+
+iop_pkg_t const attrs_multi_constraints__pkg = {
+    .name    = LSTR_IMMED("attrs_multi_constraints"),
+    .deps    = attrs_multi_constraints__deps,
+    .enums   = attrs_multi_constraints__enums,
+    .structs = attrs_multi_constraints__structs,
+    .ifaces  = attrs_multi_constraints__ifaces,
+    .mods    = attrs_multi_constraints__mods,
+};
+
+/* }}} */
+

commit 277f0010b8278102fd3d9b6434ccfa830d2d7435

iopc: fix min constraint operator on double
When min constraints are merged, the max one is kept.

The initial value was set to FLT_MIN, however, FLT_MIN
is not the smallest double value, but the smallest not
null positive value (1e-38).

So as a consequence, all the values lower or equals to 0
was ignored, leading to a min equals to FLT_MIN.

This issue was introduced by 7e64b3099. Thus all versions
delivered after 2014-09-30 are impacted:
* B-On: 1.0.9, 1.0.10
* Igloo: 4.0.10, 4.0.11, 4.1.0-RC2, 4.1.0-beta1
* LMS: 5.1.13, 5.1.14, 5.1.15, 5.1.16
* MCG: 6.3.18, 6.3.19, 6.4.10, 6.4.11, 6.4.12, 6.4.13
* MCMS: 5.0.16, 5.0.17

However it seems that only B-On and MCG are using IOP
constraints on double at least in master branch.

> git grep "@min([0-9]*\.[0-9]*)"
* MCG: sccore.Threshold, sccore.RetryDelay
* B-On: sdk.TariffPlan

Closes: #29901
Change-Id: I150215baeeeb72a3e80cca8deb0f558b9008ac41
Signed-off-by: Nicolas Gelot <nicolas.gelot@intersec.com>

iopc/iopc-lang-c.blk
@@ -1654,7 +1654,7 @@ static double merge_double(iopc_attr_t *attr) {
 
     switch (attr->desc->id) {
       case IOPC_ATTR_MIN:
-        ret = FLT_MIN;
+        ret = -FLT_MAX;
         qv_for_each_ptr(iopc_arg, arg, &attr->args) {
             ret = MAX(ret, IOPC_ATTR_GET_ARG_V(double, arg));
         }

iopc/iopc.h
@@ -16,7 +16,7 @@
 
 #define IOPC_MAJOR   3
 #define IOPC_MINOR   0
-#define IOPC_PATCH   13
+#define IOPC_PATCH   14
 
 #include <lib-common/container.h>
 #include <lib-common/iop.h>

iopc/testsuite/attrs_multi_constraints.iop
@@ -16,6 +16,16 @@ struct Test {
     Min5 c;
 };
 
+@min(-1.0)
+typedef double Neg;
+@min(0.0)
+typedef Neg Zero;
+
+struct Test2 {
+    Neg a;
+    Zero b;
+};
+
 @minLength(5) @maxLength(128)
 typedef string ExStr;
 

iopc/testsuite/reference_attrs_multi_constraints.c
@@ -178,6 +178,108 @@ const iop_struct_t attrs_multi_constraints__test__s = {
 };
 
 /* }}} */
+/* Structure attrs_multi_constraints.Test2 {{{ */
+
+static int attrs_multi_constraints__test2__a__check(const void *ptr, int n)
+{
+    for (int j = 0; j < n; j++) {
+        double    val = IOP_FIELD(double   , ptr, j);
+
+        if (val < -1.00000000000000000e+00) {
+            if (n > 1) {
+                iop_set_err("violation of constraint %s (%.17e) on field %s[%d]: val=%.17e",
+                            "min", (double)-1.00000000000000000e+00, "a", j, (double)val);
+            } else {
+                iop_set_err("violation of constraint %s (%.17e) on field %s: val=%.17e",
+                            "min", (double)-1.00000000000000000e+00, "a", (double)val);
+            }
+            return -1;
+        }
+    }
+    return 0;
+}
+static iop_field_attr_t const attrs_multi_constraints__test2__a__attrs[] = {
+    {
+        .type = 3,
+        .args = (iop_field_attr_arg_t[]){ { .v.d = -1.00000000000000000e+00 } },
+    },
+};
+static int attrs_multi_constraints__test2__b__check(const void *ptr, int n)
+{
+    for (int j = 0; j < n; j++) {
+        double    val = IOP_FIELD(double   , ptr, j);
+
+        if (val < 0.00000000000000000e+00) {
+            if (n > 1) {
+                iop_set_err("violation of constraint %s (%.17e) on field %s[%d]: val=%.17e",
+                            "min", (double)0.00000000000000000e+00, "b", j, (double)val);
+            } else {
+                iop_set_err("violation of constraint %s (%.17e) on field %s: val=%.17e",
+                            "min", (double)0.00000000000000000e+00, "b", (double)val);
+            }
+            return -1;
+        }
+    }
+    return 0;
+}
+static iop_field_attr_t const attrs_multi_constraints__test2__b__attrs[] = {
+    {
+        .type = 3,
+        .args = (iop_field_attr_arg_t[]){ { .v.d = 0.00000000000000000e+00 } },
+    },
+};
+static iop_field_attrs_t const attrs_multi_constraints__test2__desc_fields_attrs[] = {
+    {
+        .flags             = 8,
+        .attrs_len         = 1,
+        .check_constraints = &attrs_multi_constraints__test2__a__check,
+        .attrs             = attrs_multi_constraints__test2__a__attrs,
+    },
+    {
+        .flags             = 8,
+        .attrs_len         = 1,
+        .check_constraints = &attrs_multi_constraints__test2__b__check,
+        .attrs             = attrs_multi_constraints__test2__b__attrs,
+    },
+};
+static iop_field_t const attrs_multi_constraints__test2__desc_fields[] = {
+    {
+        .name      = LSTR_IMMED("a"),
+        .tag       = 1,
+        .tag_len   = 0,
+        .repeat    = IOP_R_REQUIRED,
+        .type      = IOP_T_DOUBLE,
+        .data_offs = offsetof(attrs_multi_constraints__test2__t, a),
+        .flags     = 1,
+        .size      = fieldsizeof(attrs_multi_constraints__test2__t, a),
+    },
+    {
+        .name      = LSTR_IMMED("b"),
+        .tag       = 2,
+        .tag_len   = 0,
+        .repeat    = IOP_R_REQUIRED,
+        .type      = IOP_T_DOUBLE,
+        .data_offs = offsetof(attrs_multi_constraints__test2__t, b),
+        .flags     = 1,
+        .size      = fieldsizeof(attrs_multi_constraints__test2__t, b),
+    },
+};
+static int const iop__ranges__2[] = {
+    0, 1,
+    2,
+};
+const iop_struct_t attrs_multi_constraints__test2__s = {
+    .fullname   = LSTR_IMMED("attrs_multi_constraints.Test2"),
+    .fields     = attrs_multi_constraints__test2__desc_fields,
+    .ranges     = iop__ranges__2,
+    .fields_len = countof(attrs_multi_constraints__test2__desc_fields),
+    .ranges_len = countof(iop__ranges__2) / 2,
+    .size       = sizeof(attrs_multi_constraints__test2__t),
+    .flags      = 3,
+    .fields_attrs = attrs_multi_constraints__test2__desc_fields_attrs,
+};
+
+/* }}} */
 /* Structure attrs_multi_constraints.StrTest {{{ */
 
 static int attrs_multi_constraints__str_test__a__check(const void *ptr, int n)
@@ -267,10 +369,6 @@ static iop_field_t const attrs_multi_constraints__str_test__desc_fields[] = {
         .size      = fieldsizeof(attrs_multi_constraints__str_test__t, b),
     },
 };
-static int const iop__ranges__2[] = {
-    0, 1,
-    2,
-};
 const iop_struct_t attrs_multi_constraints__str_test__s = {
     .fullname   = LSTR_IMMED("attrs_multi_constraints.StrTest"),
     .fields     = attrs_multi_constraints__str_test__desc_fields,
@@ -357,6 +455,7 @@ static const iop_enum_t *const attrs_multi_constraints__enums[] = {
 
 static const iop_struct_t *const attrs_multi_constraints__structs[] = {
     &attrs_multi_constraints__test__s,
+    &attrs_multi_constraints__test2__s,
     &attrs_multi_constraints__str_test__s,
     &attrs_multi_constraints__tab_test__s,
     NULL,