72e4ac3c3a0350fb27a2fb042bcab82da67b425f
Pierre-Yves Caneill <pierre-yves.caneill@intersec.com>
2014-12-23 10:05:18 +0100

    core-havege: ha_rand_range did not support a [INT_MIN:INT_MAX]
    When calling ha_rand_range with a range [INT_MIN:INT_MAX] the results
    were always INT_MIN.
    
    This was due to an int overflow in ha_rand_range.
    
    A test has been added to check that the bug has correctly been fix, but
    does not check the correctness of the function ha_rand_range.
    Such a test should be written
    
    Change-Id: I9a6035419c910d2e85bc8b9a9995b024da558c44
    Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

--- core-havege.c
+++ core-havege.c
@@ -295,3 +295,40 @@ int main(int argc, char *argv[])
 }
 
 #endif
+
+/*{{{ Tests */
+
+/* LCOV_EXCL_START */
+
+#include <lib-common/z.h>
+
+Z_GROUP_EXPORT(core_havege) {
+    Z_TEST(havege_range, "havege_range") {
+        int number1;
+        int numbers[10000];
+        bool is_different_than_int_min = false;
+
+        /* Test bug that existed in ha_rand_rage when INT_MIN was given, the
+         * results were always INT_MIN.
+         */
+        for (int i = 0; i < 10000; i++) {
+            numbers[i] = ha_rand_range(INT_MIN, INT_MAX);
+        }
+        for (int i = 0; i < 10000; i++) {
+            if (numbers[i] != INT_MIN) {
+                is_different_than_int_min = true;
+            }
+        }
+        Z_ASSERT(is_different_than_int_min);
+
+        number1 = ha_rand_range(INT_MIN + 1, INT_MAX - 1);
+        Z_ASSERT(number1 > INT_MIN && number1 < INT_MAX);
+        number1 = ha_rand_range(-10, 10);
+        Z_ASSERT(number1 >= -10 && number1 <= 10);
+
+    } Z_TEST_END;
+} Z_GROUP_END;
+
+/* LCOV_EXCL_STOP */
+
+/*}}} */
