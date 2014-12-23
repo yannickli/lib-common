7a63b37e9b7203587db35624035e9644f83a69c4
Pierre-Yves Caneill <pierre-yves.caneill@intersec.com>
2014-12-23 10:05:18 +0100

    core-havege: ha_rand_range did not support a [INT_MIN:INT_MAX]
    When calling ha_rand_range with a range [INT_MIN:INT_MAX] the results
    were always INT_MIN.
    
    This was due to an int overflow in ha_rand_range.
    
    A test has been added to check that the bug has correctly been fix, but
    does not check the correctness of the function ha_rand_range.
    Such a test should be written
    
    Change-Id: I9a6035419c910d2e85bc8b9a9995b024da558c44

--- core-havege.c
+++ core-havege.c
@@ -6,7 +6,7 @@
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
  *  are met:
- *  
+ *
  *    * Redistributions of source code must retain the above copyright
  *      notice, this list of conditions and the following disclaimer.
  *    * Redistributions in binary form must reproduce the above copyright
@@ -15,7 +15,7 @@
  *    * Neither the name of XySSL nor the names of its contributors may be
  *      used to endorse or promote products derived from this software
  *      without specific prior written permission.
- *  
+ *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -238,7 +238,9 @@ uint32_t ha_rand(void)
 
 int ha_rand_range(int first, int last)
 {
-    uint64_t res = (uint64_t)ha_rand() * (uint64_t)(last - first + 1);
+    uint64_t res;
+
+    res = (uint64_t)ha_rand() * ((uint64_t)last - first + 1);
     return first + (res >> 32);
 }
 
