93ec0959f2ed4f75c1c438b6125f16f57fb6d495
Xavier Chantry <xavier.chantry@intersec.com>
2015-02-26 14:05:17 +0100

    bump iopc requirement to 3.0.19 to use new local keyword
    Change-Id: Icc5e027b89aae26202e3d28bca2bf00401196fbb

--- Build/configure.inc.sh
+++ Build/configure.inc.sh
@@ -94,7 +94,7 @@ prereq() {
 }
 
 check_iopc() {
-    IOPC_VER=3.0.15
+    IOPC_VER=3.0.19
     if ! prereq "$IOPC_VER" "$(iopc --version)"; then
         warn "iopc version $IOPC_VER required, update your tools"
     fi
