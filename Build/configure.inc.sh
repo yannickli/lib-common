a6dd86cd15d1e7f1ea0e4c8bb153cab6b65daf9d
Florent Bruneau <florent.bruneau@intersec.com>
2015-04-02 16:28:00 +0200

    Build: depend on iopc 3.0.20.
    Change-Id: I2ce05350d0d259447abda298d4f930fccc27ad1e
    Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

--- Build/configure.inc.sh
+++ Build/configure.inc.sh
@@ -94,7 +94,7 @@ prereq() {
 }
 
 check_iopc() {
-    IOPC_VER=3.0.19
+    IOPC_VER=3.0.20
     if ! prereq "$IOPC_VER" "$(iopc --version)"; then
         warn "iopc version $IOPC_VER required, update your tools"
     fi
