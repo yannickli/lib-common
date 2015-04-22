a24be0d966e224157497ed311ab4584d1e50ff74
Nicolas Angevelle <nicolas.angevelle@intersec.com>
2015-04-22 17:18:14 +0200

    www/ci: Add www-coverage target
    Basically we:
    * copy the whole repo to .build-coverage-${hostname}
    * replace meaningful js files by the instrumented ones
    * Launch the proxy which receives the coverage objects and write them on
    disk
    * Run the test in .build-coverage-${hostname}
    * Create the html and lcov report from all the collected coverage*json
    files
    
    There is a few tricks:
    * the --baseline-file option creates a coverage object even for files which
    are untouched by tests. We need a unique --save-baseline name for each
    instrumented directory.
    * --complete-copy dereference links and we have recursive loops and broken
    links so we use "cp -r -a" plus the "--complete-copy" to bypass it
    * We do not want coverage on external files, yet they are needed, so we
    copy them afterward
    * Writing the instrumented files in the .build-coverage-${hostname}
    requires to remove symlink first or they'll be written in the source dir.
    
    The make www-check line seems to launch tests on both
    .build-coverage-${hostname} and source dir. It is not impacting the
    coverage but is is just twice longer to complete the coverage.
    
    Change-Id: I561b9beb5886b332a525446373c360954ecc7dad

--- Config/profile-coverage.mk
+++ Config/profile-coverage.mk
@@ -16,6 +16,8 @@ CFLAGS   += -pg -fprofile-arcs -ftest-coverage
 CXXFLAGS += -pg -fprofile-arcs -ftest-coverage
 LDFLAGS  += -lgcov
 
+REPORT_DIR=coverage
+
 # Partially copied from http://bind10.isc.org/wiki/TestCodeCoverage
 coverage:: __setup_forward
 	lcov --directory $! --base-directory $/ --zerocounters
@@ -30,3 +32,26 @@ coverage:: __setup_forward
 	lcov --directory $! --ignore-errors gcov,source --base-directory $/ --capture --output-file $!all.info
 	lcov  --remove $!all.info '/usr/*' --output $!lcov.info
 	rm $!all.info
+
+
+www-coverage:: __setup_forward
+	$(msg/rm) "$!"
+	$(RM) -rf $!*
+	$(msg/generate) "$!${REPORT_DIR}"
+	mkdir -p $!${REPORT_DIR}
+	$(msg/generate) "$!"
+	cp -a $/* $!
+	find -type d -name "javascript" -not -path '*/\.*' | while read line ; do \
+		$(msg/generate) "instrumented directory: $!$${line}"; \
+		$(RM) -rf $!$${line}; \
+		rand=`date | md5sum | head -c 9` && istanbul instrument --save-baseline --baseline-file $!${REPORT_DIR}/coverage-$${rand}-baseline.json --complete-copy --no-compact -x "**/ext/**" --output $!$${line} $/$${line} ; \
+	done
+	find -type d -name "ext" -not -path '*/\.*' | grep "javascript" | while read line ; do \
+		$(msg/generate) "external lib $!$${line}"; \
+		cp -r $/$${line} $!$${line}; \
+	done
+	server=`find -not -path '*/\.*' -name coverage-server.js` ; node $$server $!${REPORT_DIR} &
+	-MAKELEVEL=0 $(MAKE) P=$(var/profile) NOCHECK=1 L=1 www-check
+	pkill -f coverage-server
+	$(msg/generate) "report in $!${REPORT_DIR}"
+	istanbul report --root $!${REPORT_DIR} --dir $!${REPORT_DIR}
