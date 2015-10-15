yannickli/lib-common

commit e177703c2e7ee64641b70ee1cdb80269957cb184

tags: allow to index iop abstract local class
abstract local class Date in platform.iop wasn't indexed.

Change-Id: Ie14f58bf37c629bbaa4e120cab406e4b8bbcb5c8
Signed-off-by: Richard Beneyton <richard.beneyton@intersec.com>

Build/bulk.mk
@@ -163,7 +163,7 @@ __setup_buildsys_tags: | __setup_buildsys_trampoline
 	    --langmap=c:+.blk --langmap=c++:+.blkk \
 	    --langdef=iop --langmap=iop:.iop \
 	    --regex-iop='/^struct +([a-zA-Z]+)/\1/s, struct/' \
-	    --regex-iop='/^(abstract +)?class +([a-zA-Z]+)/\2/c, class/' \
+	    --regex-iop='/^(abstract +)(local +)?class +([a-zA-Z]+)/\3/c, class/' \
 	    --regex-iop='/^union +([a-zA-Z]+)/\1/u, union/' \
 	    --regex-iop='/^enum +([a-zA-Z]+)/\1/e, enum/' \
 	    --regex-iop='/^typedef +[^;]+ +([a-zA-Z]+) *;/\1/t, typedef/' \

commit 7f7c30378317fc7427c02706c86a2d4d6f1e79fa

fix tags for IOP classes
Broken by e177703c2e7ee64641b70ee1cdb80269957cb184.

Change-Id: I8add085a136522e5d9f488066a723dd93500eaba

Build/bulk.mk
@@ -187,7 +187,7 @@ __setup_buildsys_tags: | __setup_buildsys_trampoline
 	    --langmap=c:+.blk --langmap=c++:+.blkk \
 	    --langdef=iop --langmap=iop:.iop \
 	    --regex-iop='/^struct +([a-zA-Z]+)/\1/s, struct/' \
-	    --regex-iop='/^(abstract +)(local +)?class +([a-zA-Z]+)/\3/c, class/' \
+	    --regex-iop='/^(abstract +)?(local +)?class +([a-zA-Z]+)/\3/c, class/' \
 	    --regex-iop='/^union +([a-zA-Z]+)/\1/u, union/' \
 	    --regex-iop='/^enum +([a-zA-Z]+)/\1/e, enum/' \
 	    --regex-iop='/^typedef +[^;]+ +([a-zA-Z]+) *;/\1/t, typedef/' \