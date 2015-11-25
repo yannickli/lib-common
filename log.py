yannickli/lib-common

commit d63756a726c7edfb4ee23f11508f20fe1f11c139

Fix pylint warnings.
Have a pylint-clean lib-common.

Change-Id: I7c84749adaf81312c09611eab41a25f7d0c9f703
Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

log.py
@@ -25,7 +25,9 @@ logging.addLevelName("NOTICE", NOTICE)
 class IntersecLogger(logging.Logger):
 
     def __init__(self):
-        super(IntersecLogger, self).__init__("intersec", 0)
+        # The python 2.6 implementation of the logging module use the old
+        # python class model, as a consequence we must not use super() here.
+        logging.Logger.__init__("intersec", 0)
 
     def panic(self, msg, *args, **kwargs):
         self.log(PANIC, msg, *args, **kwargs)

mem_stats.py
@@ -32,7 +32,7 @@ COLUMNS = [
     "max_allocated", "max_unused", "max_used",
     "malloc_calls", "current_used", "current_allocated"
 ]
-POSITION = { s: i for (i, s) in enumerate(COLUMNS) }
+POSITION = dict((s, i) for (i, s) in enumerate(COLUMNS))
 NUM_COLUMNS = len(COLUMNS)
 
 

commit de3fcc0cb0bb3333b4d4e3c075f1d0e6ebdd3aa4

lib-common: fix Python log
d63756a7 introduced a regression. The parent constructor should be called
with self as first argument, else an exception is raised:

"TypeError: unbound method __init__() must be called with Logger instance
as first argument (got str instance instead)"

Change-Id: I741f5610ba96e9854cf38e4ff030e4c2ab98f718

log.py
@@ -27,7 +27,7 @@ class IntersecLogger(logging.Logger):
     def __init__(self):
         # The python 2.6 implementation of the logging module use the old
         # python class model, as a consequence we must not use super() here.
-        logging.Logger.__init__("intersec", 0)
+        logging.Logger.__init__(self, "intersec", 0)
 
     def panic(self, msg, *args, **kwargs):
         self.log(PANIC, msg, *args, **kwargs)