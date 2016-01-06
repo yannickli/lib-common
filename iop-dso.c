yannickli/lib-common

commit 538e22c1512ab56d3e1ef2535aae5795b779a0fc

iop: Safer/smarter packages (un)registration.
A hash tables of packages by names was recently added, but the result of
qm_add was not checked.
We now use qm_put to detect collisions in packages names (which should not
happen), and avoid calling register_class on classes already registered.

Change-Id: I538884dfb4ed05145a481e47e511d7d0419c1942

iop.blk
@@ -3748,8 +3748,18 @@ int iop_register_packages_env(const iop_pkg_t **pkgs, int len, iop_env_t *env,
 {
     for (int i = 0; i < len; i++) {
         const iop_pkg_t *pkg = pkgs[i];
+        uint32_t pos;
 
-        qm_add(iop_pkg, &env->pkgs_by_name, &pkg->name, pkg);
+        pos = qm_put(iop_pkg, &env->pkgs_by_name, &pkg->name, pkg, 0);
+        if (pos & QHASH_COLLISION) {
+            pos ^= QHASH_COLLISION;
+            if (env->pkgs_by_name.values[pos] != pkg) {
+                sb_setf(err, "a package named '%*pM' was already registered",
+                        LSTR_FMT_ARG(pkg->name));
+                return -1;
+            }
+            continue;
+        }
         for (const iop_struct_t *const *it = pkg->structs; *it; it++) {
             const iop_struct_t *desc = *it;
 
@@ -3806,7 +3816,9 @@ void iop_unregister_packages(const iop_pkg_t **pkgs, int len)
     for (int i = 0; i < len; i++) {
         const iop_pkg_t *pkg = pkgs[i];
 
-        qm_del_key(iop_pkg, &_G.env.pkgs_by_name, &pkg->name);
+        if (qm_del_key(iop_pkg, &_G.env.pkgs_by_name, &pkg->name) < 0) {
+            continue;
+        }
         for (const iop_struct_t *const *it = pkg->structs; *it; it++) {
             const iop_struct_t *desc = *it;
 

commit a021c2b8527e6cac1f115c10e63fd966a086df86

iop: Fix loading of multiple DSOs following previous commits.
The consistency check added in 538e22c1512a broke the loading of multiple
DSOs (using python for example). Indeed, this is possible in that case to
have several different packages having the same name, and we had the error
"a package named 'XXX' was already registered".

In order to properly fix that:
* have multiple packages/enums as values of the pkgs_by_name /
  enums_by_fullname hash tables of the environment, since they are not
  necessarily unique.
* Add a flag IOP_REGPKG_FROM_DSO at packages registration, to know if this
  is a package from a DSO (and in that case allow duplicates).

Change-Id: I281ecd63d1d0bb2dc65516891e6a904a1190d5ec

iop-dso.c
@@ -143,7 +143,8 @@ static int iopdso_register_pkg(iop_dso_t *dso, iop_pkg_t const *pkg,
         e_trace(1, "fixup package `%*pM`", LSTR_FMT_ARG(pkg->name));
         iopdso_fix_pkg(pkg);
     }
-    RETHROW(iop_register_packages_env(&pkg, 1, env, err));
+    RETHROW(iop_register_packages_env(&pkg, 1, env, IOP_REGPKG_FROM_DSO,
+                                      err));
     for (const iop_enum_t *const *it = pkg->enums; *it; it++) {
         qm_add(iop_enum, &dso->enum_h, &(*it)->fullname, *it);
     }

iop-priv.h
@@ -61,11 +61,13 @@ qm_kvec_t(class_name, class_name_key_t, const iop_struct_t *,
 /* }}} */
 /* {{{ IOP context */
 
+qm_kvec_t(iop_objs, lstr_t, qv_t(cvoid), qhash_lstr_hash, qhash_lstr_equal);
+
 typedef struct iop_env_t {
     qm_t(class_id)   classes_by_id;
     qm_t(class_name) classes_by_name;
-    qm_t(iop_pkg)    pkgs_by_name;
-    qm_t(iop_enum)   enums_by_fullname;
+    qm_t(iop_objs)   pkgs_by_name;
+    qm_t(iop_objs)   enums_by_fullname;
 } iop_env_t;
 
 iop_env_t *iop_env_init(iop_env_t *env);
@@ -75,7 +77,7 @@ void iop_env_get(iop_env_t *env);
 
 const iop_pkg_t *iop_get_pkg_env(lstr_t pkgname, iop_env_t *env);
 int iop_register_packages_env(const iop_pkg_t **pkgs, int len, iop_env_t *env,
-                              sb_t *err);
+                              unsigned flags, sb_t *err);
 
 /* }}} */
 

iop.blk
@@ -3470,15 +3470,22 @@ static void iop_env_copy(iop_env_t *dst, iop_env_t *src)
         qm_add(class_name, &dst->classes_by_name, &src->classes_by_name.keys[pos],
                src->classes_by_name.values[pos]);
     }
-    qm_for_each_pos(iop_pkg, pos, &src->pkgs_by_name) {
-        qm_add(iop_pkg, &dst->pkgs_by_name, &src->pkgs_by_name.keys[pos],
-               src->pkgs_by_name.values[pos]);
-    }
-    qm_for_each_pos(iop_enum, pos, &src->enums_by_fullname) {
-        qm_add(iop_enum, &dst->enums_by_fullname,
-               &src->enums_by_fullname.keys[pos],
-               src->enums_by_fullname.values[pos]);
-    }
+
+#define COPY_OBJS(_qm)                                                       \
+    do {                                                                     \
+        qm_for_each_pos(iop_objs, pos, &src->_qm) {                          \
+            qv_t(cvoid) vec;                                                 \
+            lstr_t pkg_name = lstr_dup(src->_qm.keys[pos]);                  \
+                                                                             \
+            qv_init(cvoid, &vec);                                            \
+            qv_copy(cvoid, &vec, &src->_qm.values[pos]);                     \
+            qm_add(iop_objs, &dst->_qm, &pkg_name, vec);                     \
+        }                                                                    \
+    } while (0)
+
+    COPY_OBJS(pkgs_by_name);
+    COPY_OBJS(enums_by_fullname);
+#undef COPY_OBJS
 }
 
 void iop_env_set(iop_env_t *env)
@@ -3496,17 +3503,17 @@ iop_env_t *iop_env_init(iop_env_t *env)
 {
     qm_init(class_id,   &env->classes_by_id);
     qm_init(class_name, &env->classes_by_name);
-    qm_init(iop_pkg,    &env->pkgs_by_name);
-    qm_init(iop_enum,   &env->enums_by_fullname);
+    qm_init(iop_objs,   &env->pkgs_by_name);
+    qm_init(iop_objs,   &env->enums_by_fullname);
     return env;
 }
 
 void iop_env_wipe(iop_env_t *env)
 {
-    qm_wipe(class_id,   &env->classes_by_id);
-    qm_wipe(class_name, &env->classes_by_name);
-    qm_wipe(iop_pkg,    &env->pkgs_by_name);
-    qm_wipe(iop_enum,   &env->enums_by_fullname);
+    qm_wipe(class_id,      &env->classes_by_id);
+    qm_wipe(class_name,    &env->classes_by_name);
+    qm_deep_wipe(iop_objs, &env->pkgs_by_name,      lstr_wipe, qv_cvoid_wipe);
+    qm_deep_wipe(iop_objs, &env->enums_by_fullname, lstr_wipe, qv_cvoid_wipe);
 }
 
 /* }}} */
@@ -3807,23 +3814,50 @@ static int iop_check_registered_classes(iop_env_t *env, sb_t *err)
     return 0;
 }
 
+static int
+iop_register_obj_by_name(qm_t(iop_objs) *qm, const void *obj, lstr_t name,
+                         unsigned flags)
+{
+    qv_t(cvoid) *obj_vec;
+    uint32_t pos;
+
+    pos = qm_reserve(iop_objs, qm, &name, 0);
+    if (pos & QHASH_COLLISION) {
+        pos ^= QHASH_COLLISION;
+    } else {
+        qm->keys[pos] = lstr_dup(name);
+        qv_init(cvoid, &qm->values[pos]);
+    }
+    obj_vec = &qm->values[pos];
+    qv_for_each_entry(cvoid, _obj, obj_vec) {
+        if (_obj == obj) {
+            return 0;
+        }
+    }
+    if (obj_vec->len && !(flags & IOP_REGPKG_FROM_DSO)) {
+        return -1;
+    }
+    qv_append(cvoid, obj_vec, obj);
+    return 1;
+}
+
 int iop_register_packages_env(const iop_pkg_t **pkgs, int len, iop_env_t *env,
-                              sb_t *err)
+                              unsigned flags, sb_t *err)
 {
     /* Register the packages. */
     for (int i = 0; i < len; i++) {
         const iop_pkg_t *pkg = pkgs[i];
-        uint32_t pos;
+        int ret;
 
         /* Register the package itself. */
-        pos = qm_put(iop_pkg, &env->pkgs_by_name, &pkg->name, pkg, 0);
-        if (pos & QHASH_COLLISION) {
-            pos ^= QHASH_COLLISION;
-            if (env->pkgs_by_name.values[pos] != pkg) {
-                sb_setf(err, "a package named '%*pM' was already registered",
-                        LSTR_FMT_ARG(pkg->name));
-                return -1;
-            }
+        ret = iop_register_obj_by_name(&env->pkgs_by_name, pkg, pkg->name,
+                                       flags);
+        if (ret < 0) {
+            sb_setf(err, "a package named '%*pM' was already registered",
+                    LSTR_FMT_ARG(pkg->name));
+            return -1;
+        }
+        if (ret == 0) {
             continue;
         }
 
@@ -3840,8 +3874,8 @@ int iop_register_packages_env(const iop_pkg_t **pkgs, int len, iop_env_t *env,
         for (const iop_enum_t *const *it = pkg->enums; *it; it++) {
             const iop_enum_t *desc = *it;
 
-            if (qm_add(iop_enum, &env->enums_by_fullname, &desc->fullname,
-                       desc) < 0)
+            if (iop_register_obj_by_name(&env->enums_by_fullname, desc,
+                                         desc->fullname, flags) < 0)
             {
                 sb_setf(err,
                         "an enumeration named '%*pM' was already registered",
@@ -3854,18 +3888,28 @@ int iop_register_packages_env(const iop_pkg_t **pkgs, int len, iop_env_t *env,
     return iop_check_registered_classes(env, err);
 }
 
-void iop_register_packages(const iop_pkg_t **pkgs, int len)
+void iop_register_packages(const iop_pkg_t **pkgs, int len, unsigned flags)
 {
     SB_1k(err);
 
-    if (iop_register_packages_env(pkgs, len, &_G.env, &err) < 0) {
+    if (iop_register_packages_env(pkgs, len, &_G.env, flags, &err) < 0) {
         e_panic("%*pM", SB_FMT_ARG(&err));
     }
 }
 
+static const void *iop_get_obj(qm_t(iop_objs) *qm, lstr_t name)
+{
+    int32_t pos = qm_find(iop_objs, qm, &name);
+    qv_t(cvoid) *vec;
+
+    THROW_NULL_IF(pos < 0);
+    vec = &qm->values[pos];
+    return vec->len ? vec->tab[0] : NULL;
+}
+
 const iop_pkg_t *iop_get_pkg_env(lstr_t pkgname, iop_env_t *env)
 {
-    return qm_get_def(iop_pkg, &env->pkgs_by_name, &pkgname, NULL);
+    return iop_get_obj(&env->pkgs_by_name, pkgname);
 }
 
 const iop_pkg_t *iop_get_pkg(lstr_t pkgname)
@@ -3875,7 +3919,7 @@ const iop_pkg_t *iop_get_pkg(lstr_t pkgname)
 
 const iop_enum_t *iop_get_enum(lstr_t fullname)
 {
-    return qm_get_def(iop_enum, &_G.env.enums_by_fullname, &fullname, NULL);
+    return iop_get_obj(&_G.env.enums_by_fullname, fullname);
 }
 
 static void iop_unregister_class(const iop_struct_t *desc)
@@ -3899,6 +3943,32 @@ static void iop_unregister_class(const iop_struct_t *desc)
     }
 }
 
+static int
+iop_unregister_obj(qm_t(iop_objs) *qm, const void *obj, lstr_t name)
+{
+    int32_t pos = RETHROW(qm_find(iop_objs, qm, &name));
+    qv_t(cvoid) *vec = &qm->values[pos];
+    bool found = false;
+
+    qv_for_each_pos(cvoid, pos2, vec) {
+        const void *obj2 = vec->tab[pos2];
+
+        if (obj2 == obj) {
+            qv_remove(cvoid, vec, pos2);
+            found = true;
+            break;
+        }
+    }
+
+    if (!vec->len) {
+        qv_wipe(cvoid, vec);
+        lstr_wipe(&qm->keys[pos]);
+        qm_del_at(iop_objs, qm, pos);
+    }
+
+    return found ? 0 : -1;
+}
+
 void iop_unregister_packages(const iop_pkg_t **pkgs, int len)
 {
     SB_1k(err);
@@ -3907,7 +3977,7 @@ void iop_unregister_packages(const iop_pkg_t **pkgs, int len)
         const iop_pkg_t *pkg = pkgs[i];
 
         /* Unregister the package itself. */
-        if (qm_del_key(iop_pkg, &_G.env.pkgs_by_name, &pkg->name) < 0) {
+        if (iop_unregister_obj(&_G.env.pkgs_by_name, pkg, pkg->name) < 0) {
             continue;
         }
 
@@ -3924,7 +3994,8 @@ void iop_unregister_packages(const iop_pkg_t **pkgs, int len)
         for (const iop_enum_t *const *it = pkg->enums; *it; it++) {
             const iop_enum_t *desc = *it;
 
-            qm_del_key(iop_enum, &_G.env.enums_by_fullname, &desc->fullname);
+            iop_unregister_obj(&_G.env.enums_by_fullname, desc,
+                               desc->fullname);
         }
     }
 
@@ -3935,8 +4006,10 @@ void iop_unregister_packages(const iop_pkg_t **pkgs, int len)
 
 void iop_for_each_registered_pkgs(iop_for_each_pkg_b cb)
 {
-    qm_for_each_pos(iop_pkg, pos, &_G.env.pkgs_by_name) {
-        cb(_G.env.pkgs_by_name.values[pos]);
+    qm_for_each_pos(iop_objs, pos, &_G.env.pkgs_by_name) {
+        qv_for_each_entry(cvoid, pkg, &_G.env.pkgs_by_name.values[pos]) {
+            cb(pkg);
+        }
     }
 }
 

iop.h
@@ -1421,6 +1421,10 @@ qm_kvec_t(iop_pkg, lstr_t, const iop_pkg_t *,
 
 const iop_pkg_t *iop_get_pkg(lstr_t pkgname);
 
+enum iop_register_packages_flags {
+    IOP_REGPKG_FROM_DSO = (1U << 0),
+};
+
 /** Register a list of packages.
  *
  * Registering a package is necessary if it contains classes; this should be
@@ -1430,7 +1434,7 @@ const iop_pkg_t *iop_get_pkg(lstr_t pkgname);
  *
  * You can use IOP_REGISTER_PACKAGES to avoid the array construction.
  */
-void iop_register_packages(const iop_pkg_t **pkgs, int len);
+void iop_register_packages(const iop_pkg_t **pkgs, int len, unsigned flags);
 
 /** Helper to register a list of packages.
  *
@@ -1440,7 +1444,7 @@ void iop_register_packages(const iop_pkg_t **pkgs, int len);
 #define IOP_REGISTER_PACKAGES(...)  \
     do {                                                                     \
         const iop_pkg_t *__pkgs[] = { __VA_ARGS__ };                         \
-        iop_register_packages(__pkgs, countof(__pkgs));                      \
+        iop_register_packages(__pkgs, countof(__pkgs), 0);                   \
     } while (0)
 
 /** Unregister a list of packages.