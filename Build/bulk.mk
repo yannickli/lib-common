a34bd34120129604e168456ff2feb12f31348d84
Alexis Brasy <alexis.brasy@intersec.com>
2016-08-29 12:01:40 +0200

    ctags: handle modules
    C modules are now fully handled.
    Tags on MODULE_DECLARE already worked (I don't know how) and this commit
    adds support for MODULE_BEGIN.
    
    Change-Id: I7de8ff8baa3b6c84b2c79c1643bdd73a8087fa52

--- Build/bulk.mk
+++ Build/bulk.mk
@@ -238,6 +238,7 @@ __setup_buildsys_tags: | __setup_buildsys_trampoline
 	    --regex-c='/^    .*\(\*+([^\ ]+)\)\([a-zA-Z_]+ /\1/x, cmethod/' \
 	    --regex-c='/^qvector_t\(+([a-zA-Z_]+)\,/\1/t, qvector/' \
 	    --regex-c='/^q[hm]_k.*_t\(+([a-zA-Z_]+)/\1/t, qhash/' \
+	    --regex-c='/^MODULE_BEGIN\(+([a-zA-Z_]+)/\1/m, module/' \
 	    --langdef=iop --langmap=iop:.iop \
 	    --regex-iop='/^struct +([a-zA-Z]+)/\1/s, struct/' \
 	    --regex-iop='/^(abstract +)?(local +)?class +([a-zA-Z]+)/\3/c, class/' \
