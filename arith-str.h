e10c98f9b5563616b45c70f1265f1f909dc8a04c
Florent Bruneau <florent.bruneau@intersec.com>
2016-12-14 14:05:53 +0100

    arith: use nullability attributes.
    Change-Id: I85f038df0a161bde51af14449e051c4586df89e3
    Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

--- arith-str.h
+++ arith-str.h
@@ -30,91 +30,171 @@
         return 0;                                   \
     } while (0)
 
-static inline uint16_t __ps_get_be16(pstream_t *ps) { __PS_GET(ps, 16, be); }
-static inline uint32_t __ps_get_be24(pstream_t *ps) { __PS_GET(ps, 24, be); }
-static inline uint32_t __ps_get_be32(pstream_t *ps) { __PS_GET(ps, 32, be); }
-static inline uint64_t __ps_get_be48(pstream_t *ps) { __PS_GET(ps, 48, be); }
-static inline uint64_t __ps_get_be64(pstream_t *ps) { __PS_GET(ps, 64, be); }
+static inline uint16_t __ps_get_be16(pstream_t * nonnull ps)
+{
+    __PS_GET(ps, 16, be);
+}
+static inline uint32_t __ps_get_be24(pstream_t * nonnull ps)
+{
+    __PS_GET(ps, 24, be);
+}
+static inline uint32_t __ps_get_be32(pstream_t * nonnull ps)
+{
+    __PS_GET(ps, 32, be);
+}
+static inline uint64_t __ps_get_be48(pstream_t * nonnull ps)
+{
+    __PS_GET(ps, 48, be);
+}
+static inline uint64_t __ps_get_be64(pstream_t * nonnull ps)
+{
+    __PS_GET(ps, 64, be);
+}
 
-static inline uint16_t __ps_get_le16(pstream_t *ps) { __PS_GET(ps, 16, le); }
-static inline uint32_t __ps_get_le24(pstream_t *ps) { __PS_GET(ps, 24, le); }
-static inline uint32_t __ps_get_le32(pstream_t *ps) { __PS_GET(ps, 32, le); }
-static inline uint64_t __ps_get_le48(pstream_t *ps) { __PS_GET(ps, 48, le); }
-static inline uint64_t __ps_get_le64(pstream_t *ps) { __PS_GET(ps, 64, le); }
+static inline uint16_t __ps_get_le16(pstream_t * nonnull ps)
+{
+    __PS_GET(ps, 16, le);
+}
+static inline uint32_t __ps_get_le24(pstream_t * nonnull ps)
+{
+    __PS_GET(ps, 24, le);
+}
+static inline uint32_t __ps_get_le32(pstream_t * nonnull ps)
+{
+    __PS_GET(ps, 32, le);
+}
+static inline uint64_t __ps_get_le48(pstream_t * nonnull ps)
+{
+    __PS_GET(ps, 48, le);
+}
+static inline uint64_t __ps_get_le64(pstream_t * nonnull ps)
+{
+    __PS_GET(ps, 64, le);
+}
 
-static inline uint16_t __ps_get_cpu16(pstream_t *ps) {
+static inline uint16_t __ps_get_cpu16(pstream_t * nonnull ps)
+{
     uint16_t res = get_unaligned_cpu16(ps->p);
     __ps_skip(ps, 16 / 8);
     return res;
 }
-static inline uint32_t __ps_get_cpu32(pstream_t *ps) {
+static inline uint32_t __ps_get_cpu32(pstream_t * nonnull ps)
+{
     uint32_t res = get_unaligned_cpu32(ps->p);
     __ps_skip(ps, 32 / 8);
     return res;
 }
-static inline uint64_t __ps_get_cpu64(pstream_t *ps) {
+static inline uint64_t __ps_get_cpu64(pstream_t * nonnull ps)
+{
     uint64_t res = get_unaligned_cpu64(ps->p);
     __ps_skip(ps, 64 / 8);
     return res;
 }
 
-static inline float __ps_get_float_le(pstream_t *ps) {
+static inline float __ps_get_float_le(pstream_t * nonnull ps)
+{
     float res = get_unaligned_float_le(ps->p);
     __ps_skip(ps, sizeof(float));
     return res;
 }
-static inline int ps_get_float_le(pstream_t *ps, float *out) {
+static inline int ps_get_float_le(pstream_t * nonnull ps, float * nonnull out)
+{
     PS_WANT(ps_has(ps, sizeof(float)));
     *out = __ps_get_float_le(ps);
     return 0;
 }
-static inline double __ps_get_double_le(pstream_t *ps) {
+static inline double __ps_get_double_le(pstream_t * nonnull ps)
+{
     double res = get_unaligned_double_le(ps->p);
     __ps_skip(ps, sizeof(double));
     return res;
 }
-static inline int ps_get_double_le(pstream_t *ps, double *out) {
+static inline int ps_get_double_le(pstream_t * nonnull ps, double * nonnull out)
+{
     PS_WANT(ps_has(ps, sizeof(double)));
     *out = __ps_get_double_le(ps);
     return 0;
 }
 
-static inline float __ps_get_float_be(pstream_t *ps) {
+static inline float __ps_get_float_be(pstream_t * nonnull ps)
+{
     float res = get_unaligned_float_be(ps->p);
     __ps_skip(ps, sizeof(float));
     return res;
 }
-static inline int ps_get_float_be(pstream_t *ps, float *out) {
+static inline int ps_get_float_be(pstream_t * nonnull ps, float * nonnull out)
+{
     PS_WANT(ps_has(ps, sizeof(float)));
     *out = __ps_get_float_be(ps);
     return 0;
 }
-static inline double __ps_get_double_be(pstream_t *ps) {
+static inline double __ps_get_double_be(pstream_t * nonnull ps)
+{
     double res = get_unaligned_double_be(ps->p);
     __ps_skip(ps, sizeof(double));
     return res;
 }
-static inline int ps_get_double_be(pstream_t *ps, double *out) {
+static inline int ps_get_double_be(pstream_t * nonnull ps, double * nonnull out)
+{
     PS_WANT(ps_has(ps, sizeof(double)));
     *out = __ps_get_double_be(ps);
     return 0;
 }
 
-static inline int ps_get_be16(pstream_t *ps, uint16_t *res) { PS_GET(ps, res, 16, be); }
-static inline int ps_get_be24(pstream_t *ps, uint32_t *res) { PS_GET(ps, res, 24, be); }
-static inline int ps_get_be32(pstream_t *ps, uint32_t *res) { PS_GET(ps, res, 32, be); }
-static inline int ps_get_be48(pstream_t *ps, uint64_t *res) { PS_GET(ps, res, 48, be); }
-static inline int ps_get_be64(pstream_t *ps, uint64_t *res) { PS_GET(ps, res, 64, be); }
+static inline int ps_get_be16(pstream_t * nonnull ps, uint16_t * nonnull res)
+{
+    PS_GET(ps, res, 16, be);
+}
+static inline int ps_get_be24(pstream_t * nonnull ps, uint32_t * nonnull res)
+{
+    PS_GET(ps, res, 24, be);
+}
+static inline int ps_get_be32(pstream_t * nonnull ps, uint32_t * nonnull res)
+{
+    PS_GET(ps, res, 32, be);
+}
+static inline int ps_get_be48(pstream_t * nonnull ps, uint64_t * nonnull res)
+{
+    PS_GET(ps, res, 48, be);
+}
+static inline int ps_get_be64(pstream_t * nonnull ps, uint64_t * nonnull res)
+{
+    PS_GET(ps, res, 64, be);
+}
 
-static inline int ps_get_le16(pstream_t *ps, uint16_t *res) { PS_GET(ps, res, 16, le); }
-static inline int ps_get_le24(pstream_t *ps, uint32_t *res) { PS_GET(ps, res, 24, le); }
-static inline int ps_get_le32(pstream_t *ps, uint32_t *res) { PS_GET(ps, res, 32, le); }
-static inline int ps_get_le48(pstream_t *ps, uint64_t *res) { PS_GET(ps, res, 48, le); }
-static inline int ps_get_le64(pstream_t *ps, uint64_t *res) { PS_GET(ps, res, 64, le); }
+static inline int ps_get_le16(pstream_t * nonnull ps, uint16_t * nonnull res)
+{
+    PS_GET(ps, res, 16, le);
+}
+static inline int ps_get_le24(pstream_t * nonnull ps, uint32_t * nonnull res)
+{
+    PS_GET(ps, res, 24, le);
+}
+static inline int ps_get_le32(pstream_t * nonnull ps, uint32_t * nonnull res)
+{
+    PS_GET(ps, res, 32, le);
+}
+static inline int ps_get_le48(pstream_t * nonnull ps, uint64_t * nonnull res)
+{
+    PS_GET(ps, res, 48, le);
+}
+static inline int ps_get_le64(pstream_t * nonnull ps, uint64_t * nonnull res)
+{
+    PS_GET(ps, res, 64, le);
+}
 
-static inline int ps_get_cpu16(pstream_t *ps, uint16_t *res) { PS_GET(ps, res, 16, cpu); }
-static inline int ps_get_cpu32(pstream_t *ps, uint32_t *res) { PS_GET(ps, res, 32, cpu); }
-static inline int ps_get_cpu64(pstream_t *ps, uint64_t *res) { PS_GET(ps, res, 64, cpu); }
+static inline int ps_get_cpu16(pstream_t * nonnull ps, uint16_t * nonnull res)
+{
+    PS_GET(ps, res, 16, cpu);
+}
+static inline int ps_get_cpu32(pstream_t * nonnull ps, uint32_t * nonnull res)
+{
+    PS_GET(ps, res, 32, cpu);
+}
+static inline int ps_get_cpu64(pstream_t * nonnull ps, uint64_t * nonnull res)
+{
+    PS_GET(ps, res, 64, cpu);
+}
 
 #undef __PS_GET
 #undef PS_GET
@@ -122,21 +202,60 @@ static inline int ps_get_cpu64(pstream_t *ps, uint64_t *res) { PS_GET(ps, res, 6
 #define SB_ADD(sb, u, r, endianess) \
     put_unaligned_##endianess##r(sb_growlen(sb, r / 8), u)
 
-static inline void sb_add_be16(sb_t *sb, uint16_t u) { SB_ADD(sb, u, 16, be); }
-static inline void sb_add_be24(sb_t *sb, uint32_t u) { SB_ADD(sb, u, 24, be); }
-static inline void sb_add_be32(sb_t *sb, uint32_t u) { SB_ADD(sb, u, 32, be); }
-static inline void sb_add_be48(sb_t *sb, uint64_t u) { SB_ADD(sb, u, 48, be); }
-static inline void sb_add_be64(sb_t *sb, uint64_t u) { SB_ADD(sb, u, 64, be); }
+static inline void sb_add_be16(sb_t * nonnull sb, uint16_t u)
+{
+    SB_ADD(sb, u, 16, be);
+}
+static inline void sb_add_be24(sb_t * nonnull sb, uint32_t u)
+{
+    SB_ADD(sb, u, 24, be);
+}
+static inline void sb_add_be32(sb_t * nonnull sb, uint32_t u)
+{
+    SB_ADD(sb, u, 32, be);
+}
+static inline void sb_add_be48(sb_t * nonnull sb, uint64_t u)
+{
+    SB_ADD(sb, u, 48, be);
+}
+static inline void sb_add_be64(sb_t * nonnull sb, uint64_t u)
+{
+    SB_ADD(sb, u, 64, be);
+}
 
-static inline void sb_add_le16(sb_t *sb, uint16_t u) { SB_ADD(sb, u, 16, le); }
-static inline void sb_add_le24(sb_t *sb, uint32_t u) { SB_ADD(sb, u, 24, le); }
-static inline void sb_add_le32(sb_t *sb, uint32_t u) { SB_ADD(sb, u, 32, le); }
-static inline void sb_add_le48(sb_t *sb, uint64_t u) { SB_ADD(sb, u, 48, le); }
-static inline void sb_add_le64(sb_t *sb, uint64_t u) { SB_ADD(sb, u, 64, le); }
+static inline void sb_add_le16(sb_t * nonnull sb, uint16_t u)
+{
+    SB_ADD(sb, u, 16, le);
+}
+static inline void sb_add_le24(sb_t * nonnull sb, uint32_t u)
+{
+    SB_ADD(sb, u, 24, le);
+}
+static inline void sb_add_le32(sb_t * nonnull sb, uint32_t u)
+{
+    SB_ADD(sb, u, 32, le);
+}
+static inline void sb_add_le48(sb_t * nonnull sb, uint64_t u)
+{
+    SB_ADD(sb, u, 48, le);
+}
+static inline void sb_add_le64(sb_t * nonnull sb, uint64_t u)
+{
+    SB_ADD(sb, u, 64, le);
+}
 
-static inline void sb_add_cpu16(sb_t *sb, uint16_t u) { SB_ADD(sb, u, 16, cpu); }
-static inline void sb_add_cpu32(sb_t *sb, uint32_t u) { SB_ADD(sb, u, 32, cpu); }
-static inline void sb_add_cpu64(sb_t *sb, uint64_t u) { SB_ADD(sb, u, 64, cpu); }
+static inline void sb_add_cpu16(sb_t * nonnull sb, uint16_t u)
+{
+    SB_ADD(sb, u, 16, cpu);
+}
+static inline void sb_add_cpu32(sb_t * nonnull sb, uint32_t u)
+{
+    SB_ADD(sb, u, 32, cpu);
+}
+static inline void sb_add_cpu64(sb_t * nonnull sb, uint64_t u)
+{
+    SB_ADD(sb, u, 64, cpu);
+}
 
 #undef SB_ADD
 
