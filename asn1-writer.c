6c2a69bdda1941ad41688162eb4cdc99c00e9de5
Xavier Chantry <xavier.chantry@intersec.com>
2017-01-24 17:59:10 +0100

    asn1-writer: add assert to catch buggy usage
    I was passing an invalid choice tag which caused an invalid array access,
    but this was not caught by the asn1 lib. At least add an assert.
    
    Change-Id: I1ea2aec48d65a5d7fbc1c7211a264f0d42a1a0a7

--- asn1-writer.c
+++ asn1-writer.c
@@ -453,6 +453,7 @@ static int asn1_pack_choice_size(const void *st, const asn1_desc_t *desc,
     assert (enum_spec->type == ASN1_OBJ_TYPE(enum));
 
     choice = *GET_DATA_P(st, enum_spec, int);
+    assert (choice > 0 && choice < desc->vec.len);
     choice_spec = &desc->vec.tab[choice];
 
     RETHROW(asn1_pack_field_size(st, choice_spec, stack, &len));
@@ -651,6 +652,7 @@ static uint8_t *asn1_pack_choice(uint8_t *dst, const void *st,
     assert (enum_spec->type == ASN1_OBJ_TYPE(enum));
 
     choice = *GET_DATA_P(st, enum_spec, int);
+    assert (choice > 0 && choice < desc->vec.len);
     choice_spec = &desc->vec.tab[choice];
 
     e_trace_desc(1, "serializing", desc, choice, depth);
