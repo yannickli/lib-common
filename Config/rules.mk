d0d9f16ead7324262ba5e4e6dcdbd2951cf57846
Florent Bruneau <florent.bruneau@intersec.com>
2017-01-24 14:17:48 +0100

    Build: factorize IOP building code.
    Running the IOP generator twice is a bit annoying. I'm not quite sure this
    has no unwanted side effects, though.
    
    Change-Id: I1ce25d79c3be3eadad97f15769ca9ef6569c18be
    Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

--- Config/rules.mk
+++ Config/rules.mk
@@ -70,24 +70,12 @@ define ext/expand/iop
 $3.h: $3.c
 $3.json: $3.c
 $3.swift: $3.c
-$~$3.dep: IOPINCPATH_=$l $(var/platform)/lib-inet $(var/platform)/qrrd/iop $($(1DV)_IOPINCPATH) $($1_IOPINCPATH) $($3_IOPINCPATH)
-$~$3.dep: IOPJSONPATH_=$(firstword $($(1DV)_IOPJSONPATH) $($1_IOPJSONPATH) $($3_IOPJSONPATH) $(1DV))
-$~$3.dep: IOPCLASSRANGE_=$(firstword $($(1DV)_IOPCLASSRANGE) $($1_IOPCLASSRANGE) $($3_IOPCLASSRANGE))
-$~$3.dep: IOPSWIFTDEPS_=$(firstword $($(1DV)_SWIFTDEPS) $($1_SWIFTDEPS) $($3_SWIFTDEPS))
-$~$3.dep: IOPVER_= $(firstword $($3_IOPVER) $($1_IOPVER) $($(1DV)_IOPVER) $(IOPVER))
-$~$3.dep: $3 $(IOPC)
-	$(msg/COMPILE.iop) $3
-	$(RM) $$@
-	$(IOPC) $$(IOPVER_) $(IOPFLAGS) -l c,json$(if $(SWIFTC),$(var/comma)swift) \
-	    -d$~$$<.dep -I$$(call fun/join,:,$$(IOPINCPATH_)) --json-output-path $$(IOPJSONPATH_) \
-	    $$(if $$(IOPCLASSRANGE_),--class-id-range $$(IOPCLASSRANGE_)) \
-	    $(if $(SWIFTC),$$(if $$(IOPSWIFTDEPS_),--swift-import-modules $$(IOPSWIFTDEPS_))) $$<
-$3.c: IOPINCPATH_=$l $(var/platform)/lib-inet $(var/platform)/qrrd/iop $($(1DV)_IOPINCPATH) $($1_IOPINCPATH) $($3_IOPINCPATH)
-$3.c: IOPJSONPATH_=$(firstword $($(1DV)_IOPJSONPATH) $($1_IOPJSONPATH) $($3_IOPJSONPATH) $(1DV))
-$3.c: IOPCLASSRANGE_=$(firstword $($(1DV)_IOPCLASSRANGE) $($1_IOPCLASSRANGE) $($3_IOPCLASSRANGE))
-$3.c: IOPSWIFTDEPS_=$(firstword $($(1DV)_SWIFTDEPS) $($1_SWIFTDEPS) $($3_SWIFTDEPS))
-$3.c: IOPVER_= $(firstword $($3_IOPVER) $($1_IOPVER) $($(1DV)_IOPVER) $(IOPVER))
-$3.c: $3 $(IOPC)
+$~$3.dep $3.c: IOPINCPATH_=$l $(var/platform)/lib-inet $(var/platform)/qrrd/iop $($(1DV)_IOPINCPATH) $($1_IOPINCPATH) $($3_IOPINCPATH)
+$~$3.dep $3.c: IOPJSONPATH_=$(firstword $($(1DV)_IOPJSONPATH) $($1_IOPJSONPATH) $($3_IOPJSONPATH) $(1DV))
+$~$3.dep $3.c: IOPCLASSRANGE_=$(firstword $($(1DV)_IOPCLASSRANGE) $($1_IOPCLASSRANGE) $($3_IOPCLASSRANGE))
+$~$3.dep $3.c: IOPSWIFTDEPS_=$(firstword $($(1DV)_SWIFTDEPS) $($1_SWIFTDEPS) $($3_SWIFTDEPS))
+$~$3.dep $3.c: IOPVER_= $(firstword $($3_IOPVER) $($1_IOPVER) $($(1DV)_IOPVER) $(IOPVER))
+$~$3.dep $3.c: $3 $(IOPC)
 	$(msg/COMPILE.iop) $3
 	$(RM) $$@
 	$(IOPC) $$(IOPVER_) $(IOPFLAGS) -l c,json$(if $(SWIFTC),$(var/comma)swift) \
