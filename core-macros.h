yannickli/lib-common

commit 9f5736121c91c9bdd37cac7f18375c974002845a

core: use C11 _Static_assert() when available.
_Static_assert() is available in compilers that support C11 features. On
GCC it appears to be treated as a declaration statement, which forbid its
usage in the middle of a block (because we forbid mixed declarations and
statements), that's why the STATIC_ASSERT() macro wraps the
_Static_assert() statement in a do {} while (0)

Additionally, __error__(msg) is also defined as an empty macro in that
case. It could have been implemented as a _Static_assert(false, msg),
however this breaks asn1-macros that relies on the compiler dead-code
elimination pass to detect unsupported fields but that pass is executed
after the evaluation of the static assertions.

Change-Id: I07a0fefb0698d159c313c69e8189e582d928c5de
Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

core-macros.h
@@ -156,7 +156,13 @@
  * \safemacro
  *
  */
-#ifndef __cplusplus
+#if __has_feature(c_static_assert) || \
+    (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L)
+# define __error__(msg)
+# define STATIC_ASSERT(cond)  do { _Static_assert(cond, #cond); } while (0)
+#   define ASSERT_COMPATIBLE(e1, e2) \
+     STATIC_ASSERT(__builtin_types_compatible_p(typeof(e1), typeof(e2)))
+#elif !defined(__cplusplus)
 # ifdef __GNUC__
 #   define __error__(msg)          (void)({__asm__(".error \""msg"\"");})
 #   define STATIC_ASSERT(cond) \

commit 74f000e3fa33a7c442f279546e581f3ea40a7b56

fix definition of __error__ when C11 is available
A regression was introduced in 9f5736121c91c9bdd37cac7f18375c974002845a
when dealing with the support of static asserts in C11. The __error__
needs to keep the same definition as with __GNUC__ defined, otherwise
all the code relying on __error__ to abort the compilation don't work
anymore (like the ASN.1 library).

Change-Id: Icc6ba8eef7a75303212f0fed87f16e9a17b4da11

core-macros.h
@@ -158,7 +158,7 @@
  */
 #if __has_feature(c_static_assert) || \
     (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L)
-# define __error__(msg)
+# define __error__(msg)       (void)({__asm__(".error \""msg"\"");})
 # define STATIC_ASSERT(cond)  do { _Static_assert(cond, #cond); } while (0)
 #   define ASSERT_COMPATIBLE(e1, e2) \
      STATIC_ASSERT(__builtin_types_compatible_p(typeof(e1), typeof(e2)))