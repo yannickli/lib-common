d3e89244a9d02bcf289d7cf6aafb2e64ee6a0131
Florent Bruneau <florent.bruneau@intersec.com>
2017-03-08 15:05:02 +0100

    Build: fix build from scratch.
    Change-Id: I30a656f66653c1b09dccbfd6acd7a586e33e576e
    Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

--- Build/rules-frontend.mk
+++ Build/rules-frontend.mk
@@ -17,10 +17,12 @@
 #
 # {{{ css
 
+$(var/wwwtool)lessc: _npm_tools
+
 ext/gen/css = $(if $(filter %.css,$1),$(strip $($2_DESTDIR))/$(notdir $(1:css=min.css)))
 
 define ext/expand/css
-$(strip $($1_DESTDIR))/$(notdir $(3:css=min.css)): $3 $(var/wwwtool)lessc | _npm_tools
+$(strip $($1_DESTDIR))/$(notdir $(3:css=min.css)): $3 $(var/wwwtool)lessc
 	$(msg/MINIFY.css) $3
 	mkdir -p `dirname "$~$$@"`
 	$(var/wwwtool)lessc -M $$< $$@ > $~$$@.d
@@ -41,7 +43,7 @@ endef
 ext/gen/less = $(if $(filter %.less,$1),$(strip $($2_DESTDIR))/$(notdir $(1:less=css)) $(call ext/gen/css,$(strip $($2_DESTDIR))/$(notdir $(1:less=css)),$2))
 
 define ext/expand/less
-$(strip $($1_DESTDIR))/$(notdir $(3:less=css)): $3 $(var/wwwtool)lessc | _npm_tools
+$(strip $($1_DESTDIR))/$(notdir $(3:less=css)): $3 $(var/wwwtool)lessc
 	$(msg/COMPILE.less) $3
 	mkdir -p `dirname "$~$$@"`
 	$(var/wwwtool)lessc -M $$< $$@ > $~$$@.d
