49096a8d43f9d257e0460b7ba2b8dadc5e145cc2
Romain Le Godais <romain.legodais@intersec.com>
2017-04-03 16:04:52 +0200

    iopc: Fix error case in parse_field_stmt.
    'f' was added too soon in st->fields: if the last check of the function
    failed (about the tags used in double), the field was added in a vector,
    and then deleted.
    
    Change-Id: I4579c73d7febedcae2604eef845e29d6a1f34377

--- iopc/iopc-parser.c
+++ iopc/iopc-parser.c
@@ -2082,7 +2082,6 @@ parse_field_stmt(iopc_parser_t *pp, iopc_struct_t *st, qv_t(iopc_attr) *attrs,
         qv_append(iopc_field, &st->static_fields, f);
         return f;
     }
-    qv_append(iopc_field, &st->fields, f);
 
     qv_for_each_entry(i32, t, tags) {
         if (t == tag) {
@@ -2091,6 +2090,7 @@ parse_field_stmt(iopc_parser_t *pp, iopc_struct_t *st, qv_t(iopc_attr) *attrs,
         }
     }
     qv_append(i32, tags, tag);
+    qv_append(iopc_field, &st->fields, f);
     return f;
 
   error:
