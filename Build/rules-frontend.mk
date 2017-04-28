27c0a359e2a5fbc52365708a5471d3a4accefd63
Florent Bruneau <florent.bruneau@intersec.com>
2017-04-28 17:36:16 +0200

    Build: force the use of a local up-to-date npm.
    Instead of relying on the npm available on the system, we now force the
    installation of a version of npm that will be suitable for installing
    the @types/* stuff.
    
    This will require a longer deployment time, but should ensure we don't
    depend on the system anymore.
    
    Change-Id: Idc44ac1f7142a6f1feea009d31dd1c1386d7a9bd
    Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

--- Build/rules-frontend.mk
+++ Build/rules-frontend.mk
@@ -23,14 +23,18 @@ $(var/wwwtool)r.js: _npm_tools
 $(var/wwwtool)browserify: _npm_tools
 $(var/wwwtool)exorcist: _npm_tools
 
-$/$~package-installed.json: $/package.json
+$(var/wwwtool)npm:
+	$(msg/npm) npm
+	cd $/ && npm install npm --silent > /dev/null
+
+$/$~package-installed.json: $/package.json $(var/wwwtool)npm
 	$(msg/npm) ""
-	cd $(dir $<) && npm install --silent > /dev/null
+	cd $(dir $<) && $(var/wwwtool)npm install --silent > /dev/null
 	cp $< $@
 
-$/$~%package-installed.json: $/%package.json
+$/$~%package-installed.json: $/%package.json $(var/wwwtool)npm
 	$(msg/npm) "$(patsubst $/%/package.json,%,$<)"
-	cd $(dir $<) && npm install --silent > /dev/null
+	cd $(dir $<) && $(var/wwwtool)npm install --silent > /dev/null
 	cp $< $@
 
 # }}}
