1eae0a983ef0734d3963d2c436bb2212e32a5854
Florent Bruneau <florent.bruneau@intersec.com>
2017-05-04 08:27:47 +0200

    Build: disable tslint.
    This requires a version of node that supports ES6.
    
    Change-Id: I02771f50a30143bd59acacd920dcd7b718afd479
    Signed-off-by: Florent Bruneau <florent.bruneau@intersec.com>

--- Build/bulk.mk
+++ Build/bulk.mk
@@ -288,7 +288,7 @@ tslint: | __setup_buildsys_trampoline _npm_tools
 	$(msg/CHECK.ts)
 	git ls-files -- '*.ts' | xargs -r $(var/wwwtool)tslint --project $/platform/www/modules/core/node_modules/tsconfig.json --type-check --fix
 
-www:: $(if $(NOCHECK)$(NOJSHINT),,jshint tslint)
+www:: $(if $(NOCHECK)$(NOJSHINT),,jshint)
 
 pylint:: | __setup_buildsys_trampoline
 	@$(if $(shell which pylint),,$(error "Please install pylint: pip install pylint"))
