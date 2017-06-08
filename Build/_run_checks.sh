d10702001eb14920ae4bd3f52c1094646770650b
Florent Bruneau <florent.bruneau@intersec.com>
2017-06-08 13:12:01 +0200

    Revert "Build: use testem from the repository."
    This reverts commit 53a8998b739c05c8cef83b242c636c52a874680d.

--- Build/_run_checks.sh
+++ Build/_run_checks.sh
@@ -160,7 +160,6 @@ done
 export BEHAVE_FLAGS="${BEHAVE_FLAGS} ${BEHAVE_TAGS} --format z --no-summary --no-capture-stderr"
 
 coredump=$(which core_dump)
-ROOT_PATH=$PWD
 
 while read -r zd line; do
     t="${zd}${line}"
@@ -185,7 +184,7 @@ while read -r zd line; do
             ;;
         *testem.json)
             cd $zd
-            PATH=$ROOT_PATH/node_modules/.bin:$PATH ztestem $line
+            ztestem $line
             res=$?
             cd - &>/dev/null
             ;;
