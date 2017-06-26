6dd545f8f061766dd6e426cfd809f9f0d5170686
Romain Le Godais <romain.legodais@intersec.com>
2017-06-26 12:21:01 +0200

    iopc: Fix possible invalid read of iopc_typer_g.resolve_list.
    When the resolution (first or second passed) failed, the
    iopc_typer_g.resolve_list vector was left untouched, and its elements were
    reused when compiling the next IOP file.
    
    This never happens in the standalone iopc binary, but could easily cause
    crashes when using the iopc library (for example in one-core, when
    updating the schema).
    
    Change-Id: Ice9ccfe90990c0e7e58a822eff1a6f9b1d66491a

--- iopc/iopc.c
+++ iopc/iopc.c
@@ -241,18 +241,18 @@ int main(int argc, char **argv)
         argv = (char **)only_stdin;
     }
     for (int i = 0; i < argc; i++) {
-        iopc_parser_initialize();
+        iopc_parser_typer_initialize();
 
         if (!(pkg = iopc_parse_file(&incpath, NULL, argv[i], NULL, true))) {
-            iopc_parser_shutdown();
+            iopc_parser_typer_shutdown();
             goto error;
         }
         if (iopc_resolve(pkg) < 0) {
-            iopc_parser_shutdown();
+            iopc_parser_typer_shutdown();
             goto error;
         }
         if (iopc_resolve_second_pass(pkg) < 0) {
-            iopc_parser_shutdown();
+            iopc_parser_typer_shutdown();
             goto error;
         }
 
@@ -260,12 +260,12 @@ int main(int argc, char **argv)
 
         qv_for_each_ptr(doit, doit, &doits) {
             if ((*doit->cb)(pkg, doit->outpath, &deps) < 0) {
-                iopc_parser_shutdown();
+                iopc_parser_typer_shutdown();
                 goto error;
             }
         }
 
-        iopc_parser_shutdown();
+        iopc_parser_typer_shutdown();
     }
 
     if (opts.depends) {
