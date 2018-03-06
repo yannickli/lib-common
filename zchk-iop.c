yannickli/lib-common

commit 2e8b53e418deee248af7ace92bc710e5065e2172

iop: factorize code for mp_iop_dup_desc_sz and mp_iop_copy_desc_sz.
The functions were almost the same.

Refs: #57751
Change-Id: If03d5c013898f5f8e8ffef6c28db51f063f7e1b4

iop.blk
@@ -1508,8 +1508,9 @@ static uint8_t *__iop_copy_class(const iop_struct_t *st, uint8_t *dst,
     return dst;
 }
 
-void *mp_iop_dup_desc_sz(mem_pool_t *mp, const iop_struct_t *st,
-                         const void *v, size_t * nullable psz)
+static void *__mp_iop_dup_desc_sz(mem_pool_t *mp, const iop_struct_t *st,
+                                  void *nullable *outp, const void *v,
+                                  size_t *nullable psz)
 {
     bool is_class;
     size_t sz;
@@ -1527,7 +1528,12 @@ void *mp_iop_dup_desc_sz(mem_pool_t *mp, const iop_struct_t *st,
         *psz = sz;
     }
 
-    res = mp_imalloc(mp, sz, 8, MEM_RAW);
+    if (outp) {
+        res = *outp;
+        res = mp_irealloc(mp, res, 0, sz, 8, MEM_RAW);
+    } else {
+        res = mp_imalloc(mp, sz, 8, MEM_RAW);
+    }
 
     if (is_class) {
         dst = __iop_copy_class(st, res, res, v);
@@ -1540,40 +1546,17 @@ void *mp_iop_dup_desc_sz(mem_pool_t *mp, const iop_struct_t *st,
     return res;
 }
 
-void mp_iop_copy_desc_sz(mem_pool_t *mp, const iop_struct_t *st, void **outp,
-                         const void *v, size_t * nullable psz)
+
+void *mp_iop_dup_desc_sz(mem_pool_t *mp, const iop_struct_t *st,
+                         const void *v, size_t *nullable psz)
 {
-    bool is_class;
-    size_t sz;
-    uint8_t *dst, *res = *outp;
+    return __mp_iop_dup_desc_sz(mp, st, NULL, v, psz);
+}
 
-    if (unlikely(!v)) {
-        mp_delete(mp, outp);
-        return;
-    }
-
-    if ((is_class = iop_struct_is_class(st))) {
-        st = *(const iop_struct_t **)v;
-        sz = ROUND_UP(st->size, 8) + iop_dup_size(st, v);
-    } else {
-        sz = ROUND_UP(st->size, 8) + __iop_dup_size(st, v);
-    }
-
-    if (psz) {
-        *psz = sz;
-    }
-
-    res = mp_irealloc(mp, res, 0, sz, 8, MEM_RAW);
-
-    if (is_class) {
-        dst = __iop_copy_class(st, res, res, v);
-    } else {
-        dst = realign(mempcpy(res, v, st->size));
-        dst = __iop_copy(st, dst, res, v);
-    }
-
-    assert (dst == res + sz);
-    *outp = res;
+void mp_iop_copy_desc_sz(mem_pool_t *mp, const iop_struct_t *st, void **outp,
+                         const void *v, size_t *nullable psz)
+{
+    *outp = __mp_iop_dup_desc_sz(mp, st, outp, v, psz);
 }
 
 void mp_iop_obj_copy(mem_pool_t *mp, void *out, const void *v, unsigned flags)

commit 26a671db655ce14818ce92ea8490e0b2f563be2e

iop.blk: fix iop_copy_leak.
In case of NULL source pointer, iopy_copy would set out pointer to NULL
without freeing it.
__mp_iop_dup_desc_sz now always set its out pointer if given.

The bug was introduced by 2e8b53e418deee248af7ace92bc710e5065e2172.

Change-Id: Ib01782ce61223b4ea8f1f29fa7c8a488a2a76915
Closes: #58558

iop.blk
@@ -1515,7 +1515,13 @@ static void *__mp_iop_dup_desc_sz(mem_pool_t *mp, const iop_struct_t *st,
     size_t sz;
     uint8_t *dst, *res;
 
-    RETHROW_P(v);
+    if (!v) {
+        if (outp) {
+            mp_ifree(mp, *outp);
+            *outp = NULL;
+        }
+        return NULL;
+    }
 
     if ((is_class = iop_struct_is_class(st))) {
         st = *(const iop_struct_t **)v;
@@ -1542,6 +1548,9 @@ static void *__mp_iop_dup_desc_sz(mem_pool_t *mp, const iop_struct_t *st,
     }
 
     assert (dst == res + sz);
+    if (outp) {
+        *outp = res;
+    }
     return res;
 }
 
@@ -1555,7 +1564,7 @@ void *mp_iop_dup_desc_sz(mem_pool_t *mp, const iop_struct_t *st,
 void mp_iop_copy_desc_sz(mem_pool_t *mp, const iop_struct_t *st, void **outp,
                          const void *v, size_t *nullable psz)
 {
-    *outp = __mp_iop_dup_desc_sz(mp, st, outp, v, psz);
+    __mp_iop_dup_desc_sz(mp, st, outp, v, psz);
 }
 
 void mp_iop_obj_copy(mem_pool_t *mp, void *out, const void *v, unsigned flags)

zchk-iop.c
@@ -6469,6 +6469,16 @@ Z_GROUP_EXPORT(iop)
                      "test failed for class");
     } Z_TEST_END;
     /* }}} */
+    Z_TEST(nr_58558, "avoid leak when copying an IOP with no value") { /* {{{ */
+        tstiop__my_struct_c__t st;
+        tstiop__my_struct_c__t *p;
+
+        iop_init(tstiop__my_struct_c, &st);
+        p = iop_dup(tstiop__my_struct_c, &st);
+        iop_copy(tstiop__my_struct_c, &p, NULL);
+        Z_ASSERT_NULL(p);
+    } Z_TEST_END;
+    /* }}} */
     Z_TEST(iop_field_is_pointed, "test the iop_field_is_pointed function") { /* {{{ */
         struct {
             const iop_struct_t *st;