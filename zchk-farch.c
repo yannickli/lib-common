134aaf88acc47274e4c8b37cd0e8b4b039c9b35a
Matthieu Boutier <matthieu.boutier@intersec.com>
2018-03-26 18:03:33 +0200

    farch: add unit tests.
    Refs: #59195
    Change-Id: Iadbf4f37a9f43e95ba48427bfd6c0dd146833db0

---
+++ zchk-farch.c
@@ -0,0 +1,63 @@
+/**************************************************************************/
+/*                                                                        */
+/*  Copyright (C) 2004-2018 INTERSEC SA                                   */
+/*                                                                        */
+/*  Should you receive a copy of this source code, you must check you     */
+/*  have a proper, written authorization of INTERSEC to hold it. If you   */
+/*  don't have such an authorization, you must DELETE all source code     */
+/*  files in your possession, and inform INTERSEC of the fact you obtain  */
+/*  these files. Should you not comply to these terms, you can be         */
+/*  prosecuted in the extent permitted by applicable law.                 */
+/*                                                                        */
+/**************************************************************************/
+
+#include "z.h"
+#include "farch.h"
+#include "zchk-farch.fc.c"
+
+Z_GROUP_EXPORT(farch)
+{
+    const char *farch_filenames[] = {
+        "test-data/farch/zchk-farch-intersec.txt",
+        "test-data/farch/zchk-farch-five-intersec.txt",
+        "test-data/farch/zchk-farch-lorem.txt",
+    };
+
+    MODULE_REQUIRE(farch);
+
+    Z_TEST(farch, "") {
+        for (int i = 0; i < countof(farch_filenames); i++) {
+            t_scope;
+            const farch_entry_t *entry = &farch_test[i];
+            char ffilename[FARCH_MAX_FILENAME];
+            const char *filename = farch_filenames[i];
+            lstr_t fcontents;
+            lstr_t fcontents_persist;
+            lstr_t contents;
+
+            Z_ASSERT_ZERO(lstr_init_from_file(&contents, filename, PROT_READ,
+                                              MAP_SHARED));
+
+            /* test get_filename */
+            Z_ASSERT_P(farch_get_filename(entry, ffilename));
+            Z_ASSERT_STREQUAL(ffilename, filename);
+
+            /* test get_data */
+            fcontents = t_farch_get_data(farch_test, filename);
+            Z_ASSERT_LSTREQUAL(fcontents, contents);
+            fcontents = t_farch_get_data(entry, NULL);
+            Z_ASSERT_LSTREQUAL(fcontents, contents);
+
+            /* test get_data_persist */
+            fcontents = farch_get_data_persist(farch_test, filename);
+            Z_ASSERT_LSTREQUAL(fcontents, contents);
+            fcontents_persist = farch_get_data_persist(entry, NULL);
+            Z_ASSERT_LSTREQUAL(fcontents_persist, contents);
+            Z_ASSERT(fcontents_persist.s == fcontents.s);
+
+            lstr_wipe(&contents);
+        }
+    } Z_TEST_END;
+
+    MODULE_RELEASE(farch);
+} Z_GROUP_END
