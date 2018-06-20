a0d64eb8c4bc1436df6e37b9d22ec7f4d8594f49
Xavier Chantry <xavier.chantry@intersec.com>
2018-06-20 13:59:59 +0200

    support gcc 8
    False positives here in str-buf.h and str-l.h but gcc 8 found interesting
    bugs in mmsx.
    
    str-l.h:427:12: error: ‘memcmp’ specified size between
    18446744071562067968 and 18446744073709551615 exceeds maximum object size
    9223372036854775807 [-Werror=stringop-overflow=]
         return memcmp(s1.s, s2.s, len) ?: CMP(s1.len, s2.len);
    
    str-buf.h:279:5: error: ‘memcpy’ forming offset [2, 20] is out of the
    bounds [0, 1] of object ‘__sb_slop’ with type ‘const char[1]’
    [-Werror=array-bounds]
         memcpy(sb_growlen(sb, dlen), data, dlen);
    
    Change-Id: Id0c24f4c0a66d7f851dc6c7f30ca33960fb75263
    (cherry picked from commit b28152e46a1f37a2103ead2f527281e850426d0d)

--- str-buf.h
+++ str-buf.h
@@ -241,7 +241,15 @@ static inline char *sb_growlen(sb_t *sb, int extra)
 
 static inline void sb_add(sb_t *sb, const void *data, int dlen)
 {
-    memcpy(sb_growlen(sb, dlen), data, dlen);
+    char *buf = sb_growlen(sb, dlen);
+
+    /* workaround for a false positive of -Warray-bounds in gcc 8
+     * https://gcc.gnu.org/bugzilla/show_bug.cgi?id=89689
+     */
+    if (dlen && sb->data == __sb_slop) {
+        __builtin_unreachable();
+    }
+    memcpy(buf, data, dlen);
 }
 static inline void sb_addsb(sb_t *sb, const sb_t *sb2)
 {
