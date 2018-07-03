d86e4eac4567eee877908817bb87f75795fde0f1
Romain Le Godais <romain.legodais@intersec.com>
2018-07-03 22:58:00 +0200

    build: Move ctags command to a script.
    This script will also be used by waf.
    
    Change-Id: I91adc893d4e617d27b10b75dce765d15176c237c

--- Build/bulk.mk
+++ Build/bulk.mk
@@ -231,32 +231,7 @@ www-coverage::
 __setup_buildsys_tags: | __setup_buildsys_trampoline
 	$(MAKEPARALLEL) -C $/ -f $!Makefile tags
 	@$(if $(shell which ctags),,$(error "Please install ctags: apt-get install exuberant-ctags"))
-	cd $/ && ctags $(TAGSOPTION) -o $(TAGSOUTPUT) --recurse=yes --totals=yes --links=no \
-	    --c-kinds=+p --c++-kinds=+p --fields=+liaS --extra=+q \
-	    -I 'qv_t qm_t qh_t IOP_RPC_IMPL IOP_RPC_CB qvector_t qhp_min_t qhp_max_t MODULE_BEGIN MODULE_END MODULE_DEPENDS_ON+ MODULE_NEEDED_BY+' \
-	    --langmap=c:+.blk --langmap=c:+.h --langmap=c++:+.blkk \
-	    --regex-c='/^OBJ_CLASS(_NO_TYPEDEF)?\(+([^,]+),/\2_t/o, cclass/' \
-	    --regex-c='/^    .*\(\*+([^\ ]+)\)\([a-zA-Z_]+ /\1/x, cmethod/' \
-	    --regex-c='/^qvector_t\(+([a-zA-Z_]+)\,/\1/t, qvector/' \
-	    --regex-c='/^q[hm]_k.*_t\(+([a-zA-Z_]+)/\1/t, qhash/' \
-	    --regex-c='/^MODULE_BEGIN\(+([a-zA-Z_]+)/\1/m, module/' \
-	    --langdef=iop --langmap=iop:.iop \
-	    --regex-iop='/^struct +([a-zA-Z]+)/\1/s, struct/' \
-	    --regex-iop='/^(local +)?(abstract +)?(local +)?class +([a-zA-Z]+)/\4/c, class/' \
-	    --regex-iop='/^union +([a-zA-Z]+)/\1/u, union/' \
-	    --regex-iop='/^enum +([a-zA-Z]+)/\1/e, enum/' \
-	    --regex-iop='/^typedef +[^;]+ +([a-zA-Z]+) *;/\1/t, typedef/' \
-	    --regex-iop='/^\@ctype\(+([a-zA-Z_]+)\)/\1/t, ctype/' \
-	    --regex-iop='/^interface +([a-zA-Z]+)/\1/n, interface/' \
-	    --regex-iop='/^snmpIface +([a-zA-Z]+)/\1/s, snmpiface/' \
-	    --regex-iop='/^snmpObj +([a-zA-Z]+)/\1/s, snmpobj/' \
-	    --regex-iop='/^snmpTbl +([a-zA-Z]+)/\1/s, snmptbl/' \
-	    --exclude=".build*" --exclude="Build" --exclude="Config" \
-	    --exclude=".git" --exclude=".svn" --exclude="CVS" \
-	    --exclude="old" --exclude="new" --exclude="ogu" --exclude="xxx" \
-	    --exclude="*.exe" --exclude="*.js" --exclude="*.blk.c" --exclude="*.blkk.cc" \
-	    --exclude="*.swf" --exclude="*.ini" --exclude="*fake.c" --exclude="compat.c" \
-	    --exclude="js/v8"
+	cd $/ && $(var/toolsdir)/ctags.sh "$(TAGSOPTION)" "$(TAGSOUTPUT)"
 
 tags: TAGSOPTION=
 tags: TAGSOUTPUT=.tags
