29efe4340471f51a97a67ff32ce1213d533102e6
Nicolas Pauss <nicolas.pauss@intersec.com>
2018-09-19 11:39:48 +0200

    build: only limit ctags to useful file types.
    Limit generation of ctags for c,c++,iop,python,php,javascript.
    We don't use them for other file types anyway.
    Also clean up some old unused rules.
    
    Change-Id: I832302b0513943621d9d8cbb8db3078d0c0a1e4a

--- Build/ctags.sh
+++ Build/ctags.sh
@@ -17,13 +17,16 @@ TAGSOUTPUT="$2"
 
 ctags ${TAGSOPTION} -o ${TAGSOUTPUT} --recurse=yes --totals=yes --links=no \
     --c-kinds=+p --c++-kinds=+p --fields=+liaS --extra=+q \
-    -I 'qv_t qm_t qh_t IOP_RPC_IMPL IOP_RPC_CB qvector_t qhp_min_t qhp_max_t MODULE_BEGIN MODULE_END MODULE_DEPENDS_ON+ MODULE_NEEDED_BY+' \
+    \
     --langmap=c:+.blk --langmap=c:+.h --langmap=c++:+.blkk \
+    \
+    -I 'qv_t qm_t qh_t IOP_RPC_IMPL IOP_RPC_CB qvector_t qhp_min_t qhp_max_t MODULE_BEGIN MODULE_END MODULE_DEPENDS_ON+ MODULE_NEEDED_BY+' \
     --regex-c='/^OBJ_CLASS(_NO_TYPEDEF)?\(+([^,]+),/\2_t/o, cclass/' \
     --regex-c='/^    .*\(\*+([^\ ]+)\)\([a-zA-Z_]+ /\1/x, cmethod/' \
     --regex-c='/^qvector_t\(+([a-zA-Z_]+)\,/\1/t, qvector/' \
     --regex-c='/^q[hm]_k.*_t\(+([a-zA-Z_]+)/\1/t, qhash/' \
     --regex-c='/^MODULE_BEGIN\(+([a-zA-Z_]+)/\1/m, module/' \
+    \
     --langdef=iop --langmap=iop:.iop \
     --regex-iop='/^struct +([a-zA-Z]+)/\1/s, struct/' \
     --regex-iop='/^(local +)?(abstract +)?(local +)?class +([a-zA-Z]+)/\4/c, class/' \
@@ -35,9 +38,9 @@ ctags ${TAGSOPTION} -o ${TAGSOUTPUT} --recurse=yes --totals=yes --links=no \
     --regex-iop='/^snmpIface +([a-zA-Z]+)/\1/s, snmpiface/' \
     --regex-iop='/^snmpObj +([a-zA-Z]+)/\1/s, snmpobj/' \
     --regex-iop='/^snmpTbl +([a-zA-Z]+)/\1/s, snmptbl/' \
-    --exclude=".build*" --exclude="Build" --exclude="Config" \
-    --exclude=".git" --exclude=".svn" --exclude="CVS" \
-    --exclude="old" --exclude="new" --exclude="ogu" --exclude="xxx" \
-    --exclude="*.exe" --exclude="*.js" --exclude="*.blk.c" --exclude="*.blkk.cc" \
-    --exclude="*.swf" --exclude="*.ini" --exclude="*fake.c" --exclude="compat.c" \
-    --exclude="js/v8"
+    \
+    --exclude=".build*" --exclude=".git" \
+    --exclude="*.blk.c" --exclude="*.blkk.cc" \
+    --exclude="js/v8" --exclude="ext" --exclude="node_modules/*" \
+    \
+    --languages='c,c++,iop,python,php,javascript'
