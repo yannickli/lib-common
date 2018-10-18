f846dca994c3847de91930cde8b89b679807fc23
Vincent Thiberville <vincent.thiberville@intersec.com>
2018-10-18 17:36:50 +0200

    Build: handle profile in webpack compilation
    Add WEBPACK_MODE as a variable set by different configurations, and use
    it when calling webpack.
    This is slightly different from "NODE_ENV" because we want to
    distinguish 3 use cases:
    
    * Normal usecase: we just want the web compilation to be as fast as
      possible.
    * Web development use-case: we want to have the best source maps, and
      use watchers so that changes are immediately taken into account
    * Release usecase: optimize the build
    
    This is handled in platform/www/webpack.config.js. The normal usecase is
    when mode is left unset. The web dev use-case will be mode=development,
    and will be set by a custom script (that also add watchers, etc).
    
    Change-Id: If30f2c01337fdd523909cb5d7cd95a2e68f101c4

--- Build/rules-frontend.mk
+++ Build/rules-frontend.mk
@@ -412,10 +412,11 @@ else
 $(1DV)www-check-deps:: $1
 endif
 $1: _CFGFILE=$(or $(patsubst $(1DV)%,%,$($1_CONFIG)),webpack.config.js)
+$1: _MODE=$(if $(WEBPACK_MODE),--mode $(WEBPACK_MODE))
 $1: $(var/wwwtool)webpack
 $1: FORCE
 	$(msg/PACK.js) $1
-	cd $(1DV) && $(var/wwwtool)webpack --mode=development --config $$(_CFGFILE)
+	cd $(1DV) && $(var/wwwtool)webpack --config $$(_CFGFILE) $$(_MODE)
 endef
 
 #}}}
