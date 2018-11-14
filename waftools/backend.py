eda3826d5abb8fcc37ae98cd0e3951c51da02099
Romain Le Godais <romain.legodais@intersec.com>
2018-11-14 13:57:29 +0100

    waf: Execute farch and tokens tasks before blk ones.
    I observed a build that failed on Igloo with the following error:
    
      igloo/base.blk:21:10: fatal error: 'iglootokens.h' file not found
    
    This happened because the Blk2c task for preprocessing base.blk was
    executed before the Tokens2c task that generates iglootokens.h.
    
    It was possible (but very very unlikely) because there was no constraint
    about the execution order.
    
    Change-Id: Iabbc95eaa237ea52d7950ddc2fba248443ac60dc

--- waftools/backend.py
+++ waftools/backend.py
@@ -582,6 +582,7 @@ def process_lex(self, node):
 class Fc2c(Task):
     run_str = '${FARCHC} -c -o ${TGT} ${SRC[0].abspath()}'
     color   = 'BLUE'
+    before  = ['Blk2c', 'Blkk2cc']
     ext_out = ['.h']
 
     @classmethod
@@ -635,6 +636,7 @@ class Tokens2c(Task):
     run_str = ('${TOKENS_SH} ${SRC[0].abspath()} ${TGT[0]} && ' +
                '${TOKENS_SH} ${SRC[0].abspath()} ${TGT[1]}')
     color   = 'BLUE'
+    before  = ['Blk2c', 'Blkk2cc']
     ext_out = ['.h', '.c']
 
     @classmethod
