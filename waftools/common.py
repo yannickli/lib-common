a86209e02edcf374768fbe644f8e2b0fc6168bdc
Romain Le Godais <romain.legodais@intersec.com>
2018-11-16 10:46:58 +0100

    waf: Add more execution constraints on CustomInstall tasks.
    They must always be executed after the linking tasks, just in case the
    install task generator is not the same as the one that builds the binary
    (which happens in the tools repository).
    
    Change-Id: I977a1dc44e7a5351a328f642430b5d4011171b24

--- waftools/common.py
+++ waftools/common.py
@@ -210,7 +210,7 @@ def remove_default_install_tasks(self):
 class CustomInstall(Task):
     """ Task to start custom shell commands on install. """
     color = 'PINK'
-    after = ['DeployTarget', 'vnum']
+    after = ['DeployTarget', 'cprogram', 'cshlib', 'vnum']
 
     def __str__(self):
         launch_node = self.generator.bld.launch_node()
