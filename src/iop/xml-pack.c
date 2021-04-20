e1e6ba9f50264b11dbff6a3e637634cd82b4d962
Romain Le Godais <romain.legodais@intersec.com>
2021-04-20 13:11:04 +0200

    iop[json/xml]: Use %.17g insted of %.17e to print doubles.
    The "g" formatter produces more readable output, for the same precision.
    
    Change-Id: Idaf06a5c387946a49ada3b643a32205faa96a0ec
    Refs: #77738

--- src/iop/xml-pack.c
+++ src/iop/xml-pack.c
@@ -114,7 +114,7 @@ xpack_value(sb_t *sb, const iop_struct_t *desc, const iop_field_t *f,
         if (isinf(d)) {
             sb_adds(sb, d < 0 ? "-INF" : "INF");
         } else {
-            sb_addf(sb, "%.17e", d);
+            sb_addf(sb, "%.17g", d);
         }
         break;
       case IOP_T_BOOL:
