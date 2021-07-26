d7f1bbd50173f48fd6cdc10e5b5e95a5d38607ee
Romain Le Godais <romain.legodais@intersec.com>
2021-07-26 16:25:07 +0200

    Remove LIB_COMMON_MAJOR/LIB_COMMON_MINOR & co.
    These are unused, let's simplify (a bit) the branching procedure.
    
    Change-Id: If942fe3e80d6245f8f82907fc825b08edf8d715a

--- src/core.h
+++ src/core.h
@@ -20,13 +20,6 @@
 #define IS_LIB_COMMON_CORE_H
 
 #define LIB_COMMON_VERSION  "master"
-#define LIB_COMMON_MAJOR    9999
-#define LIB_COMMON_MINOR    1
-#define __LIB_COMMON_VER(Maj, Min)  (((Maj) << 16) | Min)
-
-#define LIB_COMMON_PREREQ(Maj, Min)                                          \
-    (__LIB_COMMON_VER(Maj, Min) <= __LIB_COMMON_VER(LIB_COMMON_MAJOR,        \
-                                                    LIB_COMMON_MINOR))
 
 #if HAS_LIBCOMMON_REPOSITORY
 # define LIBCOMMON_PATH "lib-common/"
