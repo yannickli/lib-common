90a97c8c9ea615a8a018874df5a17bf1fc54ffc0
Jonathan Squirawski <jonathan.squirawski@intersec.com>
2023-03-03 10:58:21 +0100

    ctags: manually define typescript language only if needed
    Typescript is part of the builtin languages supported by Universal ctags
    and as such cannot be redefined.
    
    So we have to first detect whether the language is supported or not by
    the current ctags before manually defining it.
    
    Change-Id: Iabf40b707cb0bd6ab58fe4048d25dea068f3b45e

--- Build/ctags.sh
+++ Build/ctags.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ##########################################################################
 #                                                                        #
 #  Copyright (C) INTERSEC SA                                             #
@@ -14,6 +14,31 @@
 
 TAGSOPTION="$1"
 TAGSOUTPUT="$2"
+
+ctags_has_lang() {
+    ctags --list-languages | grep -iqF "$1"
+}
+
+TAGS_TYPESCRIPT_DEF=(
+    --langdef=typescript
+    --regex-typescript='/^[ \t]*(export[ \t]+([a-z]+[ \t]+)?)?class[ \t]+([a-zA-Z0-9_$]+)/\3/c,classes/'
+    --regex-typescript='/^[ \t]*(declare[ \t]+)?namespace[ \t]+([a-zA-Z0-9_$]+)/\2/c,modules/'
+    --regex-typescript='/^[ \t]*(export[ \t]+)?module[ \t]+([a-zA-Z0-9_$]+)/\2/n,modules/'
+    --regex-typescript='/^[ \t]*(export[ \t]+)?(async[ \t]+)?function[ \t]+([a-zA-Z0-9_$]+)/\3/f,functions/'
+    --regex-typescript='/^[ \t]*export[ \t]+(var|let|const)[ \t]+([a-zA-Z0-9_$]+)/\2/v,variables/'
+    --regex-typescript='/^[ \t]*(var|let|const)[ \t]+([a-zA-Z0-9_$]+)[ \t]*=[ \t]*function[ \t]*[*]?[ \t]*\(\)/\2/v,varlambdas/'
+    --regex-typescript='/^[ \t]*(export[ \t]+)?(public|protected|private)[ \t]+(static[ \t]+)?(abstract[ \t]+)?(((get|set)[ \t]+)|(async[ \t]+[*]*[ \t]*))?([a-zA-Z1-9_$]+)/\9/m,members/'
+    --regex-typescript='/^[ \t]*(export[ \t]+)?interface[ \t]+([a-zA-Z0-9_$]+)/\2/i,interfaces/'
+    --regex-typescript='/^[ \t]*(export[ \t]+)?type[ \t]+([a-zA-Z0-9_$]+)/\2/t,types/'
+    --regex-typescript='/^[ \t]*(export[ \t]+)?enum[ \t]+([a-zA-Z0-9_$]+)/\2/e,enums/'
+    --regex-typescript='/^[ \t]*import[ \t]+([a-zA-Z0-9_$]+)/\1/I,imports/'
+)
+
+if ctags_has_lang "typescript"; then
+    # Typescript is already builtin in ctags no need to redefine it (which is
+    # actually forbidden with Universal Ctags)
+    TAGS_TYPESCRIPT_DEF=()
+fi
 
 ctags ${TAGSOPTION} -o ${TAGSOUTPUT} --recurse=yes --totals=yes --links=no \
     --c-kinds=+p --c++-kinds=+p --fields=+liaS --extra=+q \
@@ -40,20 +65,9 @@ ctags ${TAGSOPTION} -o ${TAGSOUTPUT} --recurse=yes --totals=yes --links=no \
     --regex-iop='/^snmpTbl +([a-zA-Z]+)/\1/S,snmpobj/' \
     \
     --langmap=javascript:+.jsx \
-    --langdef=typescript \
+    "${TAGS_TYPESCRIPT_DEF[@]}" \
     --langmap=typescript:+.ts \
     --langmap=typescript:+.tsx \
-    --regex-typescript='/^[ \t]*(export[ \t]+([a-z]+[ \t]+)?)?class[ \t]+([a-zA-Z0-9_$]+)/\3/c,classes/' \
-    --regex-typescript='/^[ \t]*(declare[ \t]+)?namespace[ \t]+([a-zA-Z0-9_$]+)/\2/c,modules/' \
-    --regex-typescript='/^[ \t]*(export[ \t]+)?module[ \t]+([a-zA-Z0-9_$]+)/\2/n,modules/' \
-    --regex-typescript='/^[ \t]*(export[ \t]+)?(async[ \t]+)?function[ \t]+([a-zA-Z0-9_$]+)/\3/f,functions/' \
-    --regex-typescript='/^[ \t]*export[ \t]+(var|let|const)[ \t]+([a-zA-Z0-9_$]+)/\2/v,variables/' \
-    --regex-typescript='/^[ \t]*(var|let|const)[ \t]+([a-zA-Z0-9_$]+)[ \t]*=[ \t]*function[ \t]*[*]?[ \t]*\(\)/\2/v,varlambdas/' \
-    --regex-typescript='/^[ \t]*(export[ \t]+)?(public|protected|private)[ \t]+(static[ \t]+)?(abstract[ \t]+)?(((get|set)[ \t]+)|(async[ \t]+[*]*[ \t]*))?([a-zA-Z1-9_$]+)/\9/m,members/' \
-    --regex-typescript='/^[ \t]*(export[ \t]+)?interface[ \t]+([a-zA-Z0-9_$]+)/\2/i,interfaces/' \
-    --regex-typescript='/^[ \t]*(export[ \t]+)?type[ \t]+([a-zA-Z0-9_$]+)/\2/t,types/' \
-    --regex-typescript='/^[ \t]*(export[ \t]+)?enum[ \t]+([a-zA-Z0-9_$]+)/\2/e,enums/' \
-    --regex-typescript='/^[ \t]*import[ \t]+([a-zA-Z0-9_$]+)/\1/I,imports/' \
     \
     --exclude=".build*" --exclude=".git" \
     --exclude="*.blk.c" --exclude="*.blkk.cc" \
